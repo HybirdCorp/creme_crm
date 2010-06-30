22d60a8605934838d3c47a5a99883156fa09688f
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2010-06-30 11:57:17 +0200

    refs #3 adding icon in Image detailview + improve popup detail view. refs #1 remove old *as_p() method in CremeEntity.

--- creme/media_managers/urls.py
+++ creme/media_managers/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.defaults import patterns
 urlpatterns = patterns('media_managers.views',
     (r'^$', 'portal.portal_media_managers'),
 
-    (r'^images$',                       'image.listview'),
-    (r'^image/add$',                    'image.add'),
-    (r'^image/edit/(?P<image_id>\d+)$', 'image.edit'),
-    (r'^image/(?P<image_id>\d+)$',      'image.detailview'),
+    (r'^images$',                        'image.listview'),
+    (r'^image/add$',                     'image.add'),
+    (r'^image/edit/(?P<image_id>\d+)$',  'image.edit'),
+    (r'^image/(?P<image_id>\d+)$',       'image.detailview'),
     (r'^image/popup/(?P<image_id>\d+)$', 'image.popupview'),
 )
 
