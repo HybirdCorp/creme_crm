8ae5f347122d9545bbd87277484a8095a80b4b3e
Guillaume Englert <genglert at hybird.org@localhost>
2010-07-07 15:20:58 +0200

    refs #5 first version of customfields in header filter (not searchable neither sortable).

--- creme/creme_core/forms/header_filter.py
+++ creme/creme_core/forms/header_filter.py
@@ -28,8 +28,8 @@ from django.forms.widgets import HiddenInput
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 
-from creme_core.models.header_filter import HeaderFilterItem, HeaderFilter, HFI_FIELD, HFI_RELATION, HFI_FUNCTION
-from creme_core.models import RelationType, RelationPredicate_i18n
+from creme_core.models.header_filter import HeaderFilterItem, HeaderFilter, HFI_FIELD, HFI_RELATION, HFI_FUNCTION, HFI_CUSTOM
+from creme_core.models import RelationType, RelationPredicate_i18n, CustomField
 from creme_core.forms import CremeModelForm
 from creme_core.forms.widgets import OrderedMultipleChoiceWidget
 from creme_core.utils.meta import get_flds_with_fk_flds_str, get_model_field_infos
@@ -39,10 +39,11 @@ from creme_core.utils.id_generator import generate_string_id_and_save
 #TODO: create and edit form ????
 
 class HeaderFilterForm(CremeModelForm):
-    entity_type = ModelChoiceField(queryset=ContentType.objects.none(), widget=HiddenInput())
-    fields      = MultipleChoiceField(label=_(u'Champs normaux'), required=False, choices=(), widget=OrderedMultipleChoiceWidget)
-    relations   = MultipleChoiceField(label=_(u'Relations'),      required=False, choices=(), widget=OrderedMultipleChoiceWidget)
-    functions   = MultipleChoiceField(label=_(u'Fonctions'),      required=False, choices=(), widget=OrderedMultipleChoiceWidget)
+    entity_type   = ModelChoiceField(queryset=ContentType.objects.none(), widget=HiddenInput()) #TODO: store in a attribute instead....
+    fields        = MultipleChoiceField(label=_(u'Champs normaux'),       required=False, choices=(), widget=OrderedMultipleChoiceWidget)
+    custom_fields = MultipleChoiceField(label=_(u'Champs personnalisés'), required=False, choices=(), widget=OrderedMultipleChoiceWidget)
+    relations     = MultipleChoiceField(label=_(u'Relations'),            required=False, choices=(), widget=OrderedMultipleChoiceWidget)
+    functions     = MultipleChoiceField(label=_(u'Fonctions'),            required=False, choices=(), widget=OrderedMultipleChoiceWidget)
 
     class Meta:
         model = HeaderFilter
@@ -64,14 +65,17 @@ class HeaderFilterForm(CremeModelForm):
         ct = ContentType.objects.get_for_id(ct_id) if not instance.id else instance.entity_type
         model_klass = ct.model_class()
 
-        fields['fields'].choices    = get_flds_with_fk_flds_str(model_klass, 1)
+        fields['fields'].choices = get_flds_with_fk_flds_str(model_klass, 1)
+        fields['custom_fields'].choices = CustomField.objects.filter(content_type=ct).values_list('id', 'name')
         fields['relations'].choices = RelationType.objects.filter(Q(subject_ctypes=ct)|Q(subject_ctypes__isnull=True)).order_by('predicate').values_list('id', 'predicate')
         fields['functions'].choices = ((f['name'], f['verbose_name']) for f in model_klass.users_allowed_func)
 
         if instance.id:
+            #TODO: do 1 query instead of 3
             HFI_filter = HeaderFilterItem.objects.filter(header_filter__id=instance.id).order_by('order').filter
 
             fields['fields'].initial    = [fs.rpartition('__')[0] for fs in HFI_filter(type=HFI_FIELD).values_list('filter_string', flat=True)]
+            fields['custom_fields'].initial = [int(i) for i in HFI_filter(type=HFI_CUSTOM).values_list('name', flat=True)]
             fields['relations'].initial = HFI_filter(type=HFI_RELATION).values_list('relation_predicat_id', flat=True)
             fields['functions'].initial = HFI_filter(type=HFI_FUNCTION).values_list('name', flat=True)
 
@@ -92,9 +96,7 @@ class HeaderFilterForm(CremeModelForm):
             super(HeaderFilterForm, self).save(commit=False)
             generate_string_id_and_save(HeaderFilter, [instance], 'creme_core-userhf_%s-%s' % (ct.app_label, ct.model))
 
-
-        order        = 1
-        model_klass  = instance.entity_type.model_class() #TODO: why not instance.__class__ ?????
+        model_klass  = instance.entity_type.model_class()
         items_2_save = []
 
         get_metafield = model_klass._meta.get_field
@@ -109,44 +111,51 @@ class HeaderFilterForm(CremeModelForm):
                 elif isinstance(field_obj, models.BooleanField):
                     pattern = "%s__creme-boolean"
 
-                items_2_save.append(HeaderFilterItem(order=order,
-                                                     name=field.partition('__')[0],
+                items_2_save.append(HeaderFilterItem(name=field.partition('__')[0],
                                                      title=u" - ".join(unicode(field_info['field'].verbose_name) for field_info in field_infos),
                                                      type=HFI_FIELD,
-                                                     header_filter_id=instance.id,
                                                      has_a_filter=True,
-                                                     editable=True ,
+                                                     editable=True,
                                                      sortable=True,
                                                      filter_string=pattern % field))
-                order += 1
             except (FieldDoesNotExist, AttributeError), e:
                 debug('Exception in HeaderFilterForm.save(): %s', e)
 
-        get_relationtype = RelationType.objects.get   #TODO: regroup the retrieving with a filter + in_bulks ??
+        get_cfield = CustomField.objects.get
+        for cfield in cleaned_data['custom_fields']:
+            items_2_save.append(HeaderFilterItem(name=cfield,
+                                                 title=get_cfield(pk=cfield).name,
+                                                 type=HFI_CUSTOM,
+                                                 has_a_filter=False, #TODO
+                                                 editable=False, #TODO: make it editable
+                                                 sortable=False, #TODO: make it sortable
+                                                 filter_string=pattern % field))
+
+        #TODO: regroup the retrieving with a filter + in_bulks ??  ==> No use a 'cache' instead
+        get_relationtype = RelationType.objects.get
         for rel in cleaned_data['relations']:
             rel_name = get_relationtype(pk=rel).predicate #TODO 18n
 
-            items_2_save.append(HeaderFilterItem(order=order,
-                                                 name=rel_name.replace(' ', '_').replace("'", "_"),
+            items_2_save.append(HeaderFilterItem(name=rel_name.replace(' ', '_').replace("'", "_"),
                                                  title=rel_name,#.replace("'", " "),
                                                  type=HFI_RELATION,
-                                                 header_filter_id=instance.id,
                                                  has_a_filter=False,
                                                  editable=False ,
                                                  filter_string="",
                                                  relation_predicat_id=rel))
-            order += 1
 
         get_funcname = model_klass.get_users_func_verbose_name
         for func in cleaned_data['functions']:
-            items_2_save.append(HeaderFilterItem(order=order,
-                                                 name=func,
+            items_2_save.append(HeaderFilterItem(name=func,
                                                  title=get_funcname(func),
                                                  type=HFI_FUNCTION,
-                                                 header_filter_id=instance.id,
                                                  has_a_filter=False,
                                                  editable=False,
                                                  filter_string=""))
-            order += 1
 
-        generate_string_id_and_save(HeaderFilterItem, items_2_save,'creme_core-userhfi_%s-%s' % (ct.app_label, ct.model))
+        for i, hfi in enumerate(items_2_save):
+            hfi.order = i + 1
+            hfi.header_filter = instance
+
+        generate_string_id_and_save(HeaderFilterItem, items_2_save,
+                                    'creme_core-userhfi_%s-%s' % (ct.app_label, ct.model))
