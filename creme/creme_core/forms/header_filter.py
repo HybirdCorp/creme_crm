b2dcee01d415326fa8b564533444cf3c50ba92dc
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2010-07-13 14:29:12 +0200

    refs #5 * CustomFieldValue has been replaced by typed tables: CustomFieldInteger, CustomFieldEnum, CustomFieldDate etc... * new CF types: bool, date * research on CF in listviews temporarily disabled ; more duplicated queries on listview for CF...

--- creme/creme_core/forms/header_filter.py
+++ creme/creme_core/forms/header_filter.py
@@ -140,7 +140,8 @@ class HeaderFilterForm(CremeModelForm):
             items_2_save.append(HeaderFilterItem(name=cfield_id,
                                                  title=self._get_cfield_name(int(cfield_id)),
                                                  type=HFI_CUSTOM,
-                                                 has_a_filter=True,
+                                                 #has_a_filter=True,
+                                                 has_a_filter=False,
                                                  editable=False, #TODO: make it editable
                                                  sortable=False, #TODO: make it sortable
                                                  filter_string=''))
