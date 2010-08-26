6e17a98e5e2e7a69107b24edc20f45b478ac2e6b
Raphael BECK <rbeck at hybird.org@localhost>
2010-08-26 16:23:11 +0200

    refs #1 * New js menu

--- creme/media/js/jquery/extensions/include.js
+++ creme/media/js/jquery/extensions/include.js
@@ -46,8 +46,7 @@ creme.include('/site_media/js/i18n.js','js');
 include('/site_media/js/jquery/extensions/jquery.utils.js','js');
 include('/site_media/js/jquery/extensions/wait.js','js');
 include('/site_media/js/jquery/extensions/highlight.js','js');
-//include('/site_media/js/jquery/extensions/dynatree/dev/jquery.dynatree.js','js');
-include('/site_media/css/dynatree/ui.dynatree.css','css');
+include('/site_media/css/fg-menu-3.0/fg.menu.css','css');
 include('/site_media/js/creme/creme.utils.js','js');
 creme.include('/site_media/js/models/properties.js','js');
 creme.include('/site_media/js/models/activities.js','js'); //TODO: improve the js system to remove this file from here.....
