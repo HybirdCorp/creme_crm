b9a886db586bddb1d509a85aea6c880f50a0da85
Guillaume Englert <genglert at hybird.org@localhost>
2010-09-05 11:07:13 +0200

    refs #22 Can now select associated relation types for each root node ; can select orbital relation types too. BEWARE: Models have completely changed.

--- creme/graphs/urls.py
+++ creme/graphs/urls.py
@@ -10,6 +10,13 @@ urlpatterns = patterns('graphs.views',
     (r'^graph/(?P<graph_id>\d+)/png$',  'graph.dl_png'),
     (r'^graph/edit/(?P<graph_id>\d+)$', 'graph.edit'),
     (r'^graph/(?P<graph_id>\d+)$',      'graph.detailview'),
+
+    (r'^graph/(?P<graph_id>\d+)/relation_types/add$',   'graph.add_relation_types'),
+    (r'^graph/(?P<graph_id>\d+)/relation_type/delete$', 'graph.delete_relation_type'),
+
+    (r'^graph/(?P<graph_id>\d+)/roots/add$', 'root_node.add'),
+    (r'^root/edit/(?P<root_id>\d+)/',        'root_node.edit'),
+    (r'^root/delete$',                       'root_node.delete'),
 )
 
 urlpatterns += patterns('creme_core.views.generic',
---
+++ creme/graphs/views/root_node.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+#    Creme is a free/open-source Customer Relationship Management software
+#    Copyright (C) 2009-2010  Hybird
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render_to_response
+from django.utils.translation import ugettext as _
+from django.contrib.auth.decorators import login_required
+
+from creme_core.entities_access.functions_for_permissions import get_view_or_die, edit_object_or_die
+from creme_core.views.generic import inner_popup
+
+from graphs.models import Graph, RootNode
+from graphs.forms.root_node import AddRootNodesForm, EditRootNodeForm
+
+
+ #TODO: no inner_popup because GenericEntitiesField doesn't work with inner_popup ;(
+@login_required
+@get_view_or_die('graphs')
+def add(request, graph_id):
+    graph = get_object_or_404(Graph, pk=graph_id)
+
+    die_status = edit_object_or_die(request, graph)
+    if die_status:
+        return die_status
+
+    if request.POST:
+        nodes_form = AddRootNodesForm(graph, request.POST)
+
+        if nodes_form.is_valid():
+            nodes_form.save()
+
+            return HttpResponseRedirect(graph.get_absolute_url()) ###
+    else:
+        nodes_form = AddRootNodesForm(graph=graph)
+
+    #return inner_popup(request, 'creme_core/generics/blockform/add_popup2.html',
+                       #{
+                        #'form':   nodes_form,
+                        #'title':  _(u'Add root nodes'),
+                       #},
+                       #is_valid=nodes_form.is_valid(),
+                       #reload=False,
+                       #delegate_reload=True,
+                       #context_instance=RequestContext(request))
+
+    return render_to_response('creme_core/generics/blockform/add.html',
+                              {
+                                'form': nodes_form,
+                              },
+                              context_instance=RequestContext(request))
+
+@login_required
+@get_view_or_die('graphs')
+def edit(request, root_id):
+    root_node = get_object_or_404(RootNode, pk=root_id)
+    graph     = root_node.graph
+
+    die_status = edit_object_or_die(request, graph)
+    if die_status:
+        return die_status
+
+    if request.POST:
+        nodes_form = EditRootNodeForm(request.POST, instance=root_node)
+
+        if nodes_form.is_valid():
+            nodes_form.save()
+    else:
+        nodes_form = EditRootNodeForm(instance=root_node)
+
+    return inner_popup(request, 'creme_core/generics/blockform/edit_popup.html',
+                       {
+                        'form':   nodes_form,
+                        'title':  _(u'Edit root node for <%s>') % graph,
+                       },
+                       is_valid=nodes_form.is_valid(),
+                       reload=False,
+                       delegate_reload=True,
+                       context_instance=RequestContext(request))
+
+@login_required
+@get_view_or_die('graphs')
+def delete(request):
+    root_node = get_object_or_404(RootNode, pk=request.POST.get('id'))
+
+    die_status = edit_object_or_die(request, root_node.graph)
+    if die_status:
+        return die_status
+
+    root_node.delete()
+
+    return HttpResponse("", mimetype="text/javascript")
