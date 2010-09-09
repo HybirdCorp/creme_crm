4f16118db374b39dcc568c4ea1b49db93df11181
Raphael BECK <rbeck at hybird.org@localhost>
2010-09-09 13:14:43 +0200

    refs #21 * Pie implementation * Can change between "registered" charts

--- creme/media/js/creme/creme.graphael.js
+++ creme/media/js/creme/creme.graphael.js
@@ -149,89 +149,111 @@ creme.graphael.simple_barchart = function(options)
     }
 };
 
-creme.graphael.simple_barchart.refetch = function(url, container_selector, o_lbl, a_lbl, date_selector)
+creme.graphael.simple_pie = function(options)
 {
-    creme.ajax.json.get(url, {}, function(d){
-        var x = d.x;
-        var y = d.y;
-        var graph_id = d.graph_id;
+    if(!options || options == undefined || options.instance==undefined) return;//Need at least a Raphael's instance
+    var graphael = options.instance;
+    var container = options.container;
 
-        var $container = $(container_selector);
-        var instance = $container.data('graphael');
-        instance.clear();
-        $container.removeClass('graphael-barchart-drawn');
+    if(!container.hasClass('graphael-pie-drawn'))
+    {
+        var paddings = $.extend(creme.graphael.paddings, options.paddings);
+        var x = options.x || [], y = options.y || [];
 
-        creme.graphael.simple_barchart({
-               'instance': instance,
-               'container':$container,
-               'x': x,
-               'y': y,
-               'gen_date_selector': date_selector,
-               'abscissa_lbl':a_lbl,
-               'ordinate_lbl':o_lbl
-            });
-    });
-};
+        var lpad = paddings.L_PAD;
+        var hpad = paddings.H_PAD;
+        var ipad = paddings.I_PAD;
 
-//TODO: Do me more generic ? Permit more than 2 curves ? Why a json option object doesn't work ???
-// Rename variables
-creme.graphael._init_evolution_graph = function (targetNodeId, x1, y1, y2)
-{
-    var table = $('#'+targetNodeId);
+        var graphael_width = graphael.width;
+        var graphael_height = graphael.height;
+        var gtext = graphael.g.text;
 
-    var t_width = table.width();
-    var t_height = t_width * 0.35;
+        graphael.g.txtattr.font = "12px 'Fontin Sans', Fontin-Sans, sans-serif";
 
-    var L_PAD = 15;//left padding
-    var R_PAD = 15;//right padding
-    var H_PAD = 10;//top padding
-    var B_PAD = 10;//bottom padding
-    var I_PAD = 4;//Inter elements padding not an apple thing ;)
-    var CUR_Y = H_PAD;
-    var paid_color = '#a2bf2f';
-    var promise_color = '#94C6DB';
+        var parsed_y = [];
+        for(var i=-1, l=y.length; ++i < l;)
+        {
+            var current = parseInt(y[i]);
+            if(isNaN(current)) current = 0;
 
-    //var r = Raphael(table[0], t_width, t_height);
-    var r = Raphael(targetNodeId, t_width, t_height);
+            parsed_y.push(current);
+        }
 
-    var c_paid      = r.circle(L_PAD, CUR_Y, 5).attr({fill: paid_color, stroke: "#fff", "stroke-width": 1});
-    var lbl_paid    = r.g.text(0, 0, "Sommes effectivement versées");
-    lbl_paid.attr({
-        x : c_paid.getBBox().x + lbl_paid.getBBox().width/2 + 4*I_PAD,
-        y : c_paid.getBBox().y + c_paid.getBBox().height/2
-    });
+        var pie_width = graphael_width/5;
+        var pie = graphael.g.piechart(pie_width+lpad, graphael_height/2, pie_width, parsed_y,
+                                      {
+                                        legend: x,
+                                        legendpos: "east"
+                                      });
+        pie.hover(function (){
+            var sector = this.sector;
+            var label  = this.label;
+            sector.stop();
+            sector.scale(1.1, 1.1, this.cx, this.cy);
+            var value = (sector.value != undefined && sector.value.value != undefined) ? sector.value.value : 100;
+            this.flag = graphael.g.popup(sector.middle.x, sector.middle.y + creme.graphael.paddings.I_PAD, value);
 
-    var c_promise   = r.circle(lbl_paid.getBBox().x + lbl_paid.getBBox().width + 4*I_PAD , CUR_Y, 5).attr({fill: promise_color, stroke: "#fff", "stroke-width": 1});
-    var lbl_promise = r.g.text(0 , 0, "Sommes promises");
-    lbl_promise.attr({
-        x : c_promise.getBBox().x + lbl_promise.getBBox().width/2 + 4*I_PAD,
-        y : c_promise.getBBox().y + c_promise.getBBox().height/2
-    });
-
-    CUR_Y += lbl_promise.getBBox().y;
-
-    var lines = r.g.linechart((t_width*0.15)/2, CUR_Y, t_width*0.85, t_height-CUR_Y-B_PAD, [x1,x1], [y1, y2], {nostroke: false, axis: "0 0 1 1", symbol: "o", shade:true, smooth: false, gutter:15, axisxstep:x1.length, colors : [paid_color,promise_color]}).hoverColumn(function () {
-            this.tags = r.set();
-            for (var i = 0, ii = this.y.length; i < ii; i++) {
-                this.tags.push(r.g.tag(this.x, this.y[i], this.values[i], 160, 10).insertBefore(this).attr([{fill: "#fff"}, {fill: this.symbols[i].attr("fill")}]));
+            if (label) {
+                label[0].stop();
+                label[0].scale(1.5);
+                label[1].attr({"font-weight": 800});
             }
         }, function () {
-            this.tags && this.tags.remove();
+            var label  = this.label;
+            
+            this.sector.animate({scale: [1, 1, this.cx, this.cy]}, 500, "bounce");
+
+            this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+
+            if (label) {
+                label[0].animate({scale: 1}, 500, "bounce");
+                label[1].attr({"font-weight": 400});
+            }       
         });
-    lines.symbols.attr({r: 3});
 
-    var lbl_sum = r.g.text(0 , 0, "Somme");
-    lbl_sum.rotate(270);
-    lbl_sum.attr({
-        x : c_paid.getBBox().x,
-        y : lines.getBBox().height/2
-    });
-
-    var lbl_year = r.g.text(0 , 0, "Année");
-    lbl_year.attr({
-        x : lines.getBBox().x + lines.getBBox().width/2,
-        y : lines.getBBox().y + lines.getBBox().height + 4*I_PAD
-    });
+        if(options.gen_date_selector)
+        {
+            $(options.gen_date_selector).text(new Date().toString("dd-MM-yyyy HH:mm:ss"));//Needs Datejs but doesn't bug when not
+        }
+        container.addClass('graphael-pie-drawn');
+    }
 };
 
 
+creme.graphael.simple_refetch = function(url, type, container_selector, o_lbl, a_lbl, date_selector)
+{
+    var type_infos = creme.graphael.simple_refetch.types[type];
+    if(type_infos != undefined)
+    {
+        creme.ajax.json.get(url, {}, function(d){
+            var x = d.x;
+            var y = d.y;
+
+            var $container = $(container_selector);
+            var instance = $container.data('graphael');
+            instance.clear();
+            $container.removeClass(type_infos.css_class);
+
+            type_infos.func({
+                   'instance': instance,
+                   'container':$container,
+                   'x': x,
+                   'y': y,
+                   'gen_date_selector': date_selector,
+                   'abscissa_lbl':a_lbl,
+                   'ordinate_lbl':o_lbl
+                });
+        });
+    }
+};
+
+creme.graphael.simple_refetch.types = {
+    histogram: {
+                'css_class':'graphael-barchart-drawn',
+                'func': creme.graphael.simple_barchart
+    },
+    pie: {
+        'css_class':'graphael-pie-drawn',
+        'func': creme.graphael.simple_pie
+    }
+};
