05ca4fa54399c6aca10b47b594299a9ca740633f
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2010-09-21 17:18:01 +0200

    refs #1 First version of quick creation: only one entity is created, only Contact & Organisation types.

--- creme/creme_core/gui/quick_forms.py
+++ creme/creme_core/gui/quick_forms.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
 #    Copyright (C) 2009-2010  Hybird
@@ -17,23 +18,26 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-from django import template
-from django.contrib.contenttypes.models import ContentType
-
-from creme_core.registry import creme_registry
+from logging import warning
 
 
-register = template.Library()
+class QuickFormsRegistry(object):
+    def __init__(self):
+        self._forms = {}
 
-@register.inclusion_tag('creme_core/templatetags/search_panel.html', takes_context=True)
-def get_search_panel(context, target_node_id='sub_content'):
-    get_ct = ContentType.objects.get_for_model
-    content_types = [{'id': get_ct(model).id, 'verbose_name': model._meta.verbose_name} for model in creme_registry.iter_entity_models()]
-    content_types.sort(key=lambda k: k['verbose_name'])
+    def register(self, model, form):
+        forms = self._forms
 
-    context.update({
-            'content_types':  content_types,
-            'target_node_id': target_node_id, #Ajax version / set your target html node's id
-        })
+        if forms.has_key(model):
+            warning("A Quick Form is alerady registered for the model : %s", model) #exception instead ???
 
-    return context
+        forms[model] = form
+
+    def iter_models(self):
+        return self._forms.iterkeys()
+
+    def get_form(self, model):
+        return self._forms[model]
+
+
+quickforms_registry = QuickFormsRegistry()
--- creme/persons/forms/quick.py
+++ creme/persons/forms/quick.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
 #    Copyright (C) 2009-2010  Hybird
@@ -17,23 +18,18 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-from django import template
-from django.contrib.contenttypes.models import ContentType
+from creme_core.forms import CremeModelWithUserForm
 
-from creme_core.registry import creme_registry
+from persons.models import Contact, Organisation
 
 
-register = template.Library()
+class ContactQuickForm(CremeModelWithUserForm): #not CremeEntityForm to ignore custom fields
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'user')
 
-@register.inclusion_tag('creme_core/templatetags/search_panel.html', takes_context=True)
-def get_search_panel(context, target_node_id='sub_content'):
-    get_ct = ContentType.objects.get_for_model
-    content_types = [{'id': get_ct(model).id, 'verbose_name': model._meta.verbose_name} for model in creme_registry.iter_entity_models()]
-    content_types.sort(key=lambda k: k['verbose_name'])
 
-    context.update({
-            'content_types':  content_types,
-            'target_node_id': target_node_id, #Ajax version / set your target html node's id
-        })
-
-    return context
+class OrganisationQuickForm(CremeModelWithUserForm):
+    class Meta:
+        model = Organisation
+        fields = ('name', 'user')
