a5ea70a96ff2312118bee86651f387cc39e8feb2
Guillaume Englert <genglert at hybird.org@localhost>
2010-10-01 16:12:56 +0200

    refs #29 Improve 'persons' tests and fix save() method in Contact/Organisation that were not Django compliant.

--- creme/persons/models/contact.py
+++ creme/persons/models/contact.py
@@ -66,9 +66,9 @@ class Contact(CremeEntity):
         #return """<a href="%s">Voir</a> | <a href="%s">Éditer</a> | <a href="%s" onclick="creme.utils.confirmDelete(event, this);">Effacer</a>""" \
                 #% (self.get_absolute_url(), self.get_edit_absolute_url(), self.get_delete_absolute_url())
 
-    def save(self):
+    def save(self, *args, **kwargs):
         self.header_filter_search_field = u"%s %s %s" % (self.civility, self.first_name, self.last_name)
-        super(Contact, self).save()
+        super(Contact, self).save(*args, **kwargs)
 
     def get_employers(self):
         from organisation import Organisation
