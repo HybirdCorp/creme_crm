cac169c36817ee1fc314888f09453f138008f29e
Guillaume Englert <genglert at hybird.org@localhost>
2010-10-30 20:45:54 +0200

    refs #9 Queries optimisation in ComApp block.

--- creme/commercial/blocks.py
+++ creme/commercial/blocks.py
@@ -18,6 +18,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+from collections import defaultdict
+
+from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
 
 from creme_core.gui.block import QuerysetBlock, list4url
@@ -33,22 +36,45 @@ class ApproachesBlock(QuerysetBlock):
     template_name = 'commercial/templatetags/block_approaches.html'
     configurable  = True
 
+    #TODO: factorise with assistants blocks (CremeEntity method ??)
+    @staticmethod
+    def _populate_related_real_entities(comapps, user):
+        entities_ids_by_ct = defaultdict(set)
+
+        for comapp in comapps:
+            entities_ids_by_ct[comapp.entity_content_type_id].add(comapp.entity_id)
+
+        entities_map = {}
+        get_ct = ContentType.objects.get_for_id
+
+        for ct_id, entities_ids in entities_ids_by_ct.iteritems():
+            entities_map.update(get_ct(ct_id).model_class().objects.in_bulk(entities_ids))
+
+        for comapp in comapps:
+            comapp.creme_entity = entities_map[comapp.entity_id]
+
     def detailview_display(self, context):
         pk = context['object'].pk
         return self._render(self.get_block_template_context(context, CommercialApproach.get_approaches(pk),
                                                             update_url='/creme_core/blocks/reload/%s/%s/' % (self.id_, pk),
-                                                            ))
+                                                           ))
 
     def portal_display(self, context, ct_ids):
-        #TODO: CommercialApproach.get_for_ct() ????
-        return self._render(self.get_block_template_context(context,
-                                                            CommercialApproach.objects.filter(entity_content_type__id__in=ct_ids, ok_or_in_futur=False),
-                                                            update_url='/creme_core/blocks/reload/portal/%s/%s/' % (self.id_, list4url(ct_ids)),
-                                                            ))
+        btc = self.get_block_template_context(context,
+                                              CommercialApproach.get_approaches_for_ctypes(ct_ids),
+                                              update_url='/creme_core/blocks/reload/portal/%s/%s/' % (self.id_, list4url(ct_ids)),
+                                             )
+        self._populate_related_real_entities(btc['page'].object_list, context['request'].user)
+
+        return self._render(btc)
 
     def home_display(self, context):
-        return self._render(self.get_block_template_context(context, CommercialApproach.get_approaches(),
-                                                            update_url='/creme_core/blocks/reload/home/%s/' % self.id_,
-                                                            ))
+        btc = self.get_block_template_context(context, CommercialApproach.get_approaches(),
+                                              update_url='/creme_core/blocks/reload/home/%s/' % self.id_,
+                                             )
+        self._populate_related_real_entities(btc['page'].object_list, context['request'].user)
+
+        return self._render(btc)
+
 
 approaches_block = ApproachesBlock()
