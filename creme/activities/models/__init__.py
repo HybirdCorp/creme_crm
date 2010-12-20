5b9c46a4074fb81d41e945633250b0928898825d
MrJmad Aka Jean-Michel <354064+mrjmad@users.noreply.github.com>
2010-12-20 11:50:00 +0100

    Activities, Ajout d'un status, modification colonne des blocks

--- creme/activities/models/__init__.py
+++ creme/activities/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from activity import Activity, ActivityType, Meeting, PhoneCall, PhoneCallType, Task, TaskStatus, Calendar, CalendarActivityLink
+from activity import Activity, ActivityType, Meeting, PhoneCall, PhoneCallType, Task, Status, Calendar, CalendarActivityLink
