749f6fd764d590867bb217aca065d940a912befb
jmad <jmad@localhost>
2010-12-30 17:49:53 +0100

    ajout d'un champ dans les activity pour la gestion du occup? ou pas

--- creme/activities/models/activity.py
+++ creme/activities/models/activity.py
@@ -110,6 +110,7 @@ class Activity(CremeEntity):
     #calendar    = ForeignKey(Calendar, verbose_name=_(u"Calendar"))
     is_all_day  = BooleanField(_(u'All day ?'), blank=True, default=False)
     status      = ForeignKey(Status, verbose_name=_(u'Status'), blank=True, null=True)
+    occuped     = BooleanField(_(u'Occuped ?'), default=False)
 
 
     research_fields = CremeEntity.research_fields + ['title', 'type__name']
