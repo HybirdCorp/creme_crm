366768f4d44c596c94a5240efb8cf6c833346cb8
MrJmad Aka Jean-Michel <354064+mrjmad@users.noreply.github.com>
2010-12-31 11:41:39 +0100

    pouvoir config la TVA par defaut dans les settings

--- creme/billing/constants.py
+++ creme/billing/constants.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from decimal import Decimal, ROUND_UP, ROUND_DOWN, ROUND_HALF_EVEN
+from django.conf import settings
 
 DEFAULT_DECIMAL = Decimal()
-DEFAULT_VAT = Decimal("19.6")
+DEFAULT_VAT = Decimal(getattr(settings,"DEFAULT_VAT","19.6"))
 ROUND_POLICY = ROUND_UP
 CURRENCY = "Euro"
 
