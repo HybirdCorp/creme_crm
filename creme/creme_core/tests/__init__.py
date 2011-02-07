853393da56660bd10924968c142e8e546491b07b
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2011-02-07 12:19:12 +0100

    refs #14 #29 Shadowed unit test in creme core.

--- creme/creme_core/tests/__init__.py
+++ creme/creme_core/tests/__init__.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from unittest import TestLoader, TestSuite
-
-import utils
-import models
-import views
-import list_view_state
-
-
-def suite():
-    loadtests = TestLoader().loadTestsFromModule
-
-    return TestSuite([loadtests(module) for module in (utils, models, views, list_view_state)])
+from utils import *
+from models import *
+from views import *
+from list_view_state import *
