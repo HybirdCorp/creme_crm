ae7960da82a40f2d280056a1fe62ef381fb6318b
Guillaume Englert <genglert at hybird.org@localhost>
2011-02-11 14:19:07 +0100

    refs #1 Improve creme_core.urls.py (use more include() ).

--- creme/creme_core/urls.py
+++ creme/creme_core/urls.py
@@ -2,6 +2,16 @@
 
 from django.conf.urls.defaults import patterns, include
 
+
+entity_patterns = patterns('creme_core.views', #TODO: move delete* to entity.py
+    (r'^delete/(?P<entity_id>\d+)$',     'generic.delete.delete_entity'),
+    (r'^delete_related/(?P<ct_id>\d+)$', 'generic.delete.delete_related_to_entity'),
+    (r'^get_repr/$',                     'entity.get_entity_repr'),
+    (r'^get_repr/(?P<entity_id>\d+)$',   'entity.get_creme_entity_repr'),
+    (r'^render$',                        'entity.render_entity'),
+    (r'^json$',                          'entity.get_creme_entity_as_json'),
+)
+
 relation_patterns = patterns('creme_core.views.relation',
     (r'^add/(?P<subject_id>\d+)$',                                                                         'add_relations'),
     (r'^add/(?P<subject_id>\d+)/(?P<relation_type_id>[\w-]+)$',                                            'add_relations'),
@@ -20,45 +30,50 @@ relation_patterns = patterns('creme_core.views.relation',
     (r'^get_predicates_choices_4_ct$',                                                                     'get_predicates_choices_4_ct'),
 )
 
+property_patterns = patterns('creme_core.views.creme_property',
+    (r'^add_to_entities$',        'add_to_entities'),
+    (r'^get_types$',              'get_property_types_for_ct'),
+    (r'^add/(?P<entity_id>\d+)$', 'add_to_entity'),
+)
+
+blocks_patterns = patterns('creme_core.views.blocks',
+    (r'^relations_block/(?P<entity_id>\d+)/$',                                'reload_relations_block'),
+    (r'^relations_block/(?P<entity_id>\d+)/(?P<relation_type_ids>[\w,-]+)/$', 'reload_relations_block'),
+    (r'^(?P<block_id>[\w-]+)/(?P<entity_id>\d+)/$',                           'reload_detailview'), #TODO: change url to detailview/(?P<block_id>[\w-]+)....
+    (r'^home/(?P<block_id>[\w-]+)/$',                                         'reload_home'),
+    (r'^portal/(?P<block_id>[\w-]+)/(?P<ct_ids>[\d,]+)/$',                    'reload_portal'),
+    (r'^basic/(?P<block_id>[\w-]+)/$',                                        'reload_basic'), #most of blocks in creme_config for example
+)
+
+filter_patterns = patterns('creme_core.views.list_view_filter',
+    (r'^add/(?P<ct_id>\d+)$',                           'add'),
+    (r'^edit/(?P<ct_id>\d+)/(?P<filter_id>\d+)$',       'edit'), #TODO: ct_id useful ????
+    (r'^delete$',                                       'delete'),
+    (r'^field_has_n_get_fk$',                           'field_has_n_get_fk'),
+    (r'^register/(?P<filter_id>\d*)/(?P<ct_id>\d+)$',   'register_in_session'),
+    (r'^get_session_filter_id/(?P<ct_id>\d+)$',         'get_session_filter_id'),
+    (r'^select_entity_popup/(?P<content_type_id>\d+)$', 'get_list_view_popup_from_ct'),
+    (r'^get_4_ct/(?P<content_type_id>\d+)$',            'get_filters_4_ct'),
+)
+
+headerfilter_patterns = patterns('creme_core.views.header_filter',
+    (r'^add/(?P<content_type_id>\d+)$',      'add'),
+    (r'^delete',                             'delete'),
+    (r'^edit/(?P<header_filter_id>[\w-]+)$', 'edit'),
+    (r'^get_4_ct/(?P<content_type_id>\d+)$', 'get_hfs_4_ct'),
+)
+
 creme_core_patterns = patterns('creme_core.views',
-    (r'^relation/', include(relation_patterns)),
-
-    (r'^blocks/reload/relations_block/(?P<entity_id>\d+)/$',                                'blocks.reload_relations_block'),
-    (r'^blocks/reload/relations_block/(?P<entity_id>\d+)/(?P<relation_type_ids>[\w,-]+)/$', 'blocks.reload_relations_block'),
-    (r'^blocks/reload/(?P<block_id>[\w-]+)/(?P<entity_id>\d+)/$',                           'blocks.reload_detailview'), #TODO: change url to blocks/reload/detailview/....
-    (r'^blocks/reload/home/(?P<block_id>[\w-]+)/$',                                         'blocks.reload_home'),
-    (r'^blocks/reload/portal/(?P<block_id>[\w-]+)/(?P<ct_ids>[\d,]+)/$',                    'blocks.reload_portal'),
-    (r'^blocks/reload/basic/(?P<block_id>[\w-]+)/$',                                        'blocks.reload_basic'), #most of blocks in creme_config for example
-
-    (r'^filter/add/(?P<ct_id>\d+)$',                           'list_view_filter.add'),
-    (r'^filter/edit/(?P<ct_id>\d+)/(?P<filter_id>\d+)$',       'list_view_filter.edit'), #TODO: ct_id useful ????
-    (r'^filter/delete$',                                       'list_view_filter.delete'),
-    (r'^filter/field_has_n_get_fk$',                           'list_view_filter.field_has_n_get_fk'),
-    (r'^filter/register/(?P<filter_id>\d*)/(?P<ct_id>\d+)$',   'list_view_filter.register_in_session'),
-    (r'^filter/get_session_filter_id/(?P<ct_id>\d+)$',         'list_view_filter.get_session_filter_id'),
-    (r'^filter/select_entity_popup/(?P<content_type_id>\d+)$', 'list_view_filter.get_list_view_popup_from_ct'),
-    (r'^filter/get_4_ct/(?P<content_type_id>\d+)$',            'list_view_filter.get_filters_4_ct'),
+    (r'^entity/',        include(entity_patterns)),
+    (r'^relation/',      include(relation_patterns)),
+    (r'^property/',      include(property_patterns)),
+    (r'^blocks/reload/', include(blocks_patterns)),
+    (r'^filter/',        include(filter_patterns)),
+    (r'^header_filter/', include(headerfilter_patterns)),
 
     #(r'^edit_js$', 'ajax.edit_js'), #TODO: commented on 5 january 2011 (delete view too)
 
     (r'^clean/$', 'clean.clean'),
-
-    (r'^property/add_to_entities$',        'creme_property.add_to_entities'),
-    (r'^property/get_types$',              'creme_property.get_property_types_for_ct'),
-    (r'^property/add/(?P<entity_id>\d+)$', 'creme_property.add_to_entity'),
-
-    (r'^header_filter/add/(?P<content_type_id>\d+)$',      'header_filter.add'),
-    (r'^header_filter/delete',                             'header_filter.delete'),
-    (r'^header_filter/edit/(?P<header_filter_id>[\w-]+)$', 'header_filter.edit'),
-    (r'^header_filter/get_4_ct/(?P<content_type_id>\d+)$', 'header_filter.get_hfs_4_ct'),
-
-    #Entities
-    (r'^entity/delete/(?P<entity_id>\d+)$',     'generic.delete.delete_entity'),
-    (r'^entity/delete_related/(?P<ct_id>\d+)$', 'generic.delete.delete_related_to_entity'),
-    (r'^entity/get_repr/$',                     'entity.get_entity_repr'),
-    (r'^entity/get_repr/(?P<entity_id>\d+)$',   'entity.get_creme_entity_repr'),
-    (r'^entity/render$',                        'entity.render_entity'),
-    (r'^entity/json$',                          'entity.get_creme_entity_as_json'),
 
     (r'^list_view/import_csv/(?P<ct_id>\d+)$', 'csv_import.csv_import'),
     (r'^list_view/dl_csv/(?P<ct_id>\d+)$',     'csv_export.dl_listview_as_csv'),
