20b6f924205aa72e71cd7762cda0fe1644c383fb
Raphael BECK <rbeck at hybird.org@localhost>
2011-03-08 15:44:19 +0100

    refs #18  *  Credentials and collision are now checked on calendar

--- creme/activities/utils.py
+++ creme/activities/utils.py
@@ -20,6 +20,14 @@
 
 from datetime import timedelta
 
+from django.db.models.query_utils import Q
+from django.utils.translation import ugettext
+
+from creme_core.models.relation import Relation
+
+from activities.constants import REL_SUB_PART_2_ACTIVITY
+
+
 def get_last_day_of_a_month(date):
     rdate = date.replace(day=1)
     try:
@@ -33,6 +41,44 @@ def get_last_day_of_a_month(date):
             except:
                 rdate = rdate + timedelta(days=28)
     return rdate
+
+
+def check_activity_collisions(activity_start, activity_end, participants, exclude_activity_id=None):
+    from activities.models.activity import Activity
+
+    collision_test = ~(Q(end__lte=activity_start) | Q(start__gte=activity_end))
+    collisions     = []
+
+    for participant in participants:
+        # find activities of participant
+        activity_req = Relation.objects.filter(subject_entity=participant.id, type=REL_SUB_PART_2_ACTIVITY)
+
+        # exclude current activity if asked
+        if exclude_activity_id is not None:
+            activity_req = activity_req.exclude(object_entity=exclude_activity_id)
+
+        # get id of activities of participant
+        activity_ids = activity_req.values_list("object_entity__id", flat=True)
+
+        # do collision request
+        #TODO: can be done with less queries ?
+        #  eg:  Activity.objects.filter(relations__object_entity=participant.id, relations__object_entity__type=REL_OBJ_PART_2_ACTIVITY).filter(collision_test)
+        #activity_collisions = Activity.objects.exclude(busy=False).filter(pk__in=activity_ids).filter(collision_test)[:1]
+        activity_collisions = Activity.objects.filter(pk__in=activity_ids).filter(collision_test)[:1]
+
+        if activity_collisions:
+            collision = activity_collisions[0]
+            collision_start = max(activity_start.time(), collision.start.time())
+            collision_end   = min(activity_end.time(),   collision.end.time())
+
+            collisions.append(ugettext(u"%(participant)s already participates to the activity «%(activity)s» between %(start)s and %(end)s.") % {
+                        'participant': participant,
+                        'activity':    collision,
+                        'start':       collision_start,
+                        'end':         collision_end,
+                    })
+
+    return collisions
 
 def get_ical_date(dateTime):
     return "%(year)s%(month)02d%(day)02dT%(hour)02d%(minute)02d%(second)02dZ" % {
