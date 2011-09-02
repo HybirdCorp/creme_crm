c74e631e662254f207cfcbe2b651f555dc81f426
Guillaume Englert <genglert at hybird.org@localhost>
2011-09-02 17:00:08 +0200

    refs #29 Split creme_config unit tests.

---
+++ creme/creme_config/tests/creme_property_type.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from django.contrib.contenttypes.models import ContentType
+
+    from creme_core.models import CremeEntity, CremePropertyType
+    from creme_core.tests.base import CremeTestCase
+
+    from persons.models import Contact, Organisation #need CremeEntity
+except Exception, e:
+    print 'Error:', e
+
+
+__all__ = ('PropertyTypeTestCase',)
+
+
+class PropertyTypeTestCase(CremeTestCase):
+    def setUp(self):
+        self.populate('creme_core', 'creme_config')
+        self.login()
+
+    def test_portal(self):
+        self.assertEqual(200, self.client.get('/creme_config/property_type/portal/').status_code)
+
+    def _find_property_type(self, prop_types, text):
+        for prop_type in prop_types:
+            if prop_type.text == text:
+                return prop_type
+
+        self.fail('No property <%s>' % text)
+
+    def test_create01(self):
+        url = '/creme_config/property_type/add/'
+        self.assertEqual(200, self.client.get(url).status_code)
+
+        self.assertEqual(1, CremePropertyType.objects.count())#The one from creme_core populate
+
+        text = 'is beautiful'
+        response = self.client.post(url, data={'text': text})
+        self.assertNoFormError(response)
+        self.assertEqual(200, response.status_code)
+
+        prop_types = CremePropertyType.objects.all()
+        self.assertEqual(2, len(prop_types))
+
+        prop_type = self._find_property_type(prop_types, text)
+        self.assertEqual(0, prop_type.subject_ctypes.count())
+
+    def test_create02(self):
+        get_ct = ContentType.objects.get_for_model
+        ct_ids = [get_ct(Contact).id, get_ct(Organisation).id]
+        text   = 'is beautiful'
+        response = self.client.post('/creme_config/property_type/add/',
+                                    data={
+                                            'text':           text,
+                                            'subject_ctypes': ct_ids,
+                                         }
+                                   )
+        self.assertNoFormError(response)
+        self.assertEqual(200, response.status_code)
+
+        prop_type = self._find_property_type(CremePropertyType.objects.all(), text)
+        ctypes = prop_type.subject_ctypes.all()
+        self.assertEqual(2,           len(ctypes))
+        self.assertEqual(set(ct_ids), set(ct.id for ct in ctypes))
+
+    def test_edit01(self):
+        get_ct = ContentType.objects.get_for_model
+        pt = CremePropertyType.create('test-foobar', 'is beautiful', [get_ct(Contact)], is_custom=False)
+
+        self.assertEqual(404, self.client.get('/creme_config/property_type/edit/%s' % pt.id).status_code)
+
+    def test_edit02(self):
+        get_ct = ContentType.objects.get_for_model
+        pt = CremePropertyType.create('test-foobar', 'is beautiful', [get_ct(Contact)], is_custom=True)
+        uri = '/creme_config/property_type/edit/%s' % pt.id
+        self.assertEqual(200, self.client.get(uri).status_code)
+
+        ct_orga = get_ct(Organisation)
+        text   = 'is very beautiful'
+        response = self.client.post(uri,
+                                    data={
+                                            'text':           text,
+                                            'subject_ctypes': [ct_orga.id],
+                                         }
+                                   )
+        self.assertNoFormError(response)
+        self.assertEqual(200, response.status_code)
+
+        prop_type = CremePropertyType.objects.get(pk=pt.id)
+        self.assertEqual(text,         prop_type.text)
+        self.assertEqual([ct_orga.id], [ct.id for ct in prop_type.subject_ctypes.all()])
+
+    def test_delete01(self):
+        pt = CremePropertyType.create('test-foobar', 'is beautiful', [], is_custom=False)
+        self.assertEqual(404, self.client.post('/creme_config/property_type/delete', data={'id': pt.id}).status_code)
+
+    def test_delete02(self):
+        pt = CremePropertyType.create('test-foobar', 'is beautiful', [], is_custom=True)
+        self.assertEqual(200, self.client.post('/creme_config/property_type/delete', data={'id': pt.id}).status_code)
+        self.assertEqual(0,   CremePropertyType.objects.filter(pk=pt.id).count())
---
+++ creme/creme_config/tests/history.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from django.utils.translation import ugettext as _
+
+    from creme_core.models import RelationType, HistoryConfigItem
+    from creme_core.tests.base import CremeTestCase
+except Exception, e:
+    print 'Error:', e
+
+
+__all__ = ('HistoryConfigTestCase',)
+
+
+class HistoryConfigTestCase(CremeTestCase):
+    def setUp(self):
+        self.populate('creme_core', 'creme_config')
+
+    def test_portal(self):
+        self.login()
+        self.populate('creme_core')
+        self.assertEqual(200, self.client.get('/creme_config/history/portal/').status_code)
+
+    def test_add01(self):
+        self.login()
+        self.assertFalse(HistoryConfigItem.objects.count())
+
+        rtype01, srtype01 = RelationType.create(('test-subject_foo', 'fooes'), ('test-object_foo', 'fooed'))
+        rtype02, srtype02 = RelationType.create(('test-subject_bar', 'bars'),  ('test-object_bar', 'bared'))
+
+        url = '/creme_config/history/add/'
+        self.assertEqual(200, self.client.get(url).status_code)
+
+        rtype_ids = [rtype01.id, rtype02.id]
+        response = self.client.post(url, data={'relation_types': rtype_ids})
+        self.assertNoFormError(response)
+        self.assertEqual(200, response.status_code)
+
+        hc_items = HistoryConfigItem.objects.all()
+        self.assertEqual(2, len(hc_items))
+        self.assertEqual(set(rtype_ids), set(hc_item.relation_type.id for hc_item in hc_items))
+
+    def test_add02(self): #no doublons
+        self.login()
+
+        rtype01, srtype01 = RelationType.create(('test-subject_foo', 'fooes'), ('test-object_foo', 'fooed'))
+        rtype02, srtype02 = RelationType.create(('test-subject_bar', 'bars'),  ('test-object_bar', 'bared'))
+
+        HistoryConfigItem.objects.create(relation_type=rtype01)
+
+        rtype_ids = [rtype01.id, rtype02.id]
+        response = self.client.post('/creme_config/history/add/', data={'relation_types': rtype_ids})
+        self.assertFormError(response, 'form', field='relation_types',
+                             errors=_(u'Select a valid choice. %s is not one of the available choices.') % rtype01.id
+                            )
+
+    def test_delete(self):
+        self.login()
+
+        rtype, srtype = RelationType.create(('test-subject_foo', 'fooes'), ('test-object_foo', 'fooed'))
+        hci = HistoryConfigItem.objects.create(relation_type=rtype)
+
+        response = self.client.post('/creme_config/history/delete', data={'id': hci.id})
+        self.assertEqual(200, response.status_code)
+        self.assertFalse(HistoryConfigItem.objects.filter(pk=hci.id).count())
