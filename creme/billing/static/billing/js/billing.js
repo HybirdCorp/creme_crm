4b66200c4367899fd99f01d731cbfabb1b958001
Jonathan CARUANA <jcaruana at hybird.org@localhost>
2011-12-19 08:17:53 +0100

    refs #19 display bug fix error lines and add trad

--- creme/billing/static/billing/js/billing.js
+++ creme/billing/static/billing/js/billing.js
@@ -126,7 +126,13 @@ creme.billing.printLinesErrors = function(target, table, reload_url) {
 
     target.addClass('line-error');
     // TODO gettext
-    target.html("Des erreurs ont �t� trouv�es sur vos lignes. Cliquez => <a onclick='creme.utils.loadBlock(\"" + reload_url + "\");'><b>ici</b></a> <= pour retrouver le dernier �tat coh�rent. La correction d'une erreur sur une ligne restabilisera toutes les autres lignes.");
+    var message_one = gettext("Some errors were found on your lines. Click => ");
+    var message_two = gettext("here");
+    var message_three = "<a onclick='creme.utils.loadBlock(\"" + reload_url + "\");'><b>" + message_two + "</b></a> ";
+    var message_four = gettext("<= to restore the last coherent state. An error correction will restabilize all other ones.");
+    target.html(message_one + message_three + message_four);
+//    target.html("Des erreurs ont �t� trouv�es sur vos lignes. Cliquez => <a onclick='creme.utils.loadBlock(\"" + reload_url + "\");'><b>ici</b></a> <=
+// pour retrouver le dernier �tat coh�rent. La correction d'une erreur sur une ligne restabilisera toutes les autres lignes.");
 
     var ul = $('<ul/>');
 
@@ -146,13 +152,15 @@ creme.billing.validateDiscount = function(line) {
 
     // TODO gettext
     if (creme.billing.checkPercent($('input[name=discount]', line).val()) !== null && discount_unit == 1) {
-        return gettext("Pourcentage invalide.");
+        return gettext("Invalid percentage.");
     }
     if (discount_total == 1 && discount_unit == 2 && discount_value > unit_price * quantity) {
-        return gettext("Le montant de votre remise globale d�passe celui de la ligne sur laquelle vous essayez de l'appliquer.");
+        return gettext("Your discount amount is superior than the amount of the line where you try to apply it.");
+//        return gettext("Le montant de votre remise globale d�passe celui de la ligne sur laquelle vous essayez de l'appliquer.");
     }
     if (discount_total == 2 && discount_unit == 2 && discount_value > unit_price) {
-        return gettext("Le montant de votre remise unitaire d�passe celui du produit/service de la ligne sur laquelle vous essayez de l'appliquer.");
+        return gettext("Your unit discount is superior than the unit price of the product/service of the line where you try to apply it.");
+//        return gettext("Le montant de votre remise unitaire d�passe celui du produit/service de la ligne sur laquelle vous essayez de l'appliquer.");
     }
 
     return null;
@@ -165,8 +173,8 @@ creme.billing.validateDiscountForm = function(line, reload_url) {
     var form_error = creme.billing.validateDiscount(line);
 
     if (form_error != null) {
-        var line_error_message_first = gettext("Sur la ligne ");
-        var error_message = line_error_message_first + "<" + $('input#line_number', line).val() + "> : " + form_error;
+        var line_error_message_first = gettext("On the line");
+        var error_message = line_error_message_first + " <" + $('input#line_number', line).val() + "> : " + form_error;
         $('td#discount_td', line).addClass('discount-functional-error')
                                  .attr('discount-functional-errormessage', error_message);
 
