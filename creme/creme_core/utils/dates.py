d71cb6b917df1e8309c0c5c56c3150a4e4647544
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2012-01-02 16:12:58 +0100

    refs #1 #29 Improve some tests and README

--- creme/creme_core/utils/dates.py
+++ creme/creme_core/utils/dates.py
@@ -17,10 +17,11 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
-import pytz
 
 import time
 from datetime import datetime
+
+import pytz
 
 from django.conf import settings
 from django.utils import formats
