718c9121fca30061accd35dd23eda20b15232cd5
Guillaume Englert <genglert at hybird.org@localhost>
2012-03-19 14:06:01 +0100

    refs #37 Documents prevent the deletion on their folder.

--- creme/documents/tests.py
+++ creme/documents/tests.py
@@ -16,11 +16,23 @@ except Exception as e:
     print 'Error in <%s>: %s' % (__name__, e)
 
 
-class DocumentTestCase(CremeTestCase):
+class _DocumentsTestCase(CremeTestCase):
     @classmethod
     def setUpClass(cls):
         cls.populate('creme_core', 'creme_config', 'documents')
 
+    def _build_filedata(self, content_str):
+        tmpfile = NamedTemporaryFile()
+        tmpfile.write(content_str)
+        tmpfile.flush()
+
+        filedata = tmpfile.file
+        filedata.seek(0)
+
+        return tmpfile
+
+
+class DocumentTestCase(_DocumentsTestCase):
     def test_populate(self):
         self.assertTrue(RelationType.objects.filter(pk=REL_SUB_RELATED_2_DOC).exists())
 
@@ -34,17 +46,7 @@ class DocumentTestCase(CremeTestCase):
         self.login()
         self.assertEqual(200, self.client.get('/documents/').status_code)
 
-    def _build_filedata(self, content_str):
-        tmpfile = NamedTemporaryFile()
-        tmpfile.write(content_str)
-        tmpfile.flush()
-
-        filedata = tmpfile.file
-        filedata.seek(0)
-
-        return tmpfile
-
-    def test_add_document(self):
+    def test_createview(self):
         self.login()
 
         self.assertFalse(Document.objects.exists())
@@ -54,7 +56,7 @@ class DocumentTestCase(CremeTestCase):
 
         title       = 'Test doc'
         description = 'Test description'
-        content     = """Yes I am the content"""
+        content     = 'Yes I am the content (DocumentTestCase.test_createview)'
         filedata    = self._build_filedata(content)
         folder      = Folder.objects.all()[0]
         response = self.client.post(url, follow=True,
@@ -84,12 +86,12 @@ class DocumentTestCase(CremeTestCase):
 
         filedata.delete(filedata) #clean
 
-    def test_edit_document(self):
+    def test_editview(self):
         self.login()
 
         title       = 'Test doc'
         description = 'Test description'
-        content     = """Yes I am the content"""
+        content     = 'Yes I am the content (DocumentTestCase.test_editview)'
         filedata    = self._build_filedata(content)
         folder      = Folder.objects.all()[0]
         self.client.post('/documents/document/add',
@@ -144,7 +146,7 @@ class DocumentTestCase(CremeTestCase):
         self.assertEqual(200, self.client.get(url).status_code)
 
         title    = 'Related doc'
-        filedata = self._build_filedata("Yes I am the content")
+        filedata = self._build_filedata('Yes I am the content (DocumentTestCase.test_add_related_document)')
         response = self.client.post(url, follow=True,
                                     data={'user':         self.user.pk,
                                           'title':        title,
@@ -162,9 +164,59 @@ class DocumentTestCase(CremeTestCase):
 
         doc.filedata.delete(doc.filedata) #clean
 
-    def test_add_folder(self):
+    def test_listview(self):
         self.login()
 
+        def create_doc(title):
+            filedata = self._build_filedata('%s : Yes I am the content (DocumentTestCase.test_listview)' % title)
+            self.client.post('/documents/document/add',
+                            data={'user':         self.user.pk,
+                                'title':        title,
+                                'description':  'Test description',
+                                'filedata':     filedata.file,
+                                'folder':       Folder.objects.values_list('id', flat=True),
+                                }
+                            )
+
+            return self.get_object_or_fail(Document, title=title)
+
+        doc1 = create_doc('Test doc #1')
+        doc2 = create_doc('Test doc #2')
+
+        response = self.client.get('/documents/documents')
+        self.assertEqual(response.status_code, 200)
+
+        with self.assertNoException():
+            docs_page = response.context['entities']
+
+        self.assertIn(doc1, docs_page.object_list)
+        self.assertIn(doc2, docs_page.object_list)
+
+        #clean
+        doc1.filedata.delete(doc1.filedata)
+        doc2.filedata.delete(doc2.filedata)
+
+    def test_delete_category(self): #set to null
+        self.login()
+
+        cat = FolderCategory.objects.create(name='Manga')
+        folder = Folder.objects.create(user=self.user, title='One piece', category=cat)
+
+        response = self.client.post('/creme_config/documents/category/delete', data={'id': cat.pk})
+        self.assertEqual(200, response.status_code)
+        self.assertFalse(FolderCategory.objects.filter(pk=cat.pk).exists())
+
+        folder = self.get_object_or_fail(Folder, pk=folder.pk)
+        self.assertIsNone(folder.category)
+
+    #TODO complete
+
+
+class FolderTestCase(_DocumentsTestCase):
+    def setUp(self):
+        self.login()
+
+    def test_createview(self):
         url = '/documents/folder/add'
         self.assertEqual(200, self.client.get(url).status_code)
 
@@ -192,11 +244,9 @@ class DocumentTestCase(CremeTestCase):
         self.assertEqual(parent,      folder.parent_folder)
         self.assertEqual(category,    folder.category)
 
-    def test_edit_folder(self):
-        self.login()
-
+    def test_editview(self):
         title = u'Test folder'
-        description='Test description'
+        description = 'Test description'
         category = FolderCategory.objects.all()[0]
         folder = Folder.objects.create(title=title,
                                        description=description,
@@ -228,27 +278,30 @@ class DocumentTestCase(CremeTestCase):
         self.assertEqual(parent,      folder.parent_folder)
         self.assertEqual(category,    folder.category)
 
-    def test_delete_category(self): #set to null
-        self.login()
+    def test_listview(self):
+        user = self.user
+        category = FolderCategory.objects.all()[0]
 
-        cat = FolderCategory.objects.create(name='Manga')
-        folder = Folder.objects.create(user=self.user, title='One piece', category=cat)
+        create_folder = Folder.objects.create
+        folder1 = create_folder(user=user, title='PDF',
+                                description='Contains PDF files',
+                                parent_folder=None, category=category
+                               )
+        folder2 = create_folder(user=user, title='SVG',
+                                description='Contains SVG files',
+                                parent_folder=None, category=category
+                               )
 
-        response = self.client.post('/creme_config/documents/category/delete', data={'id': cat.pk})
-        self.assertEqual(200, response.status_code)
-        self.assertFalse(FolderCategory.objects.filter(pk=cat.pk).exists())
+        response = self.client.get('/documents/folders')
+        self.assertEqual(response.status_code, 200)
 
-        folder = self.get_object_or_fail(Folder, pk=folder.pk)
-        self.assertIsNone(folder.category)
+        with self.assertNoException():
+            folders = response.context['entities'].object_list
 
-    #TODO complete
-
-class FolderTestCase(CremeTestCase):
-    def setUp(self):
-        self.populate('creme_core', 'documents')
+        self.assertIn(folder1, folders)
+        self.assertIn(folder2, folders)
 
     def test_folder_clone01(self):
-        self.login()
         title = 'folder'
         folder = Folder.objects.create(user=self.user, title=title, description="d")
 
@@ -260,3 +313,44 @@ class FolderTestCase(CremeTestCase):
             self.assertNotEqual(stack[-1].title, clone.title)
             stack_append(clone)
 
+    def test_deleteview01(self): #no doc inside
+        folder = Folder.objects.create(user=self.user, title='PDF',
+                                       description='Contains PDF files',
+                                       parent_folder=None,
+                                       category=FolderCategory.objects.all()[0]
+                                      )
+
+        response = self.client.post('/creme_core/entity/delete/%s' % folder.pk, follow=True)
+        self.assertEqual(200, response.status_code)
+        self.assertFalse(Folder.objects.filter(pk=folder.pk).exists())
+        self.assertEqual(1, len(response.redirect_chain))
+        self.assertTrue(response.redirect_chain[0][0].endswith('/documents/folders'))
+
+    def test_deleteview02(self): #a doc inside protect from deletion
+        folder = Folder.objects.create(user=self.user, title='PDF',
+                                       description='Contains PDF files',
+                                       parent_folder=None,
+                                       category=FolderCategory.objects.all()[0]
+                                      )
+
+        title = 'Boring title'
+        filedata = self._build_filedata('Yes I am the content (FolderTestCase.test_deleteview02)')
+        response = self.client.post('/documents/document/add', follow=True,
+                                    data={'user':         self.user.pk,
+                                          'title':        title,
+                                          'description':  'Boring description too',
+                                          'filedata':     filedata.file,
+                                          'folder':       folder.id,
+                                         }
+                                   )
+        self.assertEqual(200, response.status_code)
+
+        doc = self.get_object_or_fail(Document, title=title)
+        self.assertEqual(folder, doc.folder)
+
+        self.assertEqual(200, #404
+                         self.client.post('/creme_core/entity/delete/%s' % folder.pk).status_code
+                        )
+        self.get_object_or_fail(Folder, pk=folder.pk)
+
+        doc.filedata.delete(doc.filedata) #clean
