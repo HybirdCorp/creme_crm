8863a9ba4ee7bc7b4c4e8a3a590214b64f991013
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2012-03-19 14:06:01 +0100

    refs #37 Documents prevent the deletion on their folder.

--- creme/documents/views/document.py
+++ creme/documents/views/document.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2010  Hybird
+#    Copyright (C) 2009-2012  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -20,35 +20,34 @@
 
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
 from creme_core.models import CremeEntity
 from creme_core.views.generic import add_entity, edit_entity, view_entity, list_view, add_model_with_popup
 
-from documents.models import Document
+from documents.models import Document, Folder
 from documents.forms.document import DocumentCreateForm, DocumentCreateViewForm, DocumentEditForm
+
 
 @login_required
 @permission_required('documents')
 @permission_required('documents.add_document')
 def add(request):
-    return add_entity(request, DocumentCreateForm)
+    folders = Folder.objects.all()[:1]
+
+    return add_entity(request, DocumentCreateForm,
+                      extra_initial={'folder': folders[0]} if folders else None
+                     )
 
 @login_required
 @permission_required('documents')
 @permission_required('documents.add_document')
 def add_related(request, entity_id):
-#    GET = request.GET
-#    entity = get_object_or_404(CremeEntity, pk=get_from_GET_or_404(GET, 'entity_id'))
-#
-#    entity.can_link_or_die(request.user)
-
-#    return add_entity(request, DocumentCreateViewForm, GET.get('callback_url'),
-#                      extra_initial={'entity': entity}
-#                     )
-
     entity = get_object_or_404(CremeEntity, pk=entity_id);
-    return add_model_with_popup(request, DocumentCreateViewForm, _(u'New document for <%s>') % entity, initial={'entity':entity})
+    return add_model_with_popup(request, DocumentCreateViewForm,
+                                _(u'New document for <%s>') % entity,
+                                initial={'entity': entity}
+                               )
 
 @login_required
 @permission_required('documents')
