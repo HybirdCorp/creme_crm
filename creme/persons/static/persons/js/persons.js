9c4a0ca40d7d1262625355c79257b164d3834ed1
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2012-03-22 12:18:29 +0100

    refs #70 Mergeviwe: fix 2 problems with checkboxes. - The buttons did not copy values (bug/oddness in jquery val() method). - In FireFox the width was not correct (-> use a padding for checboxes).

--- creme/persons/static/persons/js/persons.js
+++ creme/persons/static/persons/js/persons.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
     Creme is a free/open-source Customer Relationship Management software
-    Copyright (C) 2009-2010  Hybird
+    Copyright (C) 2009-2012  Hybird
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -28,49 +28,15 @@ creme.persons.copyTo = function (from_id, to_id) {
     var $to = $('#' + to_id);
     if($from.size() > 0 && $to.size() > 0) {
         var $to_fill = $to.find('input, textarea, select');
-        $from.find('input, textarea, select').each(function(ind){
+
+        //TODO: use ':input' selector ??
+        $from.find('input, textarea, select').each(function(ind) {
             $($to_fill[ind]).val($(this).val());
         });
     }
 }
 
-// Commented on 7 oct 2010
-// creme.persons.retrieveAddress = function(from_node, to_node) {
-//     var from_node_val = from_node.val();
-//     if(from_node_val != "" && !isNaN(parseInt(from_node_val))) {
-//         $.ajax({
-//             url: "/persons/address/from_organisation",
-//             type: "POST",
-//             data: {'entity_id':parseInt(from_node_val), 'ct_id': to_node.ct_id, 'verbose_field':to_node.verbose_field},
-//             dataType: "json",
-//             success: function(data) {
-//                 //$('#'+to_node.id).val(datas[to_node.field]);
-//                 var $select = $('#'+to_node.id);
-//                 $select.empty();
-//                 $select.parent().find('div').remove();
-// //                var data = resp.data;
-//                 for(var i in data) {
-//                     var current = data[i];
-//                     $select.append(
-//                         $('<option></option>')
-//                         .val(current.pk)
-//                         .text((current.fields[to_node.verbose_field])?current.fields[to_node.verbose_field]:'Adresse sans nom')
-//                     );
-//                 }
-//                 if(data.length == 0)
-//                     $select.append(
-//                         $('<option></option>')
-//                         .val(0)
-//                         .text("Aucune adresse existante ajoutez en une.")
-//                     );
-// //                $select.val(resp.current);
-//                 $select.val(to_node.current);
-//                 $select.change();
-//             }
-//         });
-//     }
-// }
-
+//TODO: rename postBecome
 creme.persons.post_become = function(atag, id_value) {
     $form = $('form', $(atag));
     $input = $('#id_become' , $form);
