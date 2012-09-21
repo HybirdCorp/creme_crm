0901a0bb4e6497271a43e7c208c2ac606de713df
Guillaume Englert <genglert at hybird.org@localhost>
2012-09-21 12:18:07 +0200

    refs #70 Persons merge form improved.

--- creme/persons/forms/merge.py
+++ creme/persons/forms/merge.py
@@ -24,109 +24,94 @@ from django.utils.translation import ugettext as _
 
 from creme_core.forms.merge import MergeEntitiesBaseForm, mergefield_factory
 
-from persons.models import Contact, Address
+from persons.models import Address #Contact
 
 
-_FIELD_NAMES = Address._INFO_FIELD_NAMES
+_FIELD_NAMES = Address._INFO_FIELD_NAMES #TODO: use introspection to get editable fields ??
+_BILL_PREFIX = 'billaddr_'
+_SHIP_PREFIX = 'shipaddr_'
 
+
+class _PersonMergeForm(MergeEntitiesBaseForm):
+    def __init__(self, entity1, entity2, *args, **kwargs):
+        if getattr(entity1, 'is_user', None) or getattr(entity2, 'is_user', None):#TODO: create a ContactMergeForm ?
+            raise self.CanNotMergeError(_('Can not merge a Contact that represents a user.'))
+
+        super(_PersonMergeForm, self).__init__(entity1, entity2, *args, **kwargs)
+
+    def _build_initial_address_dict(self, address, initial, prefix):
+        getter = (lambda fname: '') if address is None else \
+                    lambda fname: getattr(address, fname)
+
+        for fname in _FIELD_NAMES:
+            initial[prefix + fname] = getter(fname)
+
+    def _build_initial_dict(self, entity):
+        initial = super(_PersonMergeForm, self)._build_initial_dict(entity)
+
+        build = self._build_initial_address_dict
+        build(entity.billing_address,  initial, _BILL_PREFIX)
+        build(entity.shipping_address, initial, _SHIP_PREFIX)
+
+        return initial
+
+    def _save_address(self, entity1, entity2, attr_name, cleaned_data, prefix):
+        address = getattr(entity1, attr_name)
+        was_none = False
+
+        if address is None:
+            address = getattr(entity2, attr_name) or Address()
+            address.owner = entity1
+            was_none = True
+
+        for fname in _FIELD_NAMES:
+            setattr(address, fname, cleaned_data[prefix + fname])
+
+        if address:
+            address.save()
+            setattr(entity1, attr_name, address)
+            return was_none, ()
+
+        if not was_none:
+            setattr(entity1, attr_name, None)
+            return True, [address] if address.pk else ()
+
+        return True, ()
+
+    def _post_entity1_update(self, entity1, entity2, cleaned_data):
+        save_address = self._save_address
+
+        must_save1, to_del1 = save_address(entity1, entity2, 'billing_address',  cleaned_data, _BILL_PREFIX)
+        must_save2, to_del2 = save_address(entity1, entity2, 'shipping_address', cleaned_data, _SHIP_PREFIX)
+
+        if must_save1 or must_save2:
+            entity1.save()
+
+        for address in chain(to_del1, to_del2):
+            address.delete()
+
+#TODO: can we build the form once instead of build it each time ??
 #TODO: factorise with csv_import.py ?
-#TODO: code can be simplified (see BaseForm ??)
 def get_merge_form_builder():
     get_field_by_name = Address._meta.get_field_by_name
+    attrs = {}
+    billing_address_fnames = []
+    shipping_address_fnames = []
 
-    f_name    =  get_field_by_name('name')[0]
-    f_address = get_field_by_name('address')[0]
-    f_po_box  = get_field_by_name('po_box')[0]
-    f_city    = get_field_by_name('city')[0]
-    f_state   = get_field_by_name('state')[0]
-    f_zipcode = get_field_by_name('zipcode')[0]
-    f_country = get_field_by_name('country')[0]
-    f_dpt     = get_field_by_name('department')[0]
+    for field_name in _FIELD_NAMES:
+        field = get_field_by_name(field_name)[0]
 
+        form_fieldname = _BILL_PREFIX + field_name
+        attrs[form_fieldname] = mergefield_factory(field)
+        billing_address_fnames.append(form_fieldname)
 
-    class PersonMergeImportForm(MergeEntitiesBaseForm):
-        billaddr_name       = mergefield_factory(f_name)
-        billaddr_address    = mergefield_factory(f_address)
-        billaddr_po_box     = mergefield_factory(f_po_box)
-        billaddr_city       = mergefield_factory(f_city)
-        billaddr_state      = mergefield_factory(f_state)
-        billaddr_zipcode    = mergefield_factory(f_zipcode)
-        billaddr_country    = mergefield_factory(f_country)
-        billaddr_department = mergefield_factory(f_dpt)
+        form_fieldname = _SHIP_PREFIX + field_name
+        attrs[form_fieldname] = mergefield_factory(field)
+        shipping_address_fnames.append(form_fieldname)
 
-        shipaddr_name       = mergefield_factory(f_name)
-        shipaddr_address    = mergefield_factory(f_address)
-        shipaddr_po_box     = mergefield_factory(f_po_box)
-        shipaddr_city       = mergefield_factory(f_city)
-        shipaddr_state      = mergefield_factory(f_state)
-        shipaddr_zipcode    = mergefield_factory(f_zipcode)
-        shipaddr_country    = mergefield_factory(f_country)
-        shipaddr_department = mergefield_factory(f_dpt)
+    attrs['blocks'] = MergeEntitiesBaseForm.blocks.new(
+                            ('billing_address',  _(u'Billing address'),  billing_address_fnames),
+                            ('shipping_address', _(u'Shipping address'), shipping_address_fnames),
+                        )
 
-        #class Meta:
-            #exclude = ('language',)
-
-        blocks = MergeEntitiesBaseForm.blocks.new(
-                    ('billing_address',  _(u'Billing address'),  ['billaddr_' + fn for fn in _FIELD_NAMES]),
-                    ('shipping_address', _(u'Shipping address'), ['shipaddr_' + fn for fn in _FIELD_NAMES]),
-                  )
-
-        def __init__(self, entity1, entity2, *args, **kwargs):
-            if getattr(entity1, 'is_user', None) or getattr(entity2, 'is_user', None):#TODO: create a ContactMergeForm ?
-                raise self.CanNotMergeError(_('Can not merge a Contact that represents a user.'))
-
-            super(PersonMergeImportForm, self).__init__(entity1, entity2, *args, **kwargs)
-
-        def _build_initial_address_dict(self, address, initial, prefix):
-            getter = (lambda fname: '') if address is None else \
-                     lambda fname: getattr(address, fname)
-
-            for fname in _FIELD_NAMES:
-                initial[prefix + fname] = getter(fname)
-
-        def _build_initial_dict(self, entity):
-            initial = super(PersonMergeImportForm, self)._build_initial_dict(entity)
-
-            build = self._build_initial_address_dict
-            build(entity.billing_address,  initial, 'billaddr_')
-            build(entity.shipping_address, initial, 'shipaddr_')
-
-            return initial
-
-        def _save_address(self, entity1, entity2, attr_name, cleaned_data, prefix):
-            address = getattr(entity1, attr_name)
-            was_none = False
-
-            if address is None:
-                address = getattr(entity2, attr_name) or Address()
-                address.owner = entity1
-                was_none = True
-
-            for fname in _FIELD_NAMES:
-                setattr(address, fname, cleaned_data[prefix + fname])
-
-            if address:
-                address.save()
-                setattr(entity1, attr_name, address)
-                return was_none, ()
-
-            if not was_none:
-                setattr(entity1, attr_name, None)
-                return True, [address] if address.pk else ()
-
-            return True, ()
-
-        def _post_entity1_update(self, entity1, entity2, cleaned_data):
-            save_address = self._save_address
-
-            must_save1, to_del1 = save_address(entity1, entity2, 'billing_address',  cleaned_data, 'billaddr_')
-            must_save2, to_del2 = save_address(entity1, entity2, 'shipping_address', cleaned_data, 'shipaddr_')
-
-            if must_save1 or must_save2:
-                entity1.save()
-
-            for address in chain(to_del1, to_del2):
-                address.delete()
-
-
-    return PersonMergeImportForm
+    return type('PersonMergeForm', (_PersonMergeForm,), attrs)
