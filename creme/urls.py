52569c9f1419ed54ffe1fee3800071450e581080
Hugo Smett <hsmett@users.noreply.github.com>
2013-01-28 10:32:00 +0100

    refs #1 Root 'urls.py' generates now automatically urls for Creme apps.

--- creme/urls.py
+++ creme/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import url, patterns, include
 from django.conf import settings
 from django.contrib import admin
 
@@ -15,35 +15,14 @@ handler500 = 'creme_core.views.exceptions.server_error'
 urlpatterns = patterns('',
     url(r'^creme_login/$',  'django.contrib.auth.views.login', {'template_name': 'authent/creme_login.html'} , name="creme_login"),
     url(r'^creme_logout/$', 'django.contrib.auth.views.logout_then_login' , name="creme_logout"),
-    (r'^', include('creme_core.urls')),
 
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     #NB: in production, configure your web server to statically serve the files in the 'media/static/' dir (and so comment the following line)
     (r'^static_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.GENERATED_MEDIA_DIR}),
 
-    (r'^creme_config/',   include('creme_config.urls')),
-    (r'^media_managers/', include('media_managers.urls')),
-    (r'^documents/',      include('documents.urls')),
-    (r'^assistants/',     include('assistants.urls')),
-    (r'^activities/',     include('activities.urls')),
-    (r'^persons/',        include('persons.urls')),
-
-    (r'^graphs/',         include('graphs.urls')),
-    (r'^reports/',        include('reports.urls')),
-    (r'^products/',       include('products.urls')),
-    (r'^recurrents/',     include('recurrents.urls')),
-    (r'^billing/',        include('billing.urls')),
-    (r'^opportunities/',  include('opportunities.urls')),
-    (r'^commercial/',     include('commercial.urls')),
-    (r'^events/',         include('events.urls')),
-    (r'^crudity/',        include('crudity.urls')),
-    (r'^emails/',         include('emails.urls')),
-    (r'^sms/',            include('sms.urls')),
-    (r'^projects/',       include('projects.urls')),
-    (r'^tickets/',        include('tickets.urls')),
-    (r'^activesync/',     include('activesync.urls')),
-    (r'^cti/',            include('cti.urls')),
-    (r'^vcfs/',           include('vcfs.urls')),
-
     (r'^admin/(.*)', include(admin.site.urls)),
 )
+
+for app_name in settings.INSTALLED_CREME_APPS:
+    regex_url = r'^' if app_name == 'creme_core' else r'^%s/' % app_name
+    urlpatterns += patterns('', (regex_url, include('%s.urls' % app_name)))
