b1ad9598e5bb59e3efc1bad6bc3cff50bbf22d74
Guillaume Englert <genglert at hybird.org@localhost>
2013-02-05 20:13:50 +0100

    refs #29 Documents: unit tests splitted & improved (uploaded files cleaned even if there is a failure).

--- creme/documents/tests/document.py
+++ creme/documents/tests/document.py
@@ -1,69 +1,25 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 
 try:
-    from os import remove as delete_file
-    from os.path import basename
-    from tempfile import NamedTemporaryFile
-
     from django.core.serializers.json import DjangoJSONEncoder as JSONEncoder
     from django.conf import settings
     from django.contrib.contenttypes.models import ContentType
 
     from creme_core.models import CremeEntity, RelationType, HeaderFilter, SetCredentials
     from creme_core.auth.entity_credentials import EntityCredentials
-    from creme_core.tests.base import CremeTestCase
+    #from creme_core.tests.base import CremeTestCase
 
     from persons.models import Organisation
 
+    from documents.tests.base import _DocumentsTestCase
     from documents.models import Folder, FolderCategory, Document
-    from documents.constants import *
+    from documents.constants import REL_SUB_RELATED_2_DOC
     from documents.utils import get_csv_folder_or_create
 except Exception as e:
     print 'Error in <%s>: %s' % (__name__, e)
 
 
-class _DocumentsTestCase(CremeTestCase):
-    ADD_DOC_URL = '/documents/document/add'
-
-    @classmethod
-    def setUpClass(cls):
-        cls.populate('creme_core', 'creme_config', 'documents')
-
-    def setUp(self):
-        self._tmp_filepaths = []
-
-    def tearDown(self):
-        for path in self._tmp_filepaths:
-            delete_file(path)
-
-    def _build_filedata(self, content_str, suffix='.txt'):
-        tmpfile = NamedTemporaryFile(suffix=suffix, delete=False)
-        tmpfile.write(content_str)
-        tmpfile.flush()
-
-        #we close and reopen in order to have a file with the right name (so we must specify delete=False)
-        tmpfile.close()
-
-        name = tmpfile.name
-        self._tmp_filepaths.append(name)
-
-        return open(name, 'rb'), basename(name)
-
-    def _create_doc(self, title, file_obj, folder=None, description=None):
-        folder = folder or Folder.objects.all()[0]
-        data = {'user':     self.user.pk,
-                'title':    title,
-                'filedata': file_obj,
-                'folder':   folder.id,
-               }
-
-        if description is not None:
-            data['description'] = description
-
-        response = self.client.post(self.ADD_DOC_URL, follow=True, data=data)
-        self.assertNoFormError(response)
-
-        return response
+__all__ = ('DocumentTestCase', 'DocumentQuickFormTestCase', 'CSVDocumentQuickWidgetTestCase')
 
 
 class DocumentTestCase(_DocumentsTestCase):
@@ -125,8 +81,6 @@ class DocumentTestCase(_DocumentsTestCase):
                          response['Content-Disposition']
                         )
 
-        filedata.delete(filedata) #clean #TODO: in tearDown()...
-
     def test_createview02(self):
         "Unallowed extension"
         self.login()
@@ -150,8 +104,6 @@ class DocumentTestCase(_DocumentsTestCase):
         self.assertEqual('attachment; filename=%s' % file_name,
                          response['Content-Disposition']
                         )
-
-        filedata.delete(filedata) #clean
 
     def test_createview03(self):
         "Double extension (bugfix)"
@@ -177,8 +129,6 @@ class DocumentTestCase(_DocumentsTestCase):
                          response['Content-Disposition']
                         )
 
-        filedata.delete(filedata) #clean
-
     def test_createview04(self):
         "No extension"
         self.login()
@@ -198,8 +148,6 @@ class DocumentTestCase(_DocumentsTestCase):
         self.assertEqual('attachment; filename=%s.txt' % file_name,
                          response['Content-Disposition']
                         )
-
-        filedata.delete(filedata) #clean
 
     def test_editview(self):
         self.login()
@@ -221,7 +169,6 @@ class DocumentTestCase(_DocumentsTestCase):
                                             category=FolderCategory.objects.all()[0],
                                             user=self.user,
                                            )
-        file_to_delete = doc.filedata
 
         response = self.client.post(url, follow=True,
                                     data={'user':         self.user.pk,
@@ -238,8 +185,6 @@ class DocumentTestCase(_DocumentsTestCase):
         self.assertEqual(title,       doc.title)
         self.assertEqual(description, doc.description)
         self.assertEqual(folder,      doc.folder)
-
-        file_to_delete.delete(file_to_delete)#clean
 
     def test_add_related_document01(self):
         self.login()
@@ -283,9 +228,6 @@ class DocumentTestCase(_DocumentsTestCase):
         entity_folder2 = doc2.folder
         self.assertEqual(entity_folder, entity_folder2)
         self.assertEqual(ct_folder,     entity_folder2.parent_folder)
-
-        for doc in (doc1, doc2):
-            doc.filedata.delete(doc.filedata) #clean
 
     def test_add_related_document02(self): #creation credentials
         self.login(is_superuser=False, allowed_apps=['documents', 'persons'])
@@ -363,8 +305,6 @@ class DocumentTestCase(_DocumentsTestCase):
         self.assertTrue(title.startswith(u'%s_AAAAAAA' % entity.id))
         self.assertTrue(title.endswith(u'…'))
 
-        doc.filedata.delete(doc.filedata) #clean
-
     def test_listview(self):
         self.login()
 
@@ -386,10 +326,6 @@ class DocumentTestCase(_DocumentsTestCase):
 
         self.assertIn(doc1, docs_page.object_list)
         self.assertIn(doc2, docs_page.object_list)
-
-        #clean
-        doc1.filedata.delete(doc1.filedata)
-        doc2.filedata.delete(doc2.filedata)
 
     def test_delete_category(self):
         "Set to null"
@@ -451,8 +387,6 @@ class DocumentQuickFormTestCase(_DocumentsTestCase):
         filedata.open()
         self.assertEqual([content], filedata.readlines())
 
-        filedata.delete(filedata) #clean
-
 
 class CSVDocumentQuickWidgetTestCase(_DocumentsTestCase):
     def test_add_from_widget(self):
@@ -492,140 +426,3 @@ class CSVDocumentQuickWidgetTestCase(_DocumentsTestCase):
         filedata = doc.filedata
         filedata.open()
         self.assertEqual([content], filedata.readlines())
-
-        filedata.delete(filedata) #clean
-
-
-class FolderTestCase(_DocumentsTestCase):
-    def setUp(self):
-        super(FolderTestCase, self).setUp()
-        self.login()
-
-    def test_createview(self):
-        url = '/documents/folder/add'
-        self.assertGET200(url)
-
-        title = 'Test folder'
-        self.assertFalse(Folder.objects.filter(title=title).exists())
-
-        description = 'Test description'
-        parent      = Folder.objects.all()[0]
-        category    = FolderCategory.objects.all()[0]
-        response = self.client.post(url, follow=True,
-                                    data={'user':          self.user.pk,
-                                          'title':         title,
-                                          'description':   description,
-                                          'parent_folder': parent.id,
-                                          'category':      category.id,
-                                         }
-                                   )
-        self.assertNoFormError(response)
-
-        with self.assertNoException():
-            folder = Folder.objects.get(title=title)
-
-        self.assertEqual(description, folder.description)
-        self.assertEqual(parent,      folder.parent_folder)
-        self.assertEqual(category,    folder.category)
-
-    def test_editview(self):
-        title = u'Test folder'
-        description = 'Test description'
-        category = FolderCategory.objects.all()[0]
-        folder = Folder.objects.create(title=title,
-                                       description=description,
-                                       parent_folder=None,
-                                       category=category,
-                                       user=self.user,
-                                      )
-
-        url = '/documents/folder/edit/%s' % folder.id
-        self.assertGET200(url)
-
-        title       += u' edited'
-        description = description.upper()
-        parent      = Folder.objects.all()[0]
-        response = self.client.post(url, follow=True,
-                                    data={'user':          self.user.pk,
-                                          'title':         title,
-                                          'description':   description,
-                                          'parent_folder': parent.id,
-                                          'category':      category.id,
-                                         }
-                                   )
-        self.assertNoFormError(response)
-
-        folder = self.refresh(folder)
-        self.assertEqual(title,       folder.title)
-        self.assertEqual(description, folder.description)
-        self.assertEqual(parent,      folder.parent_folder)
-        self.assertEqual(category,    folder.category)
-
-    def test_listview(self):
-        user = self.user
-        category = FolderCategory.objects.all()[0]
-
-        create_folder = Folder.objects.create
-        folder1 = create_folder(user=user, title='PDF',
-                                description='Contains PDF files',
-                                parent_folder=None, category=category
-                               )
-        folder2 = create_folder(user=user, title='SVG',
-                                description='Contains SVG files',
-                                parent_folder=None, category=category
-                               )
-
-        response = self.client.get('/documents/folders')
-        self.assertEqual(200, response.status_code)
-
-        with self.assertNoException():
-            folders = response.context['entities'].object_list
-
-        self.assertIn(folder1, folders)
-        self.assertIn(folder2, folders)
-
-    def test_folder_clone01(self):
-        title = 'folder'
-        folder = Folder.objects.create(user=self.user, title=title, description="d")
-
-        stack = [folder]
-        stack_append = stack.append
-
-        for i in xrange(100):
-            clone = folder.clone()
-            self.assertNotEqual(stack[-1].title, clone.title)
-            stack_append(clone)
-
-    def test_deleteview01(self):
-        "No doc inside"
-        folder = Folder.objects.create(user=self.user, title='PDF',
-                                       description='Contains PDF files',
-                                       parent_folder=None,
-                                       category=FolderCategory.objects.all()[0],
-                                      )
-
-        response = self.client.post('/creme_core/entity/delete/%s' % folder.pk, follow=True)
-        self.assertEqual(200, response.status_code)
-        self.assertFalse(Folder.objects.filter(pk=folder.pk).exists())
-        self.assertRedirects(response, '/documents/folders')
-
-    def test_deleteview02(self):
-        "A doc inside protect from deletion"
-        folder = Folder.objects.create(user=self.user, title='PDF',
-                                       description='Contains PDF files',
-                                       parent_folder=None,
-                                       category=FolderCategory.objects.all()[0]
-                                      )
-
-        title = 'Boring title'
-        self._create_doc(title, folder=folder, description='Boring description too',
-                         file_obj=self._build_filedata('Content (FolderTestCase.test_deleteview02)')[0],
-                        )
-
-        doc = self.get_object_or_fail(Document, title=title)
-        self.assertEqual(folder, doc.folder)
-
-        self.assertPOST200('/creme_core/entity/delete/%s' % folder.pk)
-        self.get_object_or_fail(Folder, pk=folder.pk)
-
-        doc.filedata.delete(doc.filedata) #clean
