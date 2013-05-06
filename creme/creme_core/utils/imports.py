6d9636077f3fd8d79115a995fafda75fee216d99
Hugo Smett <hsmett@users.noreply.github.com>
2013-05-06 11:10:59 +0200

    refs #1 Add import features

--- creme/creme_core/utils/imports.py
+++ creme/creme_core/utils/imports.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2010  Hybird
+#    Copyright (C) 2009-2013  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -21,18 +21,40 @@
 from imp import find_module
 
 from django.conf import settings
+from django.utils.importlib import import_module
 
 
 def find_n_import(filename, imports):
     results = []
-
     for app in settings.INSTALLED_APPS:
         try:
             find_module(filename, __import__(app, {}, {}, [app.split(".")[-1]]).__path__)
-        except ImportError:
+        except ImportError as e:
             # there is no app report_backend_register.py, skip it
             continue
 
-        results.append(__import__("%s.%s" % (app, filename) , globals(), locals(), imports, -1))
-
+        results.append(__import__("%s.%s" % (app, filename), globals(), locals(), imports, -1))
     return results
+
+
+def import_object(objectpath):
+    i = objectpath.rfind('.')
+    module, attr = objectpath[:i], objectpath[i+1:]
+    try:
+        mod = import_module(module)
+    except ImportError:
+        raise
+
+    try:
+        result = getattr(mod, attr)
+    except AttributeError:
+        raise AttributeError('Module "%s" does not define a "%s" object' % (module, attr))
+
+    return result
+
+
+def safe_import_object(objectpath):
+    try:
+        return import_object(objectpath)
+    except Exception as e:
+        print 'An error occured trying to import "%s": [%s]' % (objectpath, e)
