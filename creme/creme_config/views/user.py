d5377e05cb310816e9d0c10c95f96a46942e8237
Hugo Smett <hsmett@users.noreply.github.com>
2013-12-10 10:36:03 +0100

    refs #46 As any staff user should now also be a super user, remove a useless decorator

--- creme/creme_config/views/user.py
+++ creme/creme_config/views/user.py
@@ -26,7 +26,7 @@ from django.contrib.auth.models import User
 
 from creme.creme_core.views.generic import add_model_with_popup, edit_model_with_popup
 from creme.creme_core.views.decorators import POST_only
-from creme.creme_core.auth.decorators import admin_required
+from creme.creme_core.auth.decorators import superuser_required
 from creme.creme_core.core.exceptions import ConflictError
 
 from ..forms.user import (UserAddForm, UserChangePwForm, UserEditForm,
@@ -34,19 +34,19 @@ from ..forms.user import (UserAddForm, UserChangePwForm, UserEditForm,
 
 
 @login_required
-@admin_required
+@superuser_required
 def change_password(request, user_id):
     return edit_model_with_popup(request, {'pk': user_id}, User,
                                  UserChangePwForm, _(u'Change password for <%s>')
                                 )
 
 @login_required
-@admin_required
+@superuser_required
 def add(request):
     return add_model_with_popup(request, UserAddForm, _(u'New user'))
 
 @login_required
-@admin_required
+@superuser_required
 def add_team(request):
     return add_model_with_popup(request, TeamCreateForm, _(u'New team'))
 
@@ -56,7 +56,7 @@ def portal(request):
     return render(request, 'creme_config/user_portal.html')
 
 @login_required
-@admin_required
+@superuser_required
 def edit(request, user_id):
     user_filter = {'pk':       user_id,
                    'is_team':  False}
@@ -68,7 +68,7 @@ def edit(request, user_id):
                                  UserEditForm)
 
 @login_required
-@admin_required
+@superuser_required
 def edit_team(request, user_id):
     return edit_model_with_popup(request,
                                  {'pk':       user_id,
@@ -78,7 +78,7 @@ def edit_team(request, user_id):
                                  TeamEditForm)
 
 @login_required
-@admin_required
+@superuser_required
 def delete(request, user_id):
     """Delete a User (who can be a Team). Objects linked to this User are
     linked to a new User.
@@ -106,7 +106,7 @@ def delete(request, user_id):
         return HttpResponse(_(u"You can't delete this user."), status=400)
 
 @login_required
-@admin_required
+@superuser_required
 @POST_only
 def deactivate(request, user_id):
     """Deactivate a user
@@ -128,7 +128,7 @@ def deactivate(request, user_id):
     return HttpResponse()
 
 @login_required
-@admin_required
+@superuser_required
 @POST_only
 def activate(request, user_id):
     """Deactivate a user
