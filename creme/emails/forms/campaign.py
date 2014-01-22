f9351e29db95dd385be88a9032ef47015ea98aa7
Guillaume Englert <genglert at hybird.org@localhost>
2014-01-22 18:17:02 +0100

    refs #6 Mailing lists that are already linked by a Campaign are not proposed anymore when adding MailingLists to this Campaign.

--- creme/emails/forms/campaign.py
+++ creme/emails/forms/campaign.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2013  Hybird
+#    Copyright (C) 2009-2014  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,8 +18,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-from django.forms import ValidationError
-from django.utils.translation import ugettext_lazy as _, ugettext
+#from django.forms import ValidationError
+from django.utils.translation import ugettext_lazy as _ #, ugettext
 
 from creme.creme_core.forms import CremeEntityForm, CremeForm, FieldBlockManager
 from creme.creme_core.forms.fields import MultiCreatorEntityField
@@ -48,21 +48,19 @@ class CampaignAddMLForm(CremeForm):
     def __init__(self, entity, *args, **kwargs):
         super(CampaignAddMLForm, self).__init__(*args, **kwargs)
         self.campaign = entity
+        self.fields['mailing_lists'].q_filter = {'~id__in': list(entity.mailing_lists.values_list('id', flat=True))}
 
-        #TODO: avoid duplicate like this
-        #self.fields['mailing_lists'].q_filter = {'~id__in': list(entity.mailing_lists.values_list('id', flat=True))}
+    ##in fact duplicate is not a problem with django's m2m
+    #def clean_mailing_lists(self):
+        #mailing_lists = self.cleaned_data['mailing_lists']
+        #current_mls   = frozenset(self.campaign.mailing_lists.values_list('pk', flat=True))
+        #duplicate     = [ml for ml in mailing_lists if ml.id in current_mls]
 
-    #in fact duplicate is not a problem with django's m2m
-    def clean_mailing_lists(self):
-        mailing_lists = self.cleaned_data['mailing_lists']
-        current_mls   = frozenset(self.campaign.mailing_lists.values_list('pk', flat=True))
-        duplicate     = [ml for ml in mailing_lists if ml.id in current_mls]
+        #if duplicate:
+            #raise ValidationError(ugettext(u"Following mailing lists are already related to this campaign: %s")
+                                  #% u', '.join(ml.name for ml in duplicate))
 
-        if duplicate:
-            raise ValidationError(ugettext(u"Following mailing lists are already related to this campaign: %s")
-                                  % u', '.join(ml.name for ml in duplicate))
-
-        return mailing_lists
+        #return mailing_lists
 
     def save(self):
         add_ml = self.campaign.mailing_lists.add
