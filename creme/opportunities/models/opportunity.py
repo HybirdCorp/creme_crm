6a2286d9b0193ee1ccba4aac4c225611ba00a1f8
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2014-02-03 12:02:37 +0100

    refs #25 Remove a useless private attribute.

--- creme/opportunities/models/opportunity.py
+++ creme/opportunities/models/opportunity.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2013  Hybird
+#    Copyright (C) 2009-2014  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -106,10 +106,10 @@ class Opportunity(CremeEntity):
         verbose_name = _(u'Opportunity')
         verbose_name_plural = _(u'Opportunities')
 
-    def __init__(self, *args, **kwargs):
-        super(Opportunity, self).__init__(*args, **kwargs)
+    #def __init__(self, *args, **kwargs):
+        #super(Opportunity, self).__init__(*args, **kwargs)
 
-        self._linked_activities = None
+        #self._linked_activities = None
 
     def __unicode__(self):
         return self.name
