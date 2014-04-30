abc104767a3175d1f4a94a13adc0b2b166b605f1
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2014-04-30 15:46:40 +0200

    refs #13 The 'Download' button in report preview is now disabled when the datefilter is changed.

--- creme/creme_core/static/creme_core/js/widgets/datetime.js
+++ creme/creme_core/static/creme_core/js/widgets/datetime.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
     Creme is a free/open-source Customer Relationship Management software
-    Copyright (C) 2009-2012  Hybird
+    Copyright (C) 2009-2014  Hybird
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -29,6 +29,7 @@ creme.widget.DatePicker = creme.widget.declare('ui-creme-datepicker', {
                                  .attr('type', 'button')
                                  .bind('click', function(e) {
                                                      element.datepicker('setDate', new Date());
+                                                     element.change();
                                                      return false;
                                                 });
 
