5e24fb91b3422586e8b81d5d5f197ad6dbf613a2
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2014-06-13 21:13:06 +0200

    refs #35 #29 Add unit tests for the command 'com_approaches_emails_send' + code refactorings/improvements.

--- creme/commercial/tests/commercial_approach.py
+++ creme/commercial/tests/commercial_approach.py
@@ -4,17 +4,29 @@ try:
     from datetime import timedelta #datetime
     from functools import partial
 
+    from django.conf import settings
+    from django.core import mail
     from django.utils.timezone import now
+    from django.utils.translation import ugettext as _
 
-    from creme.creme_core.models import CremeEntity, Relation
+    from creme.creme_core.constants import PROP_IS_MANAGED_BY_CREME
+    from creme.creme_core.models import CremeEntity, Relation, CremeProperty
     from creme.creme_core.tests.base import CremeTestCase
 
+    from creme.creme_config.models import SettingValue
+
+    from creme.persons.constants import (REL_SUB_CUSTOMER_SUPPLIER, 
+            REL_SUB_MANAGES, REL_SUB_EMPLOYED_BY)
     from creme.persons.models import Organisation, Contact
 
     from creme.activities.models import Activity, Status, Calendar
-    from creme.activities.constants import (ACTIVITYTYPE_MEETING, ACTIVITYSUBTYPE_MEETING_QUALIFICATION,
-                                           REL_SUB_PART_2_ACTIVITY)
+    from creme.activities.constants import (REL_SUB_PART_2_ACTIVITY,
+            ACTIVITYTYPE_MEETING, ACTIVITYSUBTYPE_MEETING_QUALIFICATION)
 
+    from creme.opportunities.models import Opportunity, SalesPhase
+
+    from ..constants import DISPLAY_ONLY_ORGA_COM_APPROACH_ON_ORGA_DETAILVIEW
+    from ..management.commands.com_approaches_emails_send import Command as EmailsSendCommand
     from ..models import CommercialApproach
 except Exception as e:
     print('Error in <%s>: %s' % (__name__, e))
@@ -23,17 +35,19 @@ except Exception as e:
 __all__ = ('CommercialApproachTestCase',)
 
 
-#TODO: test command
 class CommercialApproachTestCase(CremeTestCase):
     @classmethod
     def setUpClass(cls):
-        cls.populate('creme_core', 'creme_config', 'activities') #'commercial'
+        cls.populate('creme_core', 'creme_config', 'activities', 'opportunities', 'commercial')
+
+    def setUp(self):
+        self.login()
 
     def _build_entity_field(self, entity):
         return '[{"ctype":"%s", "entity":"%s"}]' % (entity.entity_type_id, entity.id)
 
     def test_createview(self):
-        self.login()
+        #self.login()
         entity = CremeEntity.objects.create(user=self.user)
         url = '/commercial/approach/add/%s/' % entity.id
         self.assertGET200(url)
@@ -55,11 +69,10 @@ class CommercialApproachTestCase(CremeTestCase):
         self.assertEqual(entity.id,   commapp.entity_id)
 
         self.assertLess((now() - commapp.creation_date).seconds, 10)
-
         self.assertEqual(title, unicode(commapp))
 
     def test_merge(self):
-        self.login()
+        #self.login()
         user = self.user
 
         create_orga = partial(Organisation.objects.create, user=user)
@@ -67,8 +80,6 @@ class CommercialApproachTestCase(CremeTestCase):
         orga02 = create_orga(name='Nerv')
 
         create_commapp = partial(CommercialApproach.objects.create, description='...')
-        #create_commapp(title='Commapp01', creation_date=datetime.now(), creme_entity=orga01)
-        #create_commapp(title='Commapp02', creation_date=datetime.now(), creme_entity=orga02)
         create_commapp(title='Commapp01', creme_entity=orga01)
         create_commapp(title='Commapp02', creme_entity=orga02)
         self.assertEqual(2, CommercialApproach.objects.count())
@@ -99,7 +110,7 @@ class CommercialApproachTestCase(CremeTestCase):
 
     def test_create_from_activity01(self):
         "No subjects"
-        self.login()
+        #self.login()
 
         user = self.user
         url = '/activities/activity/add'
@@ -129,7 +140,7 @@ class CommercialApproachTestCase(CremeTestCase):
         self.assertFalse(CommercialApproach.objects.all())
 
     def test_create_from_activity02(self):
-        self.login()
+        #self.login()
         user = self.user
 
         create_contact = partial(Contact.objects.create, user=user)
@@ -178,7 +189,7 @@ class CommercialApproachTestCase(CremeTestCase):
             self.assertAlmostEqual(now_value, comapp.creation_date, delta=timedelta(seconds=10))
 
     def test_sync_with_activity(self):
-        self.login()
+        #self.login()
 
         user = self.user
         title = 'meeting #01'
@@ -186,10 +197,8 @@ class CommercialApproachTestCase(CremeTestCase):
         create_dt = self.create_datetime
         meeting = Activity.objects.create(user=user, title=title, description=description,
                                           type_id=ACTIVITYTYPE_MEETING,
-                                          #start=datetime(year=2011, month=5, day=18, hour=14, minute=0),
-                                          #end=datetime(year=2011,   month=6, day=1,  hour=15, minute=0)
                                           start=create_dt(year=2011, month=5, day=18, hour=14, minute=0),
-                                          end=create_dt(year=2011,   month=6, day=1,  hour=15, minute=0)
+                                          end=create_dt(year=2011,   month=6, day=1,  hour=15, minute=0),
                                          )
         #ryoga = Contact.objects.create(user=user, first_name='Ryoga', last_name='Hibiki', is_user=user)
         #contact = self.get_object_or_fail(Contact, is_user=user)
@@ -202,7 +211,6 @@ class CommercialApproachTestCase(CremeTestCase):
 
         comapp = CommercialApproach.objects.create(title=title,
                                                    description=description,
-                                                   #creation_date=datetime.now(),
                                                    related_activity_id=meeting.id, #TODO: related_activity=instance after activities refactoring ?
                                                    #creme_entity=ryoga,
                                                    creme_entity=contact,
@@ -214,14 +222,161 @@ class CommercialApproachTestCase(CremeTestCase):
         self.assertEqual(title, self.refresh(comapp).title)
 
     def test_delete(self):
-        self.login()
+        #self.login()
 
         orga = Organisation.objects.create(user=self.user, name='NERV')
         comapp = CommercialApproach.objects.create(title='Commapp01',
                                                    description='A commercial approach',
-                                                   #creation_date=datetime.now(),
                                                    creme_entity=orga
                                                   )
 
         orga.delete()
         self.assertDoesNotExist(comapp)
+
+    def _send_mails(self):
+        EmailsSendCommand().handle(verbosity=0)
+
+    def _build_orgas(self):
+        user = self.user
+        mngd_orga = Organisation.get_all_managed_by_creme()[0]
+        customer  = Organisation.objects.create(user=user, name='NERV')
+
+        Relation.objects.create(user=user, subject_entity=customer,
+                                type_id=REL_SUB_CUSTOMER_SUPPLIER,
+                                object_entity=mngd_orga,
+                               )
+
+        return mngd_orga, customer
+
+    def test_command01(self):
+        "Customer has no CommercialApproach"
+        self._send_mails()
+        self.assertFalse(mail.outbox)
+
+        mngd_orga, customer = self._build_orgas()
+
+        self._send_mails()
+        messages = mail.outbox
+        self.assertEqual(1, len(messages))
+
+        message = messages[0]
+        self.assertEqual(_(u"[CremeCRM] The organisation <%s> seems neglected") % customer,
+                         message.subject
+                        )
+        self.assertEqual(_(u"It seems you haven't created a commercial approach "
+                           u"for the organisation «%(orga)s» since %(delay)s days.") % {
+                                'orga':  customer,
+                                'delay': 30,
+                            },
+                         message.body
+                        )
+        self.assertEqual(settings.EMAIL_SENDER, message.from_email)
+        self.assertFalse(hasattr(message, 'alternatives'))
+        self.assertFalse(message.attachments)
+        self.assertEqual([self.user.email],
+                         [recipient for message in messages
+                                    for recipient in message.recipients()
+                         ]
+                        )
+
+    def test_command02(self):
+        "A commapp is linked to the customer"
+        mngd_orga, customer = self._build_orgas()
+
+        CommercialApproach.objects.create(title='Commapp01',
+                                          description='A commercial approach',
+                                          creme_entity=customer,
+                                         )
+
+        self._send_mails()
+        self.assertFalse(mail.outbox)
+
+    def test_command03(self):
+        "The linked Commapp is to old"
+        mngd_orga, customer = self._build_orgas()
+
+        commapp = CommercialApproach.objects.create(title='Commapp01',
+                                                    description='A commercial approach',
+                                                    creme_entity=customer,
+                                                   )
+
+        CommercialApproach.objects.filter(id=commapp.id) \
+                                  .update(creation_date=commapp.creation_date - timedelta(days=31))
+
+        self._send_mails()
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_command04(self):
+        "A commapp is linked to a manager"
+        mngd_orga, customer = self._build_orgas()
+
+        manager = Contact.objects.create(user=self.user, first_name='Ryoga', last_name='Hibiki')
+        Relation.objects.create(user=self.user, subject_entity=manager,
+                                type_id=REL_SUB_MANAGES,
+                                object_entity=customer,
+                               )
+
+        CommercialApproach.objects.create(title='Commapp01',
+                                          description='A commercial approach',
+                                          creme_entity=manager,
+                                         )
+
+        self._send_mails()
+        self.assertFalse(mail.outbox)
+
+    def test_command05(self):
+        "A commapp is linked to a employee"
+        mngd_orga, customer = self._build_orgas()
+
+        employee = Contact.objects.create(user=self.user, first_name='Ryoga', last_name='Hibiki')
+        Relation.objects.create(user=self.user, subject_entity=employee,
+                                type_id=REL_SUB_EMPLOYED_BY,
+                                object_entity=customer,
+                               )
+
+        CommercialApproach.objects.create(title='Commapp01',
+                                          description='A commercial approach',
+                                          creme_entity=employee,
+                                         )
+
+        self._send_mails()
+        self.assertFalse(mail.outbox)
+
+    def test_command06(self):
+        "A commapp is linked to an Opportunity*"
+        mngd_orga, customer = self._build_orgas()
+
+        opp = Opportunity.objects.create(user=self.user, name='Opp custo',
+                                         sales_phase=SalesPhase.objects.all()[0],
+                                         emitter=mngd_orga, target=customer,
+                                        )
+
+        CommercialApproach.objects.create(title='Commapp01',
+                                          description='A commercial approach',
+                                          creme_entity=opp,
+                                         )
+
+        self._send_mails()
+        self.assertFalse(mail.outbox)
+
+    def test_command07(self):
+        "Ignore the managed orga that are customer of another managed orga"
+        mngd_orga, customer = self._build_orgas()
+
+        CremeProperty.objects.create(type_id=PROP_IS_MANAGED_BY_CREME,
+                                     creme_entity=customer,
+                                    )
+
+        self._send_mails()
+        self.assertFalse(mail.outbox)
+
+    def test_command08(self):
+        "DISPLAY_ONLY_ORGA_COM_APPROACH_ON_ORGA_DETAILVIEW setting"
+        sv = self.get_object_or_fail(SettingValue, key=DISPLAY_ONLY_ORGA_COM_APPROACH_ON_ORGA_DETAILVIEW)
+        sv.value = False
+        sv.save()
+
+        mngd_orga, customer = self._build_orgas()
+
+        self._send_mails()
+        self.assertFalse(mail.outbox)
