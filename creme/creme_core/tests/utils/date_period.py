8c300bea1bff0cd977c9292801500dd801b5140d
Hugo Smett <hsmett@hybird.org>
2014-09-19 15:40:41 +0200

    refs #1 Add a as_rrule() method to the DatePeriodField, which returns a dateutil.rrule (recurrent rule) object.

--- creme/creme_core/tests/utils/date_period.py
+++ creme/creme_core/tests/utils/date_period.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 try:
+    from dateutil.rrule import rrule, YEARLY, MONTHLY, WEEKLY, DAILY, HOURLY, MINUTELY
+
     from django.utils.translation import ugettext as _, ungettext
 
     from ..base import CremeTestCase
@@ -13,13 +15,27 @@ __all__ = ('DatePeriodTestCase',)
 
 
 class DatePeriodTestCase(CremeTestCase):
+    def _get_comparable_data(self, rrule):
+        rrule__dict__ = rrule.__dict__
+        rrule__dict__.pop('_bysecond')
+        rrule__dict__.pop('_dtstart')
+        rrule__dict__.pop('_timeset')
+        return rrule__dict__
+
+    def assertRRuleEqual(self, rrule1, rrule2):
+        self.assertEqual(self._get_comparable_data(rrule1),
+                         self._get_comparable_data(rrule2)
+                        )
+
     def test_hours(self):
         get = date_period_registry.get_period
         every_hour = get('hours', 1)
+        rrule_every_hour = rrule(HOURLY, interval=1)
         self.assertIsNotNone(every_hour)
         self.assertEqual(_('Hour(s)'), unicode(every_hour.verbose_name))
         self.assertEqual(ungettext('%s hour', '%s hours', 1) % 1, unicode(every_hour))
         self.assertEqual({'type': 'hours', 'value': 1}, every_hour.as_dict())
+        self.assertRRuleEqual(rrule_every_hour, every_hour.as_rrule())
 
         create_dt = self.create_datetime
         now_value = create_dt(year=2014, month=6, day=26, hour=22, minute=38)
@@ -28,15 +44,18 @@ class DatePeriodTestCase(CremeTestCase):
                         )
 
         every_3hours = get('hours', 3)
+        rrule_every_3hours = rrule(HOURLY, interval=3)
         self.assertEqual({'type': 'hours', 'value': 3}, every_3hours.as_dict())
         self.assertEqual(create_dt(year=2014, month=6, day=27, hour=1, minute=38),
                          now_value + every_3hours.as_timedelta()
                         )
         self.assertEqual(ungettext('%s hour', '%s hours', 3) % 3, unicode(every_3hours))
+        self.assertRRuleEqual(rrule_every_3hours, every_3hours.as_rrule())
 
     def test_minutes(self):
         get = date_period_registry.get_period
         every_3minutes = get('minutes', 3)
+        rrule_every_3minutes = rrule(MINUTELY, interval=3)
         self.assertIsNotNone(every_3minutes)
         self.assertEqual({'type': 'minutes', 'value': 3}, every_3minutes.as_dict())
         self.assertEqual(_('%s minutes') % 3, unicode(every_3minutes))
@@ -46,10 +65,12 @@ class DatePeriodTestCase(CremeTestCase):
                          create_dt(year=2014, month=6, day=27, hour=18, minute=22)
                          + every_3minutes.as_timedelta()
                         )
+        self.assertRRuleEqual(rrule_every_3minutes, every_3minutes.as_rrule())
 
     def test_days(self):
         get = date_period_registry.get_period
         every_day = get('days', 1)
+        rrule_every_day = rrule(DAILY, interval=1)
         self.assertIsNotNone(every_day)
         self.assertEqual({'type': 'days', 'value': 1}, every_day.as_dict())
         self.assertEqual(ungettext('%s day', '%s days', 1) % 1, unicode(every_day))
@@ -59,19 +80,24 @@ class DatePeriodTestCase(CremeTestCase):
         self.assertEqual(create_dt(year=2014, month=6, day=28, hour=18, minute=22),
                          now_value + every_day.as_timedelta()
                         )
+        self.assertRRuleEqual(rrule_every_day, every_day.as_rrule())
 
         every_5days = get('days', 5)
+        rrule_every_5days = rrule(DAILY, interval=5)
         self.assertEqual(create_dt(year=2014, month=7, day=2, hour=18, minute=22),
                          now_value + every_5days.as_timedelta()
                         )
         self.assertEqual(ungettext('%s day', '%s days', 5) % 5, unicode(every_5days))
+        self.assertRRuleEqual(rrule_every_5days, every_5days.as_rrule())
 
     def test_years(self):
         get = date_period_registry.get_period
         every_year = get('years', 1)
+        rrule_every_year = rrule(YEARLY, interval=1)
         self.assertIsNotNone(every_year)
         self.assertEqual({'type': 'years', 'value': 1}, every_year.as_dict())
         self.assertEqual(ungettext('%s year', '%s years', 1) % 1, unicode(every_year))
+        self.assertRRuleEqual(rrule_every_year, every_year.as_rrule())
 
         create_dt = self.create_datetime
         self.assertEqual(create_dt(year=2015, month=6, day=27, hour=18, minute=22),
@@ -86,14 +112,19 @@ class DatePeriodTestCase(CremeTestCase):
                         )
 
         # 29 february
+        every_3years = get('years', 3)
+        rrule_every_3years = rrule(YEARLY, interval=3)
         self.assertEqual(create_dt(year=2003, month=2, day=28, hour=14, minute=23),
                          create_dt(year=2000, month=2, day=29, hour=14, minute=23)
-                         + get('years', 3).as_timedelta()
+                         + every_3years.as_timedelta()
                         )
+        self.assertRRuleEqual(rrule_every_3years, every_3years.as_rrule())
+
 
     def test_months(self):
         get = date_period_registry.get_period
         every_month = get('months', 1)
+        rrule_every_month = rrule(MONTHLY, interval=1)
         self.assertIsNotNone(every_month)
         self.assertEqual({'type': 'months', 'value': 1}, every_month.as_dict())
         self.assertEqual(ungettext('%s month', '%s months', 1) % 1, unicode(every_month))
@@ -103,6 +134,7 @@ class DatePeriodTestCase(CremeTestCase):
         self.assertEqual(create_dt(year=2014, month=7, day=27, hour=18, minute=22),
                          now_value + every_month.as_timedelta()
                         )
+        self.assertRRuleEqual(rrule_every_month, every_month.as_rrule())
 
         #TODO
         ##self.assertEqual(create_dt(year=2014, month=10, day=27, hour=18, minute=22),
@@ -113,6 +145,7 @@ class DatePeriodTestCase(CremeTestCase):
     def test_weeks(self):
         get = date_period_registry.get_period
         every_week = get('weeks', 1)
+        rrule_every_week = rrule(WEEKLY, interval=1)
         self.assertIsNotNone(every_week)
         self.assertEqual({'type': 'weeks', 'value': 1}, every_week.as_dict())
         self.assertEqual(ungettext('%s week', '%s weeks', 1) % 1, unicode(every_week))
@@ -122,6 +155,7 @@ class DatePeriodTestCase(CremeTestCase):
                          create_dt(year=2014, month=6, day=27, hour=18, minute=22)
                          + every_week.as_timedelta()
                         )
+        self.assertRRuleEqual(rrule_every_week, every_week.as_rrule())
 
     def test_deserialize(self):
         get = date_period_registry.deserialize
