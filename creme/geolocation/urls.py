52aa437eec1e4ce70e58d32204eb7e95aa45ddae
Florian Fabre <ffabre at hybird.org@localhost>
2015-01-30 10:44:02 +0100

    refs #91 Fix issue with get_addresses_from_filter view url pattern that doesn't work with python 2.7.4 (stable).

--- creme/geolocation/urls.py
+++ creme/geolocation/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns('creme.geolocation.views',
     (r'^set_address_info/(?P<address_id>\d+)$', 'set_address_info'),
-    (r'^get_addresses_from_filter/(?P<filter_id>[\w-]*)?$', 'get_addresses_from_filter'),
+    (r'^get_addresses_from_filter/(?P<filter_id>[\w-]*)$', 'get_addresses_from_filter'),
     (r'^get_neighbours/(?P<address_id>\d+)/(?P<filter_id>[\w-]*)$', 'get_neighbours'),
 )
