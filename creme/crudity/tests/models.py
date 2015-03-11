085dadb68fd3df741ff47a675eca29a5ed9d8ba8
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2015-03-11 15:22:51 +0100

    refs #26 Try to avoid the staff users when assigning waiting actions.

--- creme/crudity/tests/models.py
+++ creme/crudity/tests/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 try:
+    from functools import partial
 
     from django.contrib.auth.models import User
     from django.contrib.contenttypes.models import ContentType
@@ -20,10 +21,28 @@ __all__ = ('WaitingActionTestCase',)
 
 
 class WaitingActionTestCase(CrudityTestCase):
+    @classmethod
+    def setUpClass(cls):
+        CrudityTestCase.setUpClass()
+
+        get_ct = ContentType.objects.get_for_model
+        cls.ct_entity  = get_ct(CremeEntity)
+        cls.ct_contact = get_ct(Contact)
+
+        cls._staff_user_ids_backup = list(User.objects.filter(is_staff=True)
+                                                      .values_list('id')
+                                         )
+
+    @classmethod
+    def tearDownClass(cls):
+        CrudityTestCase.tearDownClass()
+        User.objects.exclude(id__in=cls._staff_user_ids_backup).update(is_staff=False)
+
     def test_can_validate_or_delete01(self):
         "Sandbox for everyone"
-        action = WaitingAction.objects.create(user=None, source="unknown", action="create", subject="",
-                                              ct=ContentType.objects.get_for_model(CremeEntity)
+        action = WaitingAction.objects.create(user=None, source="unknown",
+                                              action="create", subject="",
+                                              ct=self.ct_entity,
                                              )
         self.assertTrue(action.can_validate_or_delete(self.user)[0])
         self.assertTrue(action.can_validate_or_delete(self.other_user)[0])
@@ -31,12 +50,15 @@ class WaitingActionTestCase(CrudityTestCase):
     def test_can_validate_or_delete02(self):
         "Sandbox by user"
         self._set_sandbox_by_user()
-        ct = ContentType.objects.get_for_model(CremeEntity)
-        action  = WaitingAction.objects.create(user=self.user, source="unknown", action="create", subject="", ct=ct)
-        self.assert_(action.can_validate_or_delete(self.user)[0])
+
+        create_waction = partial(WaitingAction.objects.create, source="unknown",
+                                 action="create", subject="", ct=self.ct_entity,
+                                )
+        action = create_waction(user=self.user)
+        self.assertTrue(action.can_validate_or_delete(self.user)[0])
         self.assertFalse(action.can_validate_or_delete(self.other_user)[0])
 
-        action2 = WaitingAction.objects.create(user=self.other_user, source="unknown", action="create", subject="", ct=ct)
+        action2 = create_waction(user=self.other_user)
         self.assertTrue(action2.can_validate_or_delete(self.user)[0])
         self.assertTrue(action2.can_validate_or_delete(self.other_user)[0])
 
@@ -44,35 +66,37 @@ class WaitingActionTestCase(CrudityTestCase):
         """If the sandbox was not by user, but now it is all WaitingAction has
         to be assigned to someone.
         """
-        action  = WaitingAction.objects.create(source="unknown", action="create", subject="",
-                                               ct=ContentType.objects.get_for_model(CremeEntity)
-                                              )
-        self.assertIsNone(action.user)
-
-        #Sandbox will be by user
-        self._set_sandbox_by_user()
-        self.assertEqual(0, WaitingAction.objects.filter(user=None).count())
-        self.assertEqual(self.user, WaitingAction.objects.filter(user__isnull=False)[0].user)
-
-    def test_auto_assignation02(self):
-        action = WaitingAction.objects.create(source="unknown", action="create", subject="",
-                                              ct=ContentType.objects.get_for_model(CremeEntity)
+        action = WaitingAction.objects.create(source="unknown", action="create",
+                                              subject="", ct=self.ct_entity,
                                              )
         self.assertIsNone(action.user)
 
-        #superuser1 = self.user
+        self.assertTrue(User.objects.filter(is_superuser=True, is_staff=False))
 
-        superuser2 = User.objects.create(username='Kirika2')
-        superuser2.set_password("Kirika2")
-        superuser2.is_superuser = True
-        superuser2.save()
+        #Sandbox will be by user
+        self._set_sandbox_by_user()
+        self.assertFalse(WaitingAction.objects.filter(user=None))
+
+        action = self.refresh(action)
+        self.assertTrue(action.user.is_superuser)
+        self.assertFalse(action.user.is_staff)
+
+    def test_auto_assignation02(self):
+        action = WaitingAction.objects.create(source="unknown", action="create",
+                                              subject="", ct=self.ct_entity,
+                                             )
+        self.assertIsNone(action.user)
+
+        User.objects.filter(is_superuser=True).update(is_staff=True)
+
+        superuser = User.objects.create(username='Kirika2', is_superuser=True)
 
         self._set_sandbox_by_user()
-        self.assertEqual(0, WaitingAction.objects.filter(user=None).count())
-        self.assertEqual(superuser2, WaitingAction.objects.filter(user__isnull=False)[0].user)
+        self.assertFalse(WaitingAction.objects.filter(user=None))
+        self.assertEqual(superuser, self.refresh(action).user)
 
     def test_data_property01(self):
-        action = WaitingAction(ct=ContentType.objects.get_for_model(Contact))
+        action = WaitingAction(ct=self.ct_contact)
         expected_data = {u'first_name': u'Mario', u'last_name': u'Bros'}
         action.data = action.set_data(expected_data)
         action.save()
@@ -80,7 +104,7 @@ class WaitingActionTestCase(CrudityTestCase):
         self.assertEqual(expected_data, self.refresh(action).get_data())
 
     def test_data_property02(self):
-        action = WaitingAction(ct=ContentType.objects.get_for_model(Contact))
+        action = WaitingAction(ct=self.ct_contact)
         expected_data = {u'first_name': u'Mario', u'last_name': u'Bros',
                          u"friends": [u"Yoshi", u"Toad"], u"lives": 99,
                          u"ennemies": {'Bowser': 1, 'Koopa':50}, "epoch": now(),
