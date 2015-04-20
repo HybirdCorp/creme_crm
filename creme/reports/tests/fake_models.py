ed721ed0b169429ce81e0ca639e5d308a0181cd4
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2015-04-20 17:40:10 +0200

    refs #29 #13 Avoid the test models of reports to be imported out of tests.

--- creme/reports/tests/fake_models.py
+++ creme/reports/tests/fake_models.py
@@ -1,52 +1,57 @@
 # -*- coding: utf-8 -*-
 
-from django.db.models import CharField, TextField, ForeignKey, PROTECT #FileField
-from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
-from creme.creme_core.models import CremeEntity
+if not settings.TESTS_ON:
+    __all__ = ()
+else:
+    from django.db.models import CharField, TextField, ForeignKey, PROTECT #FileField
+    from django.utils.translation import ugettext_lazy as _
 
-
-class FakeFolder(CremeEntity):
-    title         = CharField(_(u'Title'), max_length=100, unique=True)
-    description   = TextField(_(u'Description'), null=True, blank=True)
-
-    allowed_related = CremeEntity.allowed_related | {'fakedocument'}
-
-    class Meta:
-        app_label = 'reports'
-        verbose_name = u'Test Folder'
-        verbose_name_plural = u'Test Folders'
-        ordering = ('title',)
-
-    def __unicode__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return "/reports/tests/folder/%s" % self.id
+    from creme.creme_core.models import CremeEntity
 
 
-class FakeDocument(CremeEntity):
-    title       = CharField(_(u'Title'), max_length=100)
-    description = TextField(_(u'Description'), blank=True, null=True)
-#    filedata    = FileField(_(u'File'), max_length=500, upload_to='upload/documents')
-    folder      = ForeignKey(FakeFolder, verbose_name=_(u'Folder'), on_delete=PROTECT)
+    class FakeFolder(CremeEntity):
+        title         = CharField(_(u'Title'), max_length=100, unique=True)
+        description   = TextField(_(u'Description'), null=True, blank=True)
+
+        allowed_related = CremeEntity.allowed_related | {'fakedocument'}
+
+        class Meta:
+            app_label = 'reports'
+            verbose_name = u'Test Folder'
+            verbose_name_plural = u'Test Folders'
+            ordering = ('title',)
+
+        def __unicode__(self):
+            return self.title
+
+        def get_absolute_url(self):
+            return "/reports/tests/folder/%s" % self.id
 
 
-    class Meta:
-        app_label = 'reports'
-        verbose_name = 'Test Document'
-        verbose_name_plural = u'Test Documents'
-        ordering = ('title',)
+    class FakeDocument(CremeEntity):
+        title       = CharField(_(u'Title'), max_length=100)
+        description = TextField(_(u'Description'), blank=True, null=True)
+#        filedata    = FileField(_(u'File'), max_length=500, upload_to='upload/documents')
+        folder      = ForeignKey(FakeFolder, verbose_name=_(u'Folder'), on_delete=PROTECT)
 
-    def __unicode__(self):
-        return self.title
 
-#    def get_absolute_url(self):
-#        return "/documents/document/%s" % self.id
-#
-#    def get_edit_absolute_url(self):
-#        return "/documents/document/edit/%s" % self.id
+        class Meta:
+            app_label = 'reports'
+            verbose_name = 'Test Document'
+            verbose_name_plural = u'Test Documents'
+            ordering = ('title',)
 
-    @staticmethod
-    def get_lv_absolute_url():
-        return '/reports/tests/documents'
+        def __unicode__(self):
+            return self.title
+
+#        def get_absolute_url(self):
+#            return "/documents/document/%s" % self.id
+#    
+#        def get_edit_absolute_url(self):
+#            return "/documents/document/edit/%s" % self.id
+
+        @staticmethod
+        def get_lv_absolute_url():
+            return '/reports/tests/documents'
