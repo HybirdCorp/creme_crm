25660d7888bb2d6ad6fa7253033833786af6ec19
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2015-04-23 13:41:12 +0200

    refs #29 #13 Use the new layout for the files of tests/ (django 1.6) in 'reports'.

--- creme/reports/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf import settings
-if settings.TESTS_ON: #TODO: remove this hack with the new test layout
-    from .report import *
-    from .graph import *
-    from .utils import *
