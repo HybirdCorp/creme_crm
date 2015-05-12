621187ba5a6410016635cb31a80f5e5021aef488
Guillaume Englert <genglert at hybird.org@localhost>
2015-05-12 17:04:41 +0200

    refs #19 #86 billing.recurrents_register was broken.

--- creme/billing/recurrents_register.py
+++ creme/billing/recurrents_register.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import get_credit_note_model, get_invoice_model, get_quote_model, get_sales_order_model
+from . import (get_credit_note_model, get_invoice_model, get_quote_model,
+        get_sales_order_model, get_template_base_model)
 #from .models import Invoice, Quote, SalesOrder, TemplateBase, CreditNote
 from .forms.templatebase import TemplateBaseCreateForm
 
@@ -10,6 +11,7 @@ from .forms.templatebase import TemplateBaseCreateForm
 #               (SalesOrder, TemplateBase, TemplateBaseCreateForm),
 #               (CreditNote, TemplateBase, TemplateBaseCreateForm),
 #              )
+TemplateBase = get_template_base_model()
 to_register = ((get_invoice_model(),     TemplateBase, TemplateBaseCreateForm),
                (get_quote_model(),       TemplateBase, TemplateBaseCreateForm),
                (get_sales_order_model(), TemplateBase, TemplateBaseCreateForm),
