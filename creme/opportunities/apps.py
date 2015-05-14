8b5e9254d74ba57d27e9a1657083236ac037866d
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2015-05-14 12:32:24 +0200

    refs #25 The signal handlers of 'opportunities' now live in signals.py & are bound in ready().

--- creme/opportunities/apps.py
+++ creme/opportunities/apps.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -5,3 +7,6 @@ from django.utils.translation import ugettext_lazy as _
 class OpportunitiesConfig(AppConfig):
     name = 'creme.opportunities'
     verbose_name = _(u'Opportunities')
+
+    def ready(self):
+        from . import signals
