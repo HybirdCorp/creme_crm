ef53969823157f89cb90d927614c8bbd59c3d0d7
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2015-05-14 12:32:24 +0200

    refs #26 The signal handler of 'crudity' is bound in ready().

--- creme/crudity/apps.py
+++ creme/crudity/apps.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -5,3 +7,6 @@ from django.utils.translation import ugettext_lazy as _
 class CrudityConfig(AppConfig):
     name = 'creme.crudity'
     verbose_name = _(u'External data management')
+
+    def ready(self):
+        from . import signals
