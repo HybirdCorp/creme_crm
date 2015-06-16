4fdb253e00340628c641d4a050d05130968a10a4
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2015-06-16 16:01:36 +0200

    refs #23 The view/form which create a related Contact filter better the RelationTypes (internal, ContentType constraints).

--- creme/persons/views/contact.py
+++ creme/persons/views/contact.py
@@ -18,10 +18,12 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 
 from creme.creme_core.auth.decorators import login_required, permission_required
+from creme.creme_core.core.exceptions import ConflictError
 from creme.creme_core.models import RelationType
 from creme.creme_core.views.generic import add_entity, edit_entity, view_entity, list_view
 
@@ -53,12 +55,23 @@ def add_with_relation(request, orga_id, rtype_id=None):
 #    if predicate_id:
 #        initial['relation_type'] = get_object_or_404(RelationType, symmetric_type=predicate_id)
     if rtype_id:
-        initial['relation_type'] = get_object_or_404(RelationType, symmetric_type=rtype_id)
+        rtype = get_object_or_404(RelationType, id=rtype_id)
+
+        if rtype.is_internal:
+            raise ConflictError('This RelationType cannot be used because it is internal.')
+
+        if not rtype.is_compatible(linked_orga.entity_type_id):
+            raise ConflictError('This RelationType is not compatible with Organisation as subject')
+
+        if not rtype.symmetric_type.is_compatible(ContentType.objects.get_for_model(Contact).id): #TODO: improve API of is_compatible
+            raise ConflictError('This RelationType is not compatible with Contact as relationship-object')
+
+        initial['relation_type'] = rtype.symmetric_type
 
     return add_entity(request, RelatedContactForm,
                       #request.REQUEST.get('callback_url'),
                       request.POST.get('callback_url') or request.GET.get('callback_url'),
-                      'persons/add_contact_form.html', extra_initial=initial
+                      'persons/add_contact_form.html', extra_initial=initial,
                      )
 
 @login_required
