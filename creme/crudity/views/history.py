af3ac291143369ade74a6549d84f8118e77e4677
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2015-06-17 16:26:30 +0200

    refs #26 Small code improvement in crudity.views.history (blocks management).

--- creme/crudity/views/history.py
+++ creme/crudity/views/history.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2014  Hybird
+#    Copyright (C) 2009-2015  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -19,29 +19,36 @@
 ################################################################################
 
 from django.contrib.contenttypes.models import ContentType
-from django.shortcuts import render_to_response
+from django.shortcuts import render # render_to_response
 from django.template.context import RequestContext
 
 from creme.creme_core.auth.decorators import login_required, permission_required
 from creme.creme_core.utils import get_ct_or_404, jsonify
 
-from ..registry import crudity_registry
 from ..blocks import HistoryBlock
+from ..registry import crudity_registry
 
 
 @login_required
 @permission_required('crudity')
 def history(request):
-    blocks  = []
-    context = RequestContext(request)
+#    blocks  = []
+#    context = RequestContext(request)
+#    get_ct  = ContentType.objects.get_for_model
+#
+#    for backend in crudity_registry.get_backends():
+#        bmodel = backend.model
+#        if bmodel:
+#            blocks.append(HistoryBlock(get_ct(bmodel)).detailview_display(context))
+#
+#    return render_to_response("crudity/history.html", {'blocks': blocks}, context_instance=context)
     get_ct  = ContentType.objects.get_for_model
+    blocks  = [HistoryBlock(get_ct(backend.model))
+                   for backend in crudity_registry.get_backends()
+                       if backend.model
+              ]
 
-    for backend in crudity_registry.get_backends():
-        bmodel = backend.model
-        if bmodel:
-            blocks.append(HistoryBlock(get_ct(bmodel)).detailview_display(context))
-
-    return render_to_response("crudity/history.html", {'blocks': blocks}, context_instance=context)
+    return render(request, 'crudity/history.html', {'blocks': blocks})
 
 @jsonify
 @login_required
