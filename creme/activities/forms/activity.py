a6e15854ecf467dec7335261c659ac13797cfaf8
Guillaume Englert <genglert at hybird.org@localhost>
2015-06-19 17:01:48 +0200

    refs #18 Participants can now be created directly in the forms.

--- creme/activities/forms/activity.py
+++ creme/activities/forms/activity.py
@@ -328,7 +328,14 @@ class ActivityCreateForm(_ActivityCreateForm):
 
         fields['participating_users'].queryset = get_user_model().objects.filter(is_staff=False) \
                                                                          .exclude(pk=user.id)
-        fields['other_participants'].q_filter = {'is_user__isnull': True}
+
+        other_f = fields['other_participants']
+        other_f.q_filter = {'is_user__isnull': True}
+        # HACK : The 'q_filter' disable creation when 'creation_action_url' is empty because default creation views
+        # cannot return filtered instances.
+        # So this weird line forces a value in 'creation_action_url' in order re-enable creation button.
+        # The creation view cannot create a Contact with a non-null 'is_user'.
+        other_f.create_action_url = other_f.create_action_url
 
     def clean_my_participation(self):
         my_participation = self.cleaned_data.get('my_participation', False)
