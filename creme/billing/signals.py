de35dcca870a4c7f71bd82e6511ebf80db2904cd
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2015-07-06 17:15:05 +0200

    refs #19 #70 Fix a crash when merging 2 Organisations with a billing number algorithm configuration (integrity error with unicity).

--- creme/billing/signals.py
+++ creme/billing/signals.py
@@ -25,6 +25,7 @@ from django.dispatch import receiver
 
 from creme.creme_core.constants import PROP_IS_MANAGED_BY_CREME
 from creme.creme_core.models import Relation, CremeProperty
+from creme.creme_core.signals import pre_merge_related
 
 from creme.persons import get_organisation_model
 
@@ -60,6 +61,33 @@ def set_simple_conf_billing(sender, instance, created, **kwargs):
                 SimpleBillingAlgo.objects.create(organisation=orga, last_number=0,
                                                  prefix=prefix, ct=ct,
                                                 )
+
+@receiver(pre_merge_related)
+def handle_merge_organisations(sender, other_entity, **kwargs):
+    # NB: we assume that all CTs are covered if at least one CT is covered
+    #     because ConfigBillingAlgo/SimpleBillingAlgo instances are only
+    #     created in _simple_conf_billing_for_org_managed_by_creme().
+    orga_2_clean = None # 'cache'
+
+    def get_orga_2_clean():
+        managed_ids = set(CremeProperty.objects
+                                       .filter(creme_entity__in=(sender.id, other_entity.id))
+                                       .values_list('creme_entity', flat=True)
+                         )
+
+        return sender if len(managed_ids) == 1 and sender.id not in managed_ids else other_entity
+
+    for model in (ConfigBillingAlgo, SimpleBillingAlgo):
+        model_filter = model.objects.filter
+        orga_ids = set(model_filter(organisation__in=(sender, other_entity))
+                                    .values_list('organisation', flat=True)
+                      )
+
+        if len(orga_ids) == 2:
+            orga_2_clean = orga_2_clean or get_orga_2_clean()
+            model_filter(organisation=orga_2_clean).delete()
+        else:
+            return # we avoid the queries for the next model (if it's the first iteration)
 
 @receiver((post_save, post_delete), sender=Relation)
 def manage_linked_credit_notes(sender, instance, **kwargs):
