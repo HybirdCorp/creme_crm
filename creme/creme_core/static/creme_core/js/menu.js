0d88733fb00603e2676c2ef074a4a256418a6f08
Florian Fabre <ffabre@hybird.org>
2016-01-08 16:31:54 +0100

    refs #36 Move ugly menu javascript of base.html to menu.js. Fix some menu initial position bugs.

--- creme/creme_core/static/creme_core/js/menu.js
+++ creme/creme_core/static/creme_core/js/menu.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
     Creme is a free/open-source Customer Relationship Management software
-    Copyright (C) 2009-2013  Hybird
+    Copyright (C) 2009-2016  Hybird
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -119,3 +119,178 @@ creme.menu.NavIt = function(trigger_selector, options, listeners) {
 creme.menu.HNavIt = function(trigger_selector, options, listeners) {
     creme.menu.NavIt(trigger_selector, $.extend({ArrowSideOnRight: false}, options || {}), listeners);
 }
+
+creme.menu.sideMenu = function(options) {
+    var document_width_onload  = $(document).width();
+    var document_height_onload = $(document).height();
+
+    function activeEventsMenu() {
+        // 'fast' transition makes bugs in chrome < 6.1
+        $("#menu_collapse").mouseenter(function(e) {
+            $("#menu_expanded").show('fast');
+            $("#menu_collapse").hide('fast');
+        });
+
+        $("#menu_expanded").mouseleave(function(e) {
+            $("#menu_expanded").hide('fast');
+            $("#menu_collapse").show('fast');
+        });
+    }
+
+    function desactiveEventsMenu() {
+        $("#menu_collapse").unbind('mouseenter');
+        $("#menu_expanded").unbind('mouseleave');
+    }
+
+    function resizePageElements(menu_width, expanded) {
+        var top = $('#top');
+        var content = $('#content');
+        var footer = $('#footer');
+
+        var margin_left = (expanded) ? menu_width + 5 + 'px' : menu_width;
+
+        top.css('margin-left', margin_left);
+        content.css('margin-left', margin_left);
+        footer.css('margin-left', margin_left);
+
+        var new_width = document_width_onload - menu_width;
+        if(expanded) new_width -= 10;
+        top.width(new_width);
+        content.width(new_width);
+        footer.width(new_width);
+    }
+
+    //var content_height = $("#content").height()+$("#footer").height()+$("#top").height();//$('#footer').offset().top;//
+    var logo_container_height = $('#logo_container').outerHeight();
+    //var max_menu_height = content_height - logo_container_height;
+
+    var height = $('#menu_expanded').outerHeight();
+
+    $('#menu').menu({
+        content: $('#tree_menu2').html(),
+        flyOut: false,
+        backLink: false,
+        alwaysOpen:true,
+        maxHeight: height,
+        crumbDefaultText : options.title,
+        topLinkText : options.back
+    });
+
+    $('#menu').hide();
+    $('.fg-menu-container')
+        .appendTo('#menu_expanded')
+        .css({'top': logo_container_height, 'bottom':'auto'});
+        //.css({'top': logo_container_height, 'height':max_menu_height, 'bottom':'auto'});
+
+     $("#menu_expanded").width($('.fg-menu-container').outerWidth());
+
+    // Pin / fixed menu
+    var onPin = function() {
+        desactiveEventsMenu();
+        resizePageElements($("#menu_expanded").outerWidth(), true);
+        $.cookie('menu_creme', 'expanded', {expires: 7, path: '/', domain: '', secure: false});
+    };
+
+    // Unpin / floating menu
+    var onUnpin = function() {
+        activeEventsMenu();
+        resizePageElements($("#menu_collapse").outerWidth(), false);
+        $.cookie('menu_creme', 'collapse', {expires: 7, path: '/', domain: '', secure: false});
+    };
+
+    // jquery 1.9 migration: toggle(handler, handler, ...) doesn't exist any more.
+    if ($.cookie('menu_creme') == 'expanded') {
+        $('.pinmenu').addClass('expanded')
+        desactiveEventsMenu();
+        resizePageElements($("#menu_expanded").outerWidth(), true)
+        $("#menu_expanded").show();
+        $("#menu_collapse").hide();
+    } else {
+        activeEventsMenu();
+        resizePageElements($("#menu_collapse").outerWidth(), false)
+        $("#menu_expanded").hide();
+        $("#menu_collapse").show();
+    }
+
+    $('.pinmenu').on('click', function() {
+        $(this).toggleClass('expanded');
+
+        if ($(this).is('.expanded')) {
+            onPin();
+        } else {
+            onUnpin();
+        }
+    });
+
+    $("#menu_collapse").css('height', $(document).height());
+    $("#menu_expanded").css('height', $(document).height());
+
+    // jquery 1.9.x upgrade.
+    $('#menu_expanded').on('menu-item-selected', '#active-menuitem a', function(e, menu) {
+        e.stopImmediatePropagation();
+        var href = $(this).prop('href');
+        //$.cookie('creme-menu-selected', href);
+        creme.utils.goTo(href);
+    });
+
+    (function bind_scroll() {
+        $(window).scroll(function(){
+            var $menu = $('.fg-menu-container');
+
+            if ($(this).scrollTop() > $('#logo_container').outerHeight()){
+                $menu.addClass('top_fixed');
+                $menu.css("left", -$(window).scrollLeft() + "px");
+            }
+            else{
+                $menu.removeClass('top_fixed');
+            }
+        });
+    })();
+
+    $('#logo_container').one('load', function() {
+        //var content_height = $("#content").height()+$("#footer").height()+$("#top").height();//Is better but detailviews has float div so we have a very small height size
+        /*var content_height = $(document).height();
+        var logo_container_height = $('#logo_container').outerHeight();
+        var max_menu_height = content_height - logo_container_height;
+
+        $('.fg-menu-container').css({'top': logo_container_height, 'height':max_menu_height, 'bottom':'auto'});
+        $('.fg-menu').css('height', max_menu_height*0.9);
+        allUIMenus[0].setMaxHeight(max_menu_height*0.9);
+        $('#menu_collapse').height(content_height*0.9);*/
+
+        //var previous_selected = $.cookie('creme-menu-selected');
+
+        var logo_container_height = $('#logo_container').outerHeight(); // Webkit
+        $('.fg-menu-container').css({'top': logo_container_height}); // Webkit
+
+        var previous_selected = window.location.pathname;
+
+        if (previous_selected != null) {
+            var $initial = $('#menu_expanded div.fg-menu-container').find('[href="'+previous_selected+'"]');
+            var $ul = null;
+
+            var li_stack = [];
+            var more = true;
+
+            var $li = $initial.parent('li');
+            li_stack.push($li);
+
+            var max_deep = 30; // I think the depth of the folder will not go more than 30 (which is already huge)
+            var deep = 0;
+
+            while (more && deep <= max_deep) { //Set max deep to avoid max recursion
+                var ul = $li.parent('ul');
+                $li = ul.parent('li');
+                if($li.size() == 0) more = false;
+                else li_stack.push($li);
+                deep++;
+            }
+
+            var now = +new Date;
+
+            for (var i = li_stack.length-1; i > 0; i--) {
+                li_stack[i].find('a:first').click();
+            }
+        }
+    });
+};
