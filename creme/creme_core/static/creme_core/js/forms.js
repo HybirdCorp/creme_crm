6db22a323ffe28fabcb2e564259dc96c7713f8f9
Guillaume Englert <genglert at hybird.org@localhost>
2016-02-02 22:27:06 +0100

    refs #14 Minor fixes in JavaScript.

--- creme/creme_core/static/creme_core/js/forms.js
+++ creme/creme_core/static/creme_core/js/forms.js
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
 
-creme.forms = {}
+creme.forms = {};
 
 /*
  * Select input tools
