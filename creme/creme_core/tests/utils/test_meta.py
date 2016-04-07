1f03c22c16ef3d6dd619a91d9083f174c9c80257
Guillaume Englert <genglert at hybird.org@localhost>
2016-04-07 20:29:26 +0200

    refs #1 A method FieldInfo.value_from() has been added ; it reads the field value from an instance. Some code improvements: use the fields options instead of isinstance().
    API breaking:
     - creme_core.utils.meta.get_instance_field_info() function is deprecated ; use FieldInfo.value_from() instead.

--- creme/creme_core/tests/utils/test_meta.py
+++ creme/creme_core/tests/utils/test_meta.py
@@ -2,7 +2,7 @@
 
 try:
     from django.contrib.auth import get_user_model
-    from django.db.models import fields, FieldDoesNotExist
+    from django.db import models
     from django.utils import translation
     from django.utils.translation import ugettext as _
 
@@ -10,7 +10,7 @@ try:
     from ..fake_models import (FakeContact as Contact,
             FakeOrganisation as Organisation, FakeImage as Image,
             FakeEmailCampaign as EmailCampaign, FakeActivity)
-    from creme.creme_core.models import CremePropertyType, CremeProperty, CremeEntity
+    from creme.creme_core.models import CremePropertyType, CremeProperty, CremeEntity, Language
     from creme.creme_core.utils import meta
 except Exception as e:
     print('Error in <%s>: %s' % (__name__, e))
@@ -21,20 +21,21 @@ class MetaTestCase(CremeTestCase):
     def setUpClass(cls):
         CremeTestCase.setUpClass()
         cls._lang = translation.get_language()
-        cls._translation_deactvated = False
+        cls._translation_deactivated = False
 
     def tearDown(self):
         super(MetaTestCase, self).tearDown()
 
-        if self._translation_deactvated:
+        if self._translation_deactivated:
             translation.activate(self._lang)
-            self._translation_deactvated = False
+            self._translation_deactivated = False
 
     def _deactivate_translation(self):  # TODO: decorator ?? in CremeTestCase ?
         translation.deactivate_all()
-        self._translation_deactvated = True
+        self._translation_deactivated = True
 
-    def test_get_instance_field_info(self):
+    def test_get_instance_field_info01(self):
+        get_info = meta.get_instance_field_info
         text = 'TEXT'
 
         user   = get_user_model().objects.create(username='name')
@@ -42,13 +43,41 @@ class MetaTestCase(CremeTestCase):
         entity = CremeEntity.objects.create(user=user)
         prop   = CremeProperty(type=ptype, creme_entity=entity)
 
-        self.assertEqual((fields.CharField,    text), meta.get_instance_field_info(prop, 'type__text'))
-        self.assertEqual((fields.BooleanField, True), meta.get_instance_field_info(prop, 'type__is_custom'))
+        self.assertEqual((models.CharField,    text), get_info(prop, 'type__text'))
+        self.assertEqual((models.BooleanField, True), get_info(prop, 'type__is_custom'))
 
-        self.assertEqual((None, ''), meta.get_instance_field_info(prop, 'foobar__is_custom'))
-        self.assertEqual((None, ''), meta.get_instance_field_info(prop, 'type__foobar'))
+        self.assertEqual((None, ''), get_info(prop, 'foobar__is_custom'))
+        self.assertEqual((None, ''), get_info(prop, 'type__foobar'))
 
-        self.assertEqual(fields.CharField, meta.get_instance_field_info(prop, 'creme_entity__entity_type__model')[0])
+        self.assertEqual(models.CharField, get_info(prop, 'creme_entity__entity_type__model')[0])
+
+    def test_get_instance_field_info02(self):
+        "ManyToMany"
+        get_info = meta.get_instance_field_info
+        user = get_user_model().objects.create(username='name')
+        contact = Contact.objects.create(user=user, first_name='Alphonse', last_name='Elric')
+        self.assertEqual((models.CharField, contact.first_name), get_info(contact, 'first_name'))
+
+        # -----------
+        m2m_info = get_info(contact, 'languages')
+        self.assertEqual(models.ManyToManyField, m2m_info[0])
+
+        values = m2m_info[1]
+        self.assertEqual(Language, values.model)
+        self.assertEqual([], list(values))
+
+        # -------------
+        create_language = Language.objects.create
+        l1 = create_language(name='English',  code='EN')
+        l2 = create_language(name='French',   code='FRA')
+        l3 = create_language(name='Japanese', code='JP')
+
+        contact.languages = [l1, l3]
+        self.assertEqual({l1, l3}, set(get_info(contact, 'languages')[1]))
+
+        # -------------
+        # TODO: fix it ??
+        self.assertEqual((None, ''), get_info(contact, 'languages__code'))
 
     def test_field_info01(self):
         "Simple field"
@@ -92,6 +121,8 @@ class MetaTestCase(CremeTestCase):
 
     def test_field_info03(self):
         "Invalid fields"
+        FieldDoesNotExist = models.FieldDoesNotExist
+
         with self.assertRaises(FieldDoesNotExist):
             meta.FieldInfo(Contact, 'invalid')
 
@@ -112,6 +143,60 @@ class MetaTestCase(CremeTestCase):
         self.assertEqual(2, len(sub_fi))
         self.assertEqual(Image._meta.get_field('user'),    sub_fi[0])
         self.assertEqual(get_user_model()._meta.get_field('username'), sub_fi[1])
+
+    def test_field_info_get_value01(self):
+        FieldInfo = meta.FieldInfo
+
+        user = get_user_model().objects.create(username='alphonse')
+        al = Contact.objects.create(user=user, first_name='Alphonse', last_name='Elric')
+
+        self.assertEqual(al.first_name,
+                         FieldInfo(Contact, 'first_name').value_from(al)
+                        )
+        self.assertEqual(user,
+                         FieldInfo(Contact, 'user').value_from(al)
+                        )
+        self.assertEqual(user.username,
+                         FieldInfo(Contact, 'user__username').value_from(al)
+                        )
+
+        # Other model
+        ptype = CremePropertyType.objects.create(text='Is the hero', is_custom=True)
+        prop = CremeProperty(type=ptype, creme_entity=al)
+        self.assertEqual(ptype.text,
+                         FieldInfo(CremeProperty, 'type__text').value_from(prop)
+                        )
+        self.assertEqual(al.entity_type.model,
+                         FieldInfo(CremeProperty, 'creme_entity__entity_type__model').value_from(prop)
+                        )
+
+        with self.assertRaises(ValueError):
+            FieldInfo(CremeProperty, 'type__text').value_from(al)  # 'al' is not a CremeProperty
+
+        self.assertIsNone(FieldInfo(Contact, 'sector').value_from(al))
+        self.assertIsNone(FieldInfo(Contact, 'sector__title').value_from(al))
+
+    def test_field_info_get_value02(self):
+        "ManyToManyField"
+        FieldInfo = meta.FieldInfo
+
+        user = get_user_model().objects.create(username='alphonse')
+        al = Contact.objects.create(user=user, first_name='Alphonse', last_name='Elric')
+
+        self.assertEqual([], FieldInfo(Contact, 'languages').value_from(al))
+        self.assertEqual([], FieldInfo(Contact, 'languages__code').value_from(al))
+
+        # ----
+        create_language = Language.objects.create
+        l1 = create_language(name='English',  code='EN')
+        l2 = create_language(name='French',   code='FRA')
+        l3 = create_language(name='Japanese', code='JP')
+
+        al.languages = [l1, l3]
+        self.assertEqual([l1, l3], FieldInfo(Contact, 'languages').value_from(al))
+        self.assertEqual([l1.name, l3.name], FieldInfo(Contact, 'languages__name').value_from(al))
+
+    # TODO: test mtom1__mtom2
 
     # def test_get_date_fields(self):
     #     datefields = meta.get_date_fields(CremeEntity())
