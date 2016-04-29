1b2a305a709900e3392a97242e57f580b9524bfe
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2016-04-29 13:04:11 +0200

    refs #15 Improved command 'i18n_overload'. A new option '--comment_useless' which help to remove useless messages. A new option '--js' to inspect & create files djangojs.po. The metadata 'Plural-Forms' is not hard-coded anymore ; it is found from other .po files.

--- creme/creme_core/management/commands/i18n_overload.py
+++ creme/creme_core/management/commands/i18n_overload.py
@@ -25,7 +25,7 @@ from os.path import join, exists, isdir
 import pytz
 
 from django.apps import apps
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.utils.encoding import smart_unicode
 
@@ -41,36 +41,107 @@ class Command(BaseCommand):
     args = 'term1 term2 ... termN'
 
     def add_arguments(self, parser):
-        parser.add_argument('-l', '--language',
-                            action='store', dest='language', default='en',
-                            help='Search terms in LANGUAGE files. [default: %(default)s]',
-                           )
+        add_arg = parser.add_argument
+        add_arg('-l', '--language',
+                action='store', dest='language', default='en',
+                help='Search terms in LANGUAGE files. [default: %(default)s]',
+               )
+        add_arg('-u', '--comment_useless',
+                action='store_true', dest='comment_useless', default=False,
+                help='Search messages which are useless in the overloading file '
+                     '(ie: these messages are not found in overloaded files) '
+                     'and mark them as obsolete (ie: start with "#~" in the po file).'
+                     '[default: %(default)s]',
+               )
+        add_arg('--js',
+                action='store_true', dest='javascript', default=False,
+                help='Work with po file for javascript (ie: djangojs.po instead of djangojs.po) '
+                     '[default: %(default)s]',
+               )
 
     def handle(self, *args, **options):
-        if not args:
-            self.stderr.write('Error: give at least one term')
-            return
-
         try:
-            from polib import pofile, POFile
+            import polib
         except ImportError as e:
             self.stderr.write(str(e))
             self.stderr.write('The required "polib" library seems not installed ; aborting.')
             return
 
-        verbosity = options.get('verbosity')
+        get_opt = options.get
+        verbosity = get_opt('verbosity')
 
         if verbosity >= 2:
             self.stdout.write('OK "polib" library is installed.')
 
-        language = options.get('language')
-        catalog_entries = {}
+        language = get_opt('language')
+        file_name = 'djangojs.po'if get_opt('javascript') else 'django.po'
 
+        if get_opt('comment_useless'):
+            self._comment_useless(language, verbosity, polib, file_name, *args)
+        else:
+            self._overload_terms(language, verbosity, polib, file_name, *args)
+
+    def _get_catalog_paths(self, language, file_name):
         catalog_dirpath = join(settings.CREME_ROOT, APP_NAME, 'locale', language, 'LC_MESSAGES')
-        catalog_path = join(catalog_dirpath, 'django.po')
+        catalog_path = join(catalog_dirpath, file_name)
+
+        return catalog_dirpath, catalog_path
+
+    def _iter_pofiles(self, language, polib, file_name):
+        for app_config in apps.get_app_configs():
+            basepath = join(app_config.path, 'locale', language, 'LC_MESSAGES')
+
+            if exists(basepath):
+                for fname in listdir(basepath):
+                    if fname == file_name:
+                        path = join(basepath, fname)
+
+                        yield polib.pofile(path)
+
+    def _comment_useless(self, language, verbosity, polib, file_name, *args):
+        if args:
+            self.stderr.write('Error: in <useless> mode, given terms are not used')
+
+        catalog_dirpath, catalog_path = self._get_catalog_paths(language, file_name)
+
+        if not exists(catalog_path):
+            raise CommandError('no existing overloading %s found in "%s".' % (
+                                        file_name, catalog_dirpath,
+                                    )
+                              )
+
+        catalog = polib.pofile(catalog_path)
+        overloading_entries = {entry.msgid: entry
+                                   for entry in catalog.translated_entries()
+                              }
+
+        for app_pofile in self._iter_pofiles(language, polib, file_name):
+            for entry in app_pofile.translated_entries():
+                overloading_entries.pop(entry.msgid, None)
+
+        if not overloading_entries:
+            if verbosity >= 1:
+                self.stdout.write('No useless entry')
+        else:
+            if verbosity >= 1:
+                self.stdout.write('%s useless entries found and commented:' % len(overloading_entries))
+
+            for msgid, entry in overloading_entries.iteritems():
+                self.stdout.write(msgid)
+                entry.obsolete = True
+
+            catalog.save(catalog_path)
+
+    def _overload_terms(self, language, verbosity, polib, file_name, *args):
+        if not args:
+            raise CommandError('please give at least one term')
+
+        catalog_entries = {}
+        catalog_dirpath, catalog_path = self._get_catalog_paths(language, file_name)
+        all_plural_forms = set()
 
         if exists(catalog_path):
-            catalog = pofile(catalog_path)
+            catalog = polib.pofile(catalog_path)
 
             for entry in catalog.translated_entries():
                 entry.obsolete = True
@@ -85,7 +156,7 @@ class Command(BaseCommand):
                 self.stderr.write('"%s" exists and is not a directory.' % catalog_dirpath)
                 return
 
-            catalog = POFile()
+            catalog = polib.POFile()
             catalog.metadata = {
                     'Project-Id-Version':        'PACKAGE VERSION',
                     'Report-Msgid-Bugs-To':      '',
@@ -95,7 +166,6 @@ class Command(BaseCommand):
                     'MIME-Version':              '1.0',
                     'Content-Type':              'text/plain; charset=UTF-8',
                     'Content-Transfer-Encoding': '8bit',
-                    'Plural-Forms':              'nplurals=2; plural=n>1;',
                 }
 
         catalog.metadata['POT-Creation-Date'] = pytz.timezone(settings.TIME_ZONE) \
@@ -105,36 +175,40 @@ class Command(BaseCommand):
         terms = [smart_unicode(arg) for arg in args]
         entry_count = 0
 
-        for app_config in apps.get_app_configs():
-            basepath = join(app_config.path, 'locale', language, 'LC_MESSAGES')
+        for app_pofile in self._iter_pofiles(language, polib, file_name):
+            plural_forms = app_pofile.metadata.get('Plural-Forms')
+            if plural_forms:
+                all_plural_forms.add(plural_forms)
 
-            if exists(basepath):
-                for fname in listdir(basepath):
-                    if fname.endswith('.po'):
-                        path = join(basepath, fname)
+            for entry in app_pofile.translated_entries():
+                entry_count += 1
+                msgid = entry.msgid
+                existing_entry = catalog_entries.get(msgid)
 
-                        for entry in pofile(path).translated_entries():
-                            entry_count += 1
-                            msgid = entry.msgid
+                # TODO: manage context (key=msgid + context ?)
+                if existing_entry is not None:
+                    if existing_entry.obsolete:  # Entry has not been updated yet
+                        existing_entry.obsolete = False
+                        existing_entry.occurrences = entry.occurrences
+                    else:
+                        existing_entry.occurrences += entry.occurrences
+                else:
+                    for term in terms:
+                        if term in msgid:  # TODO: what about case sensitivity ?
+                            entry.flags.append('fuzzy')
+                            catalog.append(entry)
+                            catalog_entries[entry.msgid] = entry
+                            break
 
-                            existing_entry = catalog_entries.get(msgid)
+        if not catalog.fpath and all_plural_forms:  # Creation of the file
+            if len(all_plural_forms) > 1:
+                self.stderr.write('Different information about plural forms were found (first one used):%s' %
+                                  ''.join('\n - %s' % i for i in all_plural_forms)
+                                 )
 
-                            # TODO: manage context (key=msgid + context ?)
-                            if existing_entry is not None:
-                                if existing_entry.obsolete:  # Entry has not been updated yet
-                                    existing_entry.obsolete = False
-                                    existing_entry.occurrences = entry.occurrences
-                                else:
-                                    existing_entry.occurrences += entry.occurrences
-                            else:
-                                for term in terms:
-                                    if term in msgid:  # TODO: what about case sensitivity ?
-                                        entry.flags.append('fuzzy')
-                                        catalog.append(entry)
-                                        catalog_entries[entry.msgid] = entry
-                                        break
+            catalog.metadata['Plural-Forms'] = next(iter(all_plural_forms))
 
         catalog.save(catalog_path)
 
         if verbosity >= 1:
-            self.stdout.write('Number of examinated entries: %s' % entry_count)
+            self.stdout.write('Number of examined entries: %s' % entry_count)
