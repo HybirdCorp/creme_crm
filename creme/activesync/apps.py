b5bc4ebde3d0d113f377d7cab4a2de6bf434c645
Guillaume Englert <genglert at hybird.org@localhost>
2016-07-07 12:36:31 +0200

    refs #28 The app "activesync" is deprecated. It will probably be removed in the next release if nobody works on it.

--- creme/activesync/apps.py
+++ creme/activesync/apps.py
@@ -18,15 +18,28 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+from django.core import checks
 from django.utils.translation import ugettext_lazy as _
 
 from creme.creme_core.apps import CremeAppConfig
+from creme.creme_core.checks import Tags
 
 
 class ActivesyncConfig(CremeAppConfig):
     name = 'creme.activesync'
     verbose_name = _(u'Mobile synchronization')
     dependencies = ['creme.persons', 'creme.activities']
+
+    def ready(self):
+        def deprecate(**kwargs):
+            return [checks.Warning('The app "activesync" is deprecated.',
+                                    hint='It will probably be removed in the next release if nobody works on it.',
+                                    obj='activesync',
+                                    id='creme.activesync.E001',
+                                   ),
+                   ]
+
+        checks.register(Tags.settings)(deprecate)
 
     def all_apps_ready(self):
         super(ActivesyncConfig, self).all_apps_ready()
