6203ef20f0921818f79f855a7a4bac898c4ce2ef
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2016-08-11 11:55:17 +0200

    refs #91 Town.country/zipcode are not nullable anymore.
    API breaking:
     - Town.country & Town.zipcode are not 'null'.

---
+++ creme/geolocation/migrations/0003_v1_7__charfields_not_null_1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+# NB: it seems PostGreSQL does like that we migrate CharFields directly
+#     (ie: only '0004_v1_7__charfields_not_null_2.py')
+
+TOWN_FIELDS = ['country', 'zipcode']
+
+
+def fill_none_strings(apps, schema_editor):
+    manager = apps.get_model('geolocation', 'Town').objects
+
+    for field_name in TOWN_FIELDS:
+        manager.filter(**{field_name: None}).update(**{field_name: ''})
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('geolocation', '0002_v1_6__fix_block_ids'),
+    ]
+
+    operations = [
+        migrations.RunPython(fill_none_strings),
+    ]
