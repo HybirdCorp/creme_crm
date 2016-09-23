33c9206374db981adbab232d183933d51068a12e
Guillaume Englert <genglert at hybird.org@localhost>
2016-09-23 15:34:02 +0200

    refs #30 #46 The UserRole forms are now wizards.
    API breaking:
     - creme_config/role/add/ is now deprecated. Use creme_config/role/wizard view instead.
     - creme_config/role/edit/{{role.id}} is now deprecated. Use creme_config/role/wizard//{{role.id}} view instead.
     - UserRoleCreateForm & UserRoleEditForm are now deprecated. Use the wizard instead.

--- creme/creme_config/views/user_role.py
+++ creme/creme_config/views/user_role.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2015  Hybird
+#    Copyright (C) 2009-2016  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,31 +18,146 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+import warnings
+
+from django.db.transaction import atomic
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.utils.translation import ugettext_lazy as _
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext_lazy as _, ugettext
 
-from creme.creme_core.auth.decorators import login_required, superuser_required # permission_required
+from formtools.wizard.views import SessionWizardView
+
+from creme.creme_core.auth.decorators import login_required, superuser_required, _check_superuser
 from creme.creme_core.models import UserRole, SetCredentials
 from creme.creme_core.utils import get_from_POST_or_404
 from creme.creme_core.views.generic import add_model_with_popup, edit_model_with_popup, inner_popup
 from creme.creme_core.views.decorators import POST_only
+from creme.creme_core.views.generic.wizard import PopupWizardMixin
 
 from ..forms.user_role import (UserRoleCreateForm, UserRoleEditForm,
         AddCredentialsForm, UserRoleDeleteForm)
+from ..forms import user_role as user_role_forms
 
 
 @login_required
 @superuser_required
 def add(request):
+    warnings.warn("creme_config/role/add/ is now deprecated. "
+                  "Use creme_config/role/wizard view instead.",
+                  DeprecationWarning
+                 )
+
     return add_model_with_popup(request, UserRoleCreateForm, _(u'New role'),
                                 submit_label=_('Save the role'),
                                )
 
+
+class UserRoleCreationWizard(PopupWizardMixin, SessionWizardView):
+    class _CredentialsStep(user_role_forms.UserRoleCredentialsStep):
+        step_submit_label = _('Save the role')
+
+    form_list = (user_role_forms.UserRoleAppsStep,
+                 user_role_forms.UserRoleAdminAppsStep,
+                 user_role_forms.UserRoleCreatableCTypesStep,
+                 user_role_forms.UserRoleExportableCTypesStep,
+                 _CredentialsStep,
+                )
+    wizard_title = _(u'New role')
+    # permission = 'creme_core.can_admin'  # TODO: 'superuser' perm ??
+
+    def __init__(self, *args, **kwargs):
+        SessionWizardView.__init__(self, **kwargs)
+        self.role = UserRole()
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        _check_superuser(self.request.user)   # TODO: set public ?
+
+        return super(UserRoleCreationWizard, self).dispatch(*args, **kwargs)
+
+    def done(self, form_list, **kwargs):
+        with atomic():
+            form_list[0].partial_save()
+            form_list[1].save()
+
+            form_list[2].save()
+            form_list[3].save()
+
+            form_list[4].save()
+
+        return HttpResponse('', content_type='text/javascript')
+
+    def get_form_instance(self, step):
+        if step in ('0', '1', '2', '3'):
+            return self.role
+
+    def get_form_kwargs(self, step):
+        kwargs = super(UserRoleCreationWizard, self).get_form_kwargs(step)
+
+        if step != '0':
+            kwargs['allowed_app_names'] = self.get_cleaned_data_for_step('0')['allowed_apps']
+
+        if step == '4':
+            kwargs['role'] = self.role
+
+        return kwargs
+
+
+class UserRoleEditionWizard(PopupWizardMixin, SessionWizardView):
+    class _ExportableCTypesStep(user_role_forms.UserRoleExportableCTypesStep):
+        step_submit_label = _('Save the modifications')
+
+    form_list = (user_role_forms.UserRoleAppsStep,
+                 user_role_forms.UserRoleAdminAppsStep,
+                 user_role_forms.UserRoleCreatableCTypesStep,
+                 _ExportableCTypesStep,
+                )
+
+    wizard_title = u'Edit role'  # Overloaded in dispatch()
+    # permission = 'creme_core.can_admin'  # TODO: 'superuser' perm ??
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        _check_superuser(self.request.user)  # TODO: set public ?
+
+        self.role = role = get_object_or_404(UserRole, pk=kwargs['role_id'])
+        self.wizard_title = ugettext(u'Edit «%s»') % role
+
+        return super(UserRoleEditionWizard, self).dispatch(*args, **kwargs)
+
+    def done(self, form_list, **kwargs):
+        with atomic():
+            form_list[0].partial_save()
+            form_list[1].save()
+
+            form_list[2].save()
+            form_list[3].save()
+
+        return HttpResponse('', content_type='text/javascript')
+
+    def get_form_instance(self, step):
+        return self.role
+
+    def get_form_kwargs(self, step):
+        kwargs = super(UserRoleEditionWizard, self).get_form_kwargs(step)
+
+        if step != '0':
+            kwargs['allowed_app_names'] = self.get_cleaned_data_for_step('0')['allowed_apps']
+
+        return kwargs
+
+
 @login_required
 @superuser_required
 def edit(request, role_id):
+    warnings.warn("creme_config/role/edit/{{role.id}} is now deprecated. "
+                  "Use creme_config/role/wizard/{{role.id}} view instead.",
+                  DeprecationWarning
+                 )
+
     return edit_model_with_popup(request, {'pk': role_id}, UserRole, UserRoleEditForm)
+
 
 @login_required
 @superuser_required
@@ -59,13 +174,14 @@ def add_credentials(request, role_id):
 
     return inner_popup(request, 'creme_core/generics/blockform/edit_popup.html',
                        {'form':  add_form,
-                        'title': _(u'Add creddentials to «%s»') % role,
-                        'submit_label': _('Add the creddentials'),
+                        'title': _(u'Add credentials to «%s»') % role,
+                        'submit_label': _('Add the credentials'),
                        },
                        is_valid=add_form.is_valid(),
                        reload=False,
                        delegate_reload=True,
                       )
+
 
 @login_required
 @superuser_required
@@ -74,10 +190,11 @@ def delete_credentials(request):
     get_object_or_404(SetCredentials, pk=get_from_POST_or_404(request.POST, 'id')).delete()
     return HttpResponse()
 
+
 @login_required
-#@permission_required('creme_config')
 def portal(request):
     return render(request, 'creme_config/user_role_portal.html')
+
 
 @login_required
 @superuser_required
