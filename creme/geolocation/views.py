f83dbe5d3a0a9072485652251847b1acefc9a2c9
Guillaume Englert <genglert at hybird.org@localhost>
2017-02-23 10:04:51 +0100

    refs #91 Use named URLs in 'geolocation'.
    API breaking:
     - geolocation.views.set_address_info(): the URL argument "address_id" is deprecated ; use the GET parameter "id" instead.
     - geolocation.views.get_addresses_from_filter(): the URL argument "filter_id" is deprecated ; use the GET parameter "id" instead.
     - geolocation.views.get_neighbours(): the URL arguments "address_id"/"filter_id" are deprecated ; use GET parameters instead.
     - JavaScript:
      - creme.geolocation.AddressesBlock(): hard-coded "addresses_url"/"set_address_info_url" are deprecated ; give them as the second/third parameters.
      - creme.geolocation.PersonsNeighborhoodBlock(): hard-coded "neighbours_url"/"set_address_info_url" are deprecated ; give them as third/fourth parameters.
      - creme.geolocation.GoogleMapController(): hard-coded "set_address_info_url" is deprecated ; give it as parameter.
      - creme.geolocation.PersonsBlock(): hard-coded "set_address_info_url" is deprecated ; give it as parameter.
     - (internal) tests.test_views.SetAddressInfoTestCase.SET_ADDRESS_URL is not a format string any more (& build_set_address_url() is deprecated).
     - (internal) tests.test_views.GetAddressesTestCase.GET_ADDRESSES_URL is not a format string any more (& build_get_addresses_url() is deprecated).
     - (internal) tests.test_views.GetNeighboursTestCase.GET_NEIGHBOURS_URL is not a format string any more (& build_get_neighbours_url is deprecated).

--- creme/geolocation/views.py
+++ creme/geolocation/views.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2014-2016  Hybird
+#    Copyright (C) 2014-2017  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -20,6 +20,7 @@
 
 from functools import partial
 from itertools import chain
+import warnings
 
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse
@@ -27,7 +28,7 @@ from django.shortcuts import get_object_or_404
 
 from creme.creme_core.auth.decorators import permission_required, login_required
 from creme.creme_core.models import EntityFilter
-from creme.creme_core.utils import jsonify, get_from_POST_or_404
+from creme.creme_core.utils import jsonify, get_from_GET_or_404, get_from_POST_or_404
 from creme.creme_core.views.decorators import POST_only
 
 from creme.persons import get_contact_model, get_organisation_model, get_address_model
@@ -42,9 +43,19 @@ from .utils import address_as_dict, addresses_from_persons, get_radius  # get_se
 @login_required
 @permission_required('persons')
 @POST_only
-def set_address_info(request, address_id):
+# def set_address_info(request, address_id):
+def set_address_info(request, address_id=None):
     get = partial(get_from_POST_or_404, request.POST)
-    address = get_object_or_404(get_address_model(), pk=address_id)
+
+    if address_id is None:
+        address_id = get('id', int)
+    else:
+        warnings.warn('geolocation.views.set_address_info(): the URL argument "address_id" is deprecated ; '
+                      'use the GET parameter "id" instead.',
+                      DeprecationWarning
+                     )
+
+    address = get_object_or_404(get_address_model(), id=address_id)
 
     request.user.has_perm_to_change_or_die(address.owner)
 
@@ -65,11 +76,20 @@ def set_address_info(request, address_id):
 @login_required
 @permission_required('persons')
 @jsonify
-def get_addresses_from_filter(request, filter_id):
-    user = request.user
+# def get_addresses_from_filter(request, filter_id):
+def get_addresses_from_filter(request, filter_id=None):  # TODO: rename 'get_addresses()'
+    if filter_id is None:
+        filter_id = request.GET.get('id')
+    else:
+        warnings.warn('geolocation.views.get_addresses_from_filter(): the URL argument "filter_id" is deprecated ; '
+                      'use the GET parameter "id" instead.',
+                      DeprecationWarning
+                     )
+
     entity_filter = get_object_or_404(EntityFilter, pk=filter_id) if filter_id else None
-#    owner_groups = (Contact.objects, Organisation.objects,)
     owner_groups = (get_contact_model().objects, get_organisation_model().objects,)
+
+    user = request.user
 
     if entity_filter:
         model = entity_filter.entity_type.model_class()
@@ -85,14 +105,32 @@ def get_addresses_from_filter(request, filter_id):
 @login_required
 @permission_required('persons')
 @jsonify
-def get_neighbours(request, address_id, filter_id):
-    user = request.user
-    source = get_object_or_404(get_address_model(), pk=address_id)
+# def get_neighbours(request, address_id, filter_id):
+def get_neighbours(request, address_id=None, filter_id=None):
+    GET = request.GET
+
+    if address_id is None:
+        address_id = get_from_GET_or_404(GET, 'address_id', int)
+    else:
+        warnings.warn('geolocation.views.get_neighbours(): the URL argument "address_id" is deprecated ; '
+                      'use the GET parameter instead.',
+                      DeprecationWarning
+                     )
+
+    if filter_id is None:
+        filter_id = get_from_GET_or_404(GET, 'filter_id', default=None)
+    else:
+        warnings.warn('geolocation.views.get_neighbours(): the URL argument "filter_id" is deprecated ; '
+                      'use the GET parameter instead.',
+                      DeprecationWarning
+                     )
+
+    source = get_object_or_404(get_address_model(), id=address_id)
     # distance = get_setting(NEIGHBOURHOOD_DISTANCE, DEFAULT_SEPARATING_NEIGHBOURS)
     distance = get_radius()
-    entity_filter = get_object_or_404(EntityFilter, pk=filter_id) if filter_id else None
+    entity_filter = get_object_or_404(EntityFilter, id=filter_id) if filter_id else None
 
-    query_distance = request.GET.get('distance', '')
+    query_distance = GET.get('distance', '')
 
     if query_distance.isdigit():
         distance = float(query_distance)
@@ -112,8 +150,8 @@ def get_neighbours(request, address_id, filter_id):
                                        address__object_id__in=owner_ids,
                                       )
 
-    # filter credentials
-    has_perm = user.has_perm_to_view
+    # Filter credentials
+    has_perm = request.user.has_perm_to_view
     addresses = [address_as_dict(neighbor.address)
                     for neighbor in neighbours if has_perm(neighbor.address.owner)
                 ]
