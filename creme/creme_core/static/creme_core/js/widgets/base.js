b23381b58b319ed6160c459d2b950cdc28360751
Florian Fabre <ffabre@hybird.org>
2017-09-08 16:49:07 +0200

    refs #36 Fix widget base tools:  - Fix linter errors using rules in .eslintrc file.  - Improve unit tests coverage.

--- creme/creme_core/static/creme_core/js/widgets/base.js
+++ creme/creme_core/static/creme_core/js/widgets/base.js
@@ -16,7 +16,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
 
-(function($) {"use strict";
+(function($) {
+"use strict";
 
 creme.object = {
     isnone: function(obj) {
@@ -26,76 +27,53 @@ creme.object = {
 
     isempty: function(obj) {
         console.warn('creme.utils.isempty is deprecated ; use Object.isEmpty() instead.');
-        return this.isnone(obj) || obj.length === 0 || (obj !== 0 && $.isEmptyObject(obj))
+        return this.isnone(obj) || obj.length === 0 || (obj !== 0 && $.isEmptyObject(obj));
     },
 
-    invoke: function()
-    {
-        if (arguments.length == 0)
+    invoke: function() {
+        if (arguments.length === 0) {
             return;
+        }
 
         var cb = arguments[0];
 
-        if (typeof cb !== 'function')
-            return;
-
-        var args = [];
-
-        for(var i = 1; i < arguments.length; ++i)
-            args.push(arguments[i]);
-
-        return cb.apply(this, args);
+        if (Object.isFunc(cb)) {
+            return cb.apply(this, Array.copy(arguments, 1));
+        }
     },
 
-    delegate: function()
-    {
-        if (arguments.length < 2)
+    delegate: function() {
+        if (arguments.length < 2) {
             return;
+        }
 
         var delegate = arguments[0];
         var cb = arguments[1];
-        var args = [];
 
-        if (this.isempty(delegate) || typeof delegate !== 'object')
+        if (Object.isEmpty(delegate) || typeof delegate !== 'object') {
             return;
-
-        if (typeof cb === 'string')
-            cb = delegate[cb];
-
-        if (typeof cb !== 'function')
-            return;
-
-        for(var i = 2; i < arguments.length; ++i)
-            args.push(arguments[i]);
-
-        return cb.apply(delegate, args);
-    },
-
-    build_callback: function(script, parameters)
-    {
-        return creme.utils.lambda(script, parameters);
-        /*
-        if (typeof script === 'function')
-            return script;
-
-        var parameters = parameters || [];
-
-        try {
-            var script = script ? 'cb = function(' + parameters.join(',') + ') {' + (script.indexOf('return') === -1 ? 'return ' : '') + script + '};' : undefined;
-            return script ? eval(script) : undefined;
-        } catch(e) {
-            throw Error('Invalid callback script : ' + e);
         }
-        */
+
+        if (Object.isString(cb)) {
+            cb = delegate[cb];
+        }
+
+        if (Object.isFunc(cb)) {
+            return cb.apply(delegate, Array.copy(arguments, 2));
+        }
     },
 
-    deferred_start: function(element, name, func, delay)
-    {
+    build_callback: function(script, parameters) {
+        return creme.utils.lambda(script, parameters);
+    },
+
+    deferred_start: function(element, name, func, delay) {
         var key = 'deferred__' + name;
         var previous = element.data(key);
 
-        if (previous !== undefined)
+        if (previous !== undefined) {
             previous.reject();
+        }
 
         var deferred = $.Deferred();
 
@@ -107,7 +85,9 @@ creme.object = {
         element.data(key, deferred);
 
         if (delay) {
-            window.setTimeout(function() {deferred.resolve();}, delay);
+            window.setTimeout(function() {
+                deferred.resolve();
+            }, delay);
         } else {
             deferred.resolve();
         }
@@ -115,8 +95,7 @@ creme.object = {
         return deferred;
     },
 
-    deferred_cancel: function(element, name)
-    {
+    deferred_cancel: function(element, name) {
         var key = 'deferred__' + name;
         var previous = element.data(key);
 
@@ -129,7 +108,10 @@ creme.object = {
     },
 
     uuid: function() {
-        return $.uidGen({prefix: 'jqplot_target_', mode:'random'})
+        return $.uidGen({
+            prefix: 'jqplot_target_',
+            mode: 'random'
+        });
     },
 
     isFalse: function(value) {
@@ -145,8 +127,8 @@ creme.widget = {};
 
 creme.widget.Widget = function() {
     return {
-        options:{},
-        arguments:{},
+        options: {},
+        arguments: {},
 
         _create: function(element, options, cb, sync) {},
         _destroy: function(element) {},
@@ -159,10 +141,10 @@ creme.widget.Widget = function() {
             return creme.widget.is_valid(element);
         },
 
-        cleanedval: function(element)
-        {
-            if (!this.isActive(element))
+        cleanedval: function(element) {
+            if (!this.isActive(element)) {
                 return null;
+            }
 
             var value = element.creme().widget().val();
             return creme.widget.cleanval(value, value);
@@ -171,7 +153,7 @@ creme.widget.Widget = function() {
 };
 
 $.extend(creme.widget, {
-    _widgets : {},
+    _widgets: {},
 
     ready: function(root) {
         var self = creme.widget;
@@ -181,8 +163,7 @@ $.extend(creme.widget, {
         });
     },
 
-    shutdown: function(root)
-    {
+    shutdown: function(root) {
         var self = creme.widget;
 
         $('.ui-creme-widget.widget-ready', root).each(function() {
@@ -195,34 +176,36 @@ $.extend(creme.widget, {
     },
 
     unregister: function(name) {
-        if (creme.widget._widgets[name] !== undefined)
+        if (creme.widget._widgets[name] !== undefined) {
             delete creme.widget._widgets[name];
+        }
     },
 
     find: function(name) {
         return creme.widget._widgets[name];
     },
 
-    proxy: function(element, delegate)
-    {
-        if (Object.isNone(delegate))
+    proxy: function(element, delegate) {
+        if (Object.isNone(delegate)) {
             return;
+        }
 
         var widget = {
-           element: element,
-           delegate: delegate,
-           options: function() {
-               return this.delegate.options;
-           },
-           arguments: function() {
-               return this.delegate.arguments;
-           }
+            element: element,
+            delegate: delegate,
+            options: function() {
+                return this.delegate.options;
+            },
+            arguments: function() {
+                return this.delegate.arguments;
+            }
         };
 
         (function(widget) {
             $.each(widget.delegate, function(key, value) {
-                if (typeof value !== 'function' || key.match('^_.*') !== null)
+                if (typeof value !== 'function' || key.match('^_.*') !== null) {
                     return;
+                }
 
                 widget[key] = function() {
                     return value.apply(widget.delegate, [widget.element].concat(Array.copy(arguments)));
@@ -233,19 +216,20 @@ $.extend(creme.widget, {
         return widget;
     },
 
-    create: function(element, options, cb, sync)
-    {
-        if (element.data('CremeWidget') !== undefined)
+    create: function(element, options, cb, sync) {
+        if (element.data('CremeWidget') !== undefined) {
             return;
+        }
 
         var widget = creme.widget.find(element.attr('widget'));
 
-        if (typeof widget !== 'object')
+        if (typeof widget !== 'object') {
             return;
+        }
 
         var delegate = $.extend({}, widget);
         delegate.options = creme.widget.parseopt(element, widget.options, options);
-        delegate.arguments = creme.widget.parseattr(element, $.extend({'widget':'', 'class':''}, delegate.options));
+        delegate.arguments = creme.widget.parseattr(element, $.extend({'widget': '', 'class': ''}, delegate.options));
 
         var proxy = creme.widget.proxy(element, delegate);
 
@@ -257,10 +241,10 @@ $.extend(creme.widget, {
         return proxy;
     },
 
-    destroy: function(element)
-    {
-        if (!creme.widget.is_valid(element))
+    destroy: function(element) {
+        if (!creme.widget.is_valid(element)) {
             return element;
+        }
 
         element.data('CremeWidget').delegate._destroy(element);
         element.removeData('CremeWidget');
@@ -269,89 +253,93 @@ $.extend(creme.widget, {
         return element;
     },
 
-    declare: function(name, object, parent)
-    {
+    declare: function(name, object, parent) {
         var widget = $.extend(new creme.widget.Widget(), parent, object);
         creme.widget.register(name, widget);
         return widget;
     },
 
-    template: function(template, values)
-    {
-        if (template === undefined || values === undefined)
+    // TODO : remove it and replace it by creme.utils.template or String.template
+    template: function(template, values) {
+        if (template === undefined || values === undefined) {
             return template;
+        }
 
         var entries = template.match(/\$\{[\w\d]+\}/g);
 
-        if (entries === null)
+        if (entries === null) {
             return template;
+        }
 
-        var res = new String(template);
+        var res = '' + template;
+        var getter;
 
-        if (typeof values !== 'function')
-            var getter = function(key) {return values[key];}
-        else
-            var getter = values;
+        if (typeof values !== 'function') {
+            getter = function(key) { return values[key]; };
+        } else {
+            getter = values;
+        }
 
-        for(var i = 0; i < entries.length; i++)
-        {
+        for (var i = 0; i < entries.length; i++) {
             var entry = entries[i];
             var key = entry.slice(2, -1);
             var value = getter(key);
 
-            if (value !== undefined)
+            if (value !== undefined) {
                 res = res.replace(entry, getter(key));
+            }
         }
 
         return res;
     },
 
-    parseattr: function(element, excludes)
-    {
+    parseattr: function(element, excludes) {
         var attributes = {};
+        var index;
 
-        if (element.length === 0)
+        if (element.length === 0) {
             return attributes;
+        }
 
-        for(var index = 0; index < element[0].attributes.length; ++index)
-        {
+        for (index = 0; index < element[0].attributes.length; ++index) {
             var attr = element[0].attributes[index];
             attributes[attr.name] = attr.value;
         }
 
-        var excludes = excludes || [];
+        excludes = excludes || [];
 
-        if ($.isArray(excludes))
-        {
-            for(var index in excludes) {
-                if (attributes[excludes[index]] !== undefined)
+        if ($.isArray(excludes)) {
+            for (index in excludes) {
+                if (attributes[excludes[index]] !== undefined) {
                     delete attributes[excludes[index]];
+                }
             }
         } else {
-            for(var exclude in excludes) {
-                if (attributes[exclude] !== undefined)
+            for (var exclude in excludes) {
+                if (attributes[exclude] !== undefined) {
                     delete attributes[exclude];
+                }
             }
         }
 
-        //console.log('parseattr > attributes:', attributes, ', excludes:', $.isArray(excludes) ? excludes : Object.keys(excludes));
+        // console.log('parseattr > attributes:', attributes, ', excludes:', $.isArray(excludes) ? excludes : Object.keys(excludes));
         return attributes;
     },
 
-    parseopt: function(element, defaults, options)
-    {
+    parseopt: function(element, defaults, options) {
         var opts = {};
 
         Object.keys(defaults || {}).forEach(function(name) {
             var value = element.attr(name);
 
-            if (value !== undefined)
+            if (value !== undefined) {
                 opts[name] = element.attr(name);
+            }
         });
 
         opts = $.extend({}, defaults, opts, options);
 
-        //console.log('parseopt > options:', opts, ', defaults:', defaults, ', attributes:', attributes);
+        // console.log('parseopt > options:', opts, ', defaults:', defaults, ', attributes:', attributes);
         return opts;
     },
 
@@ -363,29 +351,27 @@ $.extend(creme.widget, {
         return value;
     },
 
-    cleanval: function(value, default_value, parser) {
+    cleanval: function(value, defaultval, parser) {
         var cleanparser = (parser !== undefined) ? parser : creme.ajax.json.parse;
         var result = (typeof value === 'object') ? value : creme.widget.parseval(value, cleanparser);
 
-        return (result !== null && result !== undefined) ? result : default_value;
+        return (result !== null && result !== undefined) ? result : defaultval;
     },
 
-    val: function(element, value)
-    {
+    val: function(element, value) {
         var widget = $(element).creme().widget() || $(element);
 
-        if (value === undefined)
+        if (value === undefined) {
             return widget.val();
+        }
 
         return widget.val(value);
     },
 
-    values_list: function(elements, data, parser)
-    {
+    values_list: function(elements, data, parser) {
         var values;
 
-        if (data === undefined)
-        {
+        if (data === undefined) {
             values = [];
 
             elements.each(function() {
@@ -395,8 +381,7 @@ $.extend(creme.widget, {
             return values;
         }
 
-        var values = creme.widget.parseval(data, parser);
-
+        values = creme.widget.parseval(data, parser);
         values = (values === null) ? '' : values;
         values = (!$.isArray(values)) ? [values] : values;
 
@@ -406,49 +391,49 @@ $.extend(creme.widget, {
         });
     },
 
-    input: function(element)
-    {
+    input: function(element) {
         var query = 'input.ui-creme-input.' + $(element).attr('widget');
         var inputs = $('input.ui-creme-input.' + $(element).attr('widget'), element);
 
-        if ($(element).is(query))
+        if ($(element).is(query)) {
             inputs.push(element);
+        }
 
         return inputs;
     },
 
-    is_valid: function(element)
-    {
-        if (Object.isEmpty(element))
+    is_valid: function(element) {
+        if (Object.isEmpty(element)) {
             return false;
+        }
 
         return Object.isFunc(element.data) && !Object.isNone(element.data('CremeWidget'));
     },
 
-    writeAttr: function(element, options)
-    {
-        for(var key in options)
-        {
+    writeAttr: function(element, options) {
+        for (var key in options) {
             var value = options[key];
 
-            if (value !== undefined)
-                element.attr(key, value)
+            if (value !== undefined) {
+                element.attr(key, value);
+            }
         }
 
         return element;
     },
 
-    buildTag: function(element, widget, options, auto)
-    {
+    buildTag: function(element, widget, options, auto) {
         element.addClass('ui-creme-widget')
                .addClass(widget)
                .attr('widget', widget);
 
-        if (auto === true)
+        if (auto === true) {
             element.addClass('widget-auto');
+        }
 
-        if (Object.isEmpty(options))
+        if (Object.isEmpty(options)) {
             return element;
+        }
 
         return this.writeAttr(element, options);
     }
@@ -473,12 +458,10 @@ $.fn.creme = function() {
         isActive: function() {
             return creme.widget.is_valid(self);
         }
-    }
+    };
 };
-
 
 $(document).ready(function() {
     creme.widget.ready();
 });
-
 }(jQuery));
