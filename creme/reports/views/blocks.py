7cdaa6fcc45895440e08e66290a5e484adaaa5e9
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2017-12-12 14:31:46 +0100

    refs #13 #106 Some module renaming.
    API breaking:
     - reports.forms.blocks is deprecated ; use reports.forms.bricks instead.
     - reports.views.blocks is deprecated ; use reports.views.bricks instead.

--- creme/reports/views/blocks.py
+++ creme/reports/views/blocks.py
@@ -1,55 +1,5 @@
-# -*- coding: utf-8 -*-
+import warnings
 
-################################################################################
-#    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2016  Hybird
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+from .bricks import add_graph_instance_brick as add_graph_instance_block
 
-from django.shortcuts import get_object_or_404
-from django.utils.translation import ugettext_lazy as _
-
-from creme.creme_core.auth.decorators import login_required, permission_required
-from creme.creme_core.views.generic.popup import inner_popup
-
-from .. import get_rgraph_model
-from ..forms.blocks import GraphInstanceBlockForm
-
-
-# TODO: use add_to_entity() generic view => Post
-#       => Problem: it doesn't fit to the needs (credential admin instead of change/link)
-
-@login_required
-@permission_required(('reports', 'reports.can_admin'))
-def add_graph_instance_block(request, graph_id):
-    graph = get_object_or_404(get_rgraph_model(), pk=graph_id)
-
-    if request.method == 'POST':
-        graph_form = GraphInstanceBlockForm(graph=graph, user=request.user, data=request.POST)
-
-        if graph_form.is_valid():
-            graph_form.save()
-    else:
-        graph_form = GraphInstanceBlockForm(graph=graph, user=request.user)
-
-    return inner_popup(request, 'creme_core/generics/blockform/add_popup.html',
-                       {'form':   graph_form,
-                        'title': _(u'Create an instance block for «%s»') % graph,
-                        'submit_label': _('Save the block'),
-                       },
-                       is_valid=graph_form.is_valid(),
-                       reload=False,
-                       delegate_reload=True,
-                      )
+warnings.warn('reports.views.blocks is deprecated ; use reports.views.bricks instead.', DeprecationWarning)
