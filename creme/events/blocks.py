60099c9a6e29a5973c666c8553923663b966075f
Guillaume Englert <genglert at hybird.org@localhost>
2018-02-15 16:07:23 +0100

    refs #32 Remove deprecated code in 'events'.
    API breaking:
     - Remove the module 'blocks'.
     - Remove the template file events/block_results.html.

--- creme/events/blocks.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-from .bricks import ResutsBrick as ResutsBlock
-
-warnings.warn('events.blocks is deprecated ; use events.bricks instead.', DeprecationWarning)
-
-resuts_block = ResutsBlock()
