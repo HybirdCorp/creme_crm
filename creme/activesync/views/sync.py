c7ef74757bd242719134cd383aa0d7f4fa32b161
Guillaume Englert <genglert at hybird.org@localhost>
2018-02-15 16:07:21 +0100

    refs #28 Remove deprecated code/templates from 'activesync'.
    API breaking:
     - Remove 'activesync/templatetags/block_*.html' files.
     - Remove the files 'activesync/main_sync.html' & 'activesync/frags/ajax/main_sync.html'.
     - Remove the module 'blocks'.
     - Remove the view function views.sync.main_sync().
     - Remove the templatetags {% get_history_type_img %} & {% get_history_where_img %}.
     - Remove models.active_sync.USER_HISTORY_TYPE_IMG & USER_HISTORY_WHERE_IMG.

--- creme/activesync/views/sync.py
+++ creme/activesync/views/sync.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2017  Hybird
+#    Copyright (C) 2009-2018  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,15 +18,15 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-import warnings
+# import warnings
 
-from django.conf import settings
+# from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 
 from creme.creme_core.auth.decorators import login_required, permission_required
-from creme.creme_core.views.bricks import bricks_render_info
 from creme.creme_core.utils import jsonify
+from creme.creme_core.views.bricks import bricks_render_info
 
 from .. import bricks
 from ..errors import CremeActiveSyncError
@@ -34,50 +34,50 @@ from ..messages import MessageError, _ERROR
 from ..sync import Synchronization
 
 
-@login_required
-@permission_required('activesync')
-def main_sync(request):
-    warnings.warn('activesync.views.sync.main_sync() is deprecated ; use sync_portal() instead.',
-                  DeprecationWarning
-                 )
-
-    try:
-        sync = Synchronization(request.user)
-    except CremeActiveSyncError as e:
-        tpl_dict = {'all_messages': [(_ERROR, [MessageError(message=e)])],
-                    'fatal_error':  True,
-                   }
-    else:
-        try:
-            sync.synchronize()
-        except CremeActiveSyncError as e:
-            sync.add_error_message(e)
-
-        tpl_dict = {
-            'bricks_reload_url': reverse('activesync__sync_n_reload_bricks'),
-
-            'server_url': sync.server_url,
-            'login':      sync.login,
-            'domain':     sync.domain,
-            'server_ssl': sync.server_ssl,
-            'last_sync':  sync.last_sync,
-
-            'all_messages':   sync.messages(),
-            'sync_calendars': sync.is_user_sync_calendars,
-            'sync_contacts':  sync.is_user_sync_contacts,
-
-            # DEBUG
-            'xml':          sync._data['debug']['xml'],
-            'debug_info':   sync._data['debug']['info'],
-            'debug_errors': sync._data['debug']['errors'],
-            'ACTIVE_SYNC_DEBUG': settings.ACTIVE_SYNC_DEBUG,
-        }
-
-    if request.is_ajax():
-        # TODO: template in a var
-        return render(request, 'activesync/frags/ajax/main_sync.html', tpl_dict)
-
-    return render(request, 'activesync/main_sync.html', tpl_dict)
+# @login_required
+# @permission_required('activesync')
+# def main_sync(request):
+#     warnings.warn('activesync.views.sync.main_sync() is deprecated ; use sync_portal() instead.',
+#                   DeprecationWarning
+#                  )
+#
+#     try:
+#         sync = Synchronization(request.user)
+#     except CremeActiveSyncError as e:
+#         tpl_dict = {'all_messages': [(_ERROR, [MessageError(message=e)])],
+#                     'fatal_error':  True,
+#                    }
+#     else:
+#         try:
+#             sync.synchronize()
+#         except CremeActiveSyncError as e:
+#             sync.add_error_message(e)
+#
+#         tpl_dict = {
+#             'bricks_reload_url': reverse('activesync__sync_n_reload_bricks'),
+#
+#             'server_url': sync.server_url,
+#             'login':      sync.login,
+#             'domain':     sync.domain,
+#             'server_ssl': sync.server_ssl,
+#             'last_sync':  sync.last_sync,
+#
+#             'all_messages':   sync.messages(),
+#             'sync_calendars': sync.is_user_sync_calendars,
+#             'sync_contacts':  sync.is_user_sync_contacts,
+#
+#             # DEBUG
+#             'xml':          sync._data['debug']['xml'],
+#             'debug_info':   sync._data['debug']['info'],
+#             'debug_errors': sync._data['debug']['errors'],
+#             'ACTIVE_SYNC_DEBUG': settings.ACTIVE_SYNC_DEBUG,
+#         }
+#
+#     if request.is_ajax():
+#         # TODO: template in a var
+#         return render(request, 'activesync/frags/ajax/main_sync.html', tpl_dict)
+#
+#     return render(request, 'activesync/main_sync.html', tpl_dict)
 
 
 def _sync_n_bricks(request):
