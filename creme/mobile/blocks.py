fd982bb5eac714d8e772bc9175a9eacc15b7f176
Guillaume Englert <genglert at hybird.org@localhost>
2018-02-18 16:39:46 +0100

    refs #90 Remove deprecated code in 'mobile'.
    API breaking:
     - Remove the module 'blocks'.
     - Remove the template file mobile/templatetags/block_favorite.html

--- creme/mobile/blocks.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-from .bricks import FavoritePersonsBrick as FavoritePersonsBlock
-
-warnings.warn('mobile.blocks is deprecated ; use mobile.bricks instead.', DeprecationWarning)
-
-favorite_persons_block = FavoritePersonsBlock()
