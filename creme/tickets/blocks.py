e3e5d931003c084060e9f295c23071d321b283c0
Guillaume Englert <genglert at hybird.org@localhost>
2018-02-18 16:39:48 +0100

    refs #41 Remove deprecated code in 'tickets'.
    API breaking:
     - Remove the module 'blocks'.
     - Remove the template file tickets/templatetags/block_ticket.html

--- creme/tickets/blocks.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-from creme.creme_core.gui.block import SimpleBlock
-
-warnings.warn('tickets.blocks is deprecated ; use tickets.bricks instead.', DeprecationWarning)
-
-
-class TicketBlock(SimpleBlock):
-    template_name = 'tickets/templatetags/block_ticket.html'
