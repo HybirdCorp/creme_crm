c65acc91f1456f5810430a51dc23ee8b271b4e7e
Guillaume Englert <genglert at hybird.org@localhost>
2018-02-18 16:39:47 +0100

    refs # Remove deprecated code in 'products'.
    API breaking:
     - Remove the module 'blocks'.
     - Remove the template file products/templates/products/block_images.html
     - Remove the class forms.fields.CategorySelector

--- creme/products/blocks.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-from .bricks import ImagesBrick as ImagesBlock
-
-warnings.warn('products.blocks is deprecated ; use products.bricks instead.', DeprecationWarning)
-
-images_block = ImagesBlock()
