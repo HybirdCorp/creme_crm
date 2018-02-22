19ec4bc52c8b7eefe314f3066e0870f6b71f638f
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2018-02-22 18:20:42 +0100

    refs #100 Rework some code to be more future-proof (ie: django API evolution).

--- creme/sms/forms/recipient.py
+++ creme/sms/forms/recipient.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2016  Hybird
+#    Copyright (C) 2009-2018  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -29,8 +29,9 @@ from ..models.recipient import Recipient
 
 
 class MessagingListAddRecipientsForm(CremeForm):
-    # TODO : see for phonelist widget
-    recipients = PhoneListField(widget=Textarea(), label=_(u'Recipients'),
+    # TODO: see for phonelist widget
+    # recipients = PhoneListField(widget=Textarea(), label=_(u'Recipients'),
+    recipients = PhoneListField(widget=Textarea, label=_(u'Recipients'),
                                 help_text=_(u'One phone number per line'),
                                )
 
