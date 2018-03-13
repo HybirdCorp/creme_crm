f57b5eddb37e381d644d84bc3968ee7783c8cd2c
Florian Fabre <ffabre@hybird.org>
2018-03-13 15:51:00 +0100

    refs #30 Improve entity permission validation function error message when the user is missing or anonymous.

--- creme/creme_core/forms/validators.py
+++ creme/creme_core/forms/validators.py
@@ -20,14 +20,19 @@
 
 from django.core.exceptions import PermissionDenied
 from django.forms.utils import ValidationError
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext as _, ugettext_lazy
 
 from ..utils import entities2unicode
 
+def validate_authenticated_user(user, message, code, **kwargs):
+    if user is None or not user.is_authenticated():
+        raise ValidationError(message.format(**kwargs), code=code)
 
 # TODO: factorise
 # VIEW ----------------------
 def validate_viewable_entity(entity, user, code='viewnotallowed'):
+    validate_authenticated_user(user, ugettext_lazy(u'Not authenticated user is not allowed to view entities'), code)
+
     try:
         user.has_perm_to_view_or_die(entity)
     except PermissionDenied as e:
@@ -37,6 +42,8 @@ def validate_viewable_entity(entity, user, code='viewnotallowed'):
 
 
 def validate_viewable_entities(entities, user, code='viewnotallowed'):
+    validate_authenticated_user(user, ugettext_lazy(u'Not authenticated user is not allowed to view entities'), code)
+
     has_perm = user.has_perm_to_view
     unviewable = entities2unicode((e for e in entities if not has_perm(e)), user)
 
@@ -48,6 +55,8 @@ def validate_viewable_entities(entities, user, code='viewnotallowed'):
 
 # CHANGE ----------------------
 def validate_editable_entity(entity, user, code='changenotallowed'):
+    validate_authenticated_user(user, ugettext_lazy(u'Not authenticated user is not allowed to edit entities'), code)
+
     try:
         user.has_perm_to_change_or_die(entity)
     except PermissionDenied as e:
@@ -57,6 +66,8 @@ def validate_editable_entity(entity, user, code='changenotallowed'):
 
 
 def validate_editable_entities(entities, user, code='changenotallowed'):
+    validate_authenticated_user(user, ugettext_lazy(u'Not authenticated user is not allowed to edit entities'), code)
+
     has_perm = user.has_perm_to_change
     uneditable = entities2unicode((e for e in entities if not has_perm(e)), user)
 
@@ -68,6 +79,8 @@ def validate_editable_entities(entities, user, code='changenotallowed'):
 
 # LINK ----------------------
 def validate_linkable_entity(entity, user, code='linknotallowed'):
+    validate_authenticated_user(user, ugettext_lazy(u'Not authenticated user is not allowed to link entities'), code)
+
     try:
         user.has_perm_to_link_or_die(entity)
     except PermissionDenied as e:
@@ -77,6 +90,8 @@ def validate_linkable_entity(entity, user, code='linknotallowed'):
 
 
 def validate_linkable_entities(entities, user, code='linknotallowed'):
+    validate_authenticated_user(user, ugettext_lazy(u'Not authenticated user is not allowed to link entities'), code)
+
     has_perm = user.has_perm_to_link
     unlinkable = entities2unicode((e for e in entities if not has_perm(e)), user)
 
@@ -87,6 +102,10 @@ def validate_linkable_entities(entities, user, code='linknotallowed'):
 
 
 def validate_linkable_model(model, user, owner, code='linknotallowed'):
+    validate_authenticated_user(user, ugettext_lazy(u'Not authenticated user is not allowed to link «{model}»'),
+                                code=code,
+                                model=model._meta.verbose_name_plural)
+
     if not user.has_perm_to_link(model, owner=owner):
         raise ValidationError(_(u'You are not allowed to link with the «%s» of this user.') %
                                     model._meta.verbose_name_plural,
---
+++ creme/creme_core/tests/forms/test_validators.py
@@ -0,0 +1,414 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from django.contrib.auth import get_user
+    from django.core.exceptions import ValidationError
+    from django.utils.translation import ugettext as _
+
+    from creme.creme_core.auth.entity_credentials import EntityCredentials
+    from creme.creme_core.forms import validators
+    from creme.creme_core.models import CremeUser, FakeContact
+    from creme.creme_core.models.auth import SetCredentials
+    from creme.creme_core.tests.base import CremeTestCase
+except Exception as e:
+    print('Error in <%s>: %s' % (__name__, e))
+
+
+class CredsValidatorTestCase(CremeTestCase):
+    def _set_user_credentials(self, user, credentials, coverage):
+        SetCredentials.objects.create(role=user.role,
+                                      value=credentials,
+                                      set_type=coverage,
+                                     )
+
+    def test_validate_none_user(self):
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_authenticated_user(None, 'nobody', code='viewnotallowed')
+
+        self.assertEqual(e.exception.message, 'nobody')
+        self.assertEqual(e.exception.code, 'viewnotallowed')
+
+    def test_validate_anonymous_user(self):
+        user = get_user(self.client)
+        self.assertTrue(user.is_anonymous())
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_authenticated_user(None, 'we are legion', code='viewnotallowed')
+
+        self.assertEqual(e.exception.message, 'we are legion')
+        self.assertEqual(e.exception.code, 'viewnotallowed')
+
+    def test_validate_authenticated_user(self):
+        user = self.login(is_superuser=False)
+        with self.assertNoException():
+            validators.validate_authenticated_user(user, 'none or anonymous not allowed', code='viewnotallowed')
+
+    def test_validate_viewable_entity_owner(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=user)
+
+        # view permission set for owned entities
+        self._set_user_credentials(user, EntityCredentials.VIEW, SetCredentials.ESET_OWN)
+
+        with self.assertNoException():
+            validators.validate_viewable_entity(a, user)
+
+    def test_validate_viewable_entity_allowed(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        # view permission set for all entities
+        self._set_user_credentials(user, EntityCredentials.VIEW, SetCredentials.ESET_ALL)
+
+        with self.assertNoException():
+            validators.validate_viewable_entity(a, user)
+
+    def test_validate_viewable_entity_anonymous(self):
+        other_user = CremeUser.objects.create(username='other')
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=other_user)
+
+        user = get_user(self.client)
+        self.assertTrue(user.is_anonymous())
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_viewable_entity(a, user)
+
+        self.assertEqual(e.exception.message, _(u'Not authenticated user is not allowed to view entities'))
+        self.assertEqual(e.exception.code, 'viewnotallowed')
+
+    def test_validate_viewable_entity_notallowed_other(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.VIEW, SetCredentials.ESET_OWN)
+
+        # view permission set for owned entities
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_viewable_entity(a, user)
+
+    def test_validate_viewable_entity_notallowed_all(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.CHANGE, SetCredentials.ESET_ALL)
+
+        # view permission not set
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_viewable_entity(a, user)
+
+        self.assertEqual(e.exception.code, 'viewnotallowed')
+
+    def test_validate_viewable_entities_owner(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='A', user=user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=user)
+
+        self._set_user_credentials(user, EntityCredentials.VIEW,  SetCredentials.ESET_OWN)
+
+        with self.assertNoException():
+            validators.validate_viewable_entities([a, b], user)
+
+    def test_validate_viewable_entities_allowed(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='A', user=self.other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.VIEW,  SetCredentials.ESET_ALL)
+
+        with self.assertNoException():
+            validators.validate_viewable_entities([a, b], user)
+
+    def test_validate_viewable_entities_anonymous(self):
+        other_user = CremeUser.objects.create(username='other')
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=other_user)
+
+        user = get_user(self.client)
+        self.assertTrue(user.is_anonymous())
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_viewable_entities([a, b], user)
+
+        self.assertEqual(e.exception.code, 'viewnotallowed')
+
+    def test_validate_viewable_entities_notallowed_other(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.VIEW,  SetCredentials.ESET_OWN)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_viewable_entities([a, b], user)
+
+        self.assertEqual(e.exception.code, 'viewnotallowed')
+
+    def test_validate_viewable_entities_notallowed_all(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.CHANGE,  SetCredentials.ESET_ALL)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_viewable_entities([a, b], user)
+
+        self.assertEqual(e.exception.code, 'viewnotallowed')
+
+    def test_validate_editable_entity_owner(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=user)
+
+        self._set_user_credentials(user, EntityCredentials.CHANGE,  SetCredentials.ESET_OWN)
+
+        with self.assertNoException():
+            validators.validate_editable_entity(a, user)
+
+    def test_validate_editable_entity_allowed(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.CHANGE,  SetCredentials.ESET_ALL)
+
+        with self.assertNoException():
+            validators.validate_editable_entity(a, user)
+
+    def test_validate_editable_entity_anonymous(self):
+        other_user = CremeUser.objects.create(username='other')
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=other_user)
+
+        user = get_user(self.client)
+        self.assertTrue(user.is_anonymous())
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_editable_entity(a, user)
+
+        self.assertEqual(e.exception.message, _(u'Not authenticated user is not allowed to edit entities'))
+        self.assertEqual(e.exception.code, 'changenotallowed')
+
+    def test_validate_editable_entity_notallowed_other(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.CHANGE, SetCredentials.ESET_OWN)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_editable_entity(a, user)
+
+        self.assertEqual(e.exception.code, 'changenotallowed')
+
+    def test_validate_editable_entity_notallowed_all(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.VIEW, SetCredentials.ESET_ALL)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_editable_entity(a, user)
+
+        self.assertEqual(e.exception.code, 'changenotallowed')
+
+    def test_validate_editable_entities_owner(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='A', user=user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=user)
+
+        self._set_user_credentials(user, EntityCredentials.CHANGE, SetCredentials.ESET_OWN)
+
+        with self.assertNoException():
+            validators.validate_editable_entities([a, b], user)
+
+    def test_validate_editable_entities_anonymous(self):
+        other_user = CremeUser.objects.create(username='other')
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=other_user)
+
+        user = get_user(self.client)
+        self.assertTrue(user.is_anonymous())
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_editable_entities([a, b], user)
+
+        self.assertEqual(e.exception.code, 'changenotallowed')
+
+    def test_validate_editable_entities_notallowed_other(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.CHANGE, SetCredentials.ESET_OWN)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_editable_entities([a, b], user)
+
+        self.assertEqual(e.exception.code, 'changenotallowed')
+
+    def test_validate_editable_entities_notallowed_all(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.VIEW, SetCredentials.ESET_ALL)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_editable_entities([a, b], user)
+
+        self.assertEqual(e.exception.code, 'changenotallowed')
+
+    def test_validate_linkable_entity_owner(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=user)
+
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_OWN)
+
+        with self.assertNoException():
+            validators.validate_linkable_entity(a, user)
+
+    def test_validate_linkable_entity_allowed(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_ALL)
+
+        with self.assertNoException():
+            validators.validate_linkable_entity(a, user))
+
+    def test_validate_linkable_entity_anonymous(self):
+        other_user = CremeUser.objects.create(username='other')
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=other_user)
+
+        user = get_user(self.client)
+        self.assertTrue(user.is_anonymous())
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_entity(a, user)
+
+        self.assertEqual(e.exception.message, _(u'Not authenticated user is not allowed to link entities'))
+        self.assertEqual(e.exception.code, 'linknotallowed')
+
+    def test_validate_linkable_entity_notallowed_other(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_OWN)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_entity(a, user)
+
+        self.assertEqual(e.exception.code, 'linknotallowed')
+
+    def test_validate_linkable_entity_notallowed_all(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.VIEW, SetCredentials.ESET_ALL)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_entity(a, user)
+
+        self.assertEqual(e.exception.code, 'linknotallowed')
+
+    def test_validate_linkable_entities_owner(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='A', user=user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=user)
+
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_OWN)
+
+        with self.assertNoException():
+            validators.validate_linkable_entities([a, b], user)
+
+    def test_validate_linkable_entities_allowed(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='A', user=self.other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_ALL)
+
+        with self.assertNoException():
+            validators.validate_linkable_entities([a, b], user)
+
+    def test_validate_linkable_entities_anonymous(self):
+        other_user = CremeUser.objects.create(username='other')
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=other_user)
+
+        user = get_user(self.client)
+        self.assertTrue(user.is_anonymous())
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_entities([a, b], user)
+
+        self.assertEqual(e.exception.message, _(u'Not authenticated user is not allowed to link entities'))
+        self.assertEqual(e.exception.code, 'linknotallowed')
+
+    def test_validate_linkable_entities_notallowed_other(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_OWN)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_entities([a, b], user)
+
+        self.assertEqual(e.exception.code, 'linknotallowed')
+
+    def test_validate_linkable_entities_notallowed_all(self):
+        user = self.login(is_superuser=False)
+        a = FakeContact.objects.create(last_name='Doe', first_name='John', user=self.other_user)
+        b = FakeContact.objects.create(last_name='Doe', first_name='B', user=self.other_user)
+
+        self._set_user_credentials(user, EntityCredentials.VIEW, SetCredentials.ESET_ALL)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_entities([a, b], user)
+
+        self.assertEqual(e.exception.code, 'linknotallowed')
+
+    def test_validate_linkable_model(self):
+        user = self.login(is_superuser=False)
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_OWN)
+
+        with self.assertNoException():
+            validators.validate_linkable_model(FakeContact, user, user)
+
+    def test_validate_linkable_model_allowed(self):
+        user = self.login(is_superuser=False)
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_ALL)
+
+        with self.assertNoException():
+            validators.validate_linkable_model(FakeContact, user, self.other_user)
+
+    def test_validate_linkable_model_anonymous(self):
+        user = get_user(self.client)
+        self.assertTrue(user.is_anonymous())
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_model(FakeContact, user, user)
+
+        self.assertEqual(e.exception.message,
+                         _(u'Not authenticated user is not allowed to link «{model}»').format(
+                             model=FakeContact._meta.verbose_name_plural
+                         )
+                        )
+
+        self.assertEqual(e.exception.code, 'linknotallowed')
+
+    def test_validate_linkable_model_notallowed_other(self):
+        user = self.login(is_superuser=False)
+        self._set_user_credentials(user, EntityCredentials.LINK, SetCredentials.ESET_OWN)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_model(FakeContact, user, self.other_user)
+
+        self.assertEqual(e.exception.code, 'linknotallowed')
+
+    def test_validate_linkable_model_notallowed_all(self):
+        user = self.login(is_superuser=False)
+        self._set_user_credentials(user, EntityCredentials.VIEW, SetCredentials.ESET_ALL)
+
+        with self.assertRaises(ValidationError) as e:
+            validators.validate_linkable_model(FakeContact, user, user)
+
+        self.assertEqual(e.exception.code, 'linknotallowed')
