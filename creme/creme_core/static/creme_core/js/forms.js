8d94119d007f502d857038693ab9282f2d310a29
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2018-04-13 13:43:14 +0200

    refs #44 The checkbox to create new CustomField values is hidden when no column is selected (as in regular fields widgets).
    API breaking:
     - The JS function creme.forms.toImportField() takes now a second argument (the query to get the element to hide).

--- creme/creme_core/static/creme_core/js/forms.js
+++ creme/creme_core/static/creme_core/js/forms.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
     Creme is a free/open-source Customer Relationship Management software
-    Copyright (C) 2009-2015  Hybird
+    Copyright (C) 2009-2018  Hybird
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -513,10 +513,12 @@ creme.forms.toOrderedMultiSelect = function(table_id, reduced) {
     creme.forms._toDualColumnMultiSelect(table_id, true, buildColumns, refreshTable, reduced);
 };
 
-creme.forms.toImportField = function(table_id) {
+//creme.forms.toImportField = function(table_id) {
+creme.forms.toImportField = function(table_id, target_query) {
     var $table = $('#' + table_id);
     var $csv_select    = $table.find('.csv_col_select');
-    var $fields_select = $table.find('.csv_subfields_select');
+//    var $fields_select = $table.find('.csv_subfields_select');
+    var $fields_select = $table.find(target_query);
 
     function not_in_csv() {
         return $csv_select.find('[value="0"]').attr('selected');
