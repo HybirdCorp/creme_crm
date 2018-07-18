932ad067e2eb115eb8b13f7aa78e5c14b58c7c3b
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2018-07-18 10:03:15 +0200

    refs #77 Use the new "default" argument of 'max()'.

--- creme/creme_core/utils/id_generator.py
+++ creme/creme_core/utils/id_generator.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2015  Hybird
+#    Copyright (C) 2009-2018  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -45,7 +45,8 @@ def generate_string_id_and_save(model, objects, prefix):
               ]
     # TODO: do-able in SQL ????
     # TODO: would it be cool to fill the 'holes' in id ranges ???
-    index = max(id_list) if id_list else 0
+    # index = max(id_list) if id_list else 0
+    index = max(id_list, default=0)
     last_exception = None
 
     for obj in objects:
