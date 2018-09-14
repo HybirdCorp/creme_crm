5205af1bbe72d42a1d5c27d8da2a649a6de61933
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2018-09-14 16:35:24 +0200

    refs #23 Avoid the creation of duplicated Relations with the buttons "Transform into a ...".

--- creme/persons/views/crud_relations.py
+++ creme/persons/views/crud_relations.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2017  Hybird
+#    Copyright (C) 2009-2018  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,6 +18,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+import logging
+
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 
@@ -26,6 +28,8 @@ from creme.creme_core.models import CremeEntity, Relation, RelationType
 from creme.creme_core.utils import get_from_POST_or_404
 
 from .. import get_organisation_model, constants
+
+logger = logging.getLogger(__name__)
 
 
 # TODO: generalise and move to creme_core ??
@@ -41,19 +45,25 @@ def _link(request, entity_id, relation_type_id):
 
     # TODO: in a Relation type method() ??
     if not relation_type.subject_ctypes.filter(id=entity.entity_type_id).exists():
-        raise Http404('Incompatible relation type for subject')  # meh
+        raise Http404('Incompatible relation type for subject')  # TODO: ConflictError
 
     # TODO: in a Relation type method() ??
     if not relation_type.object_ctypes.filter(id=managed_orga.entity_type_id).exists():
-        raise Http404('Incompatible relation type for object')  # meh
+        raise Http404('Incompatible relation type for object')  # TODO: ConflictError
 
     has_perm_or_die = user.has_perm_to_link_or_die
     has_perm_or_die(entity)
     has_perm_or_die(managed_orga)
 
-    Relation.objects.create(subject_entity=entity, type_id=relation_type_id,
-                            object_entity=managed_orga, user=user,
-                            )
+    try:
+        Relation.objects.get_or_create(subject_entity=entity, type_id=relation_type_id,
+                                       object_entity=managed_orga,
+                                       defaults={'user': user},
+                                      )
+    except Relation.MultipleObjectsReturned:
+        logger.warning('_link(): duplicated Relation <subject=%s type=%s object=%s>',
+                       entity.id, relation_type_id, managed_orga.id,
+                      )
 
     return redirect(entity)
 
