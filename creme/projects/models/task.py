19db78869e2c379b0b2a1529c733eeb0e3f1d309
Guillaume Englert <genglert at hybird.org@localhost>
2018-10-11 19:26:55 +0200

    refs #51 Clean a field in AbstractProjectTask.

--- creme/projects/models/task.py
+++ creme/projects/models/task.py
@@ -20,6 +20,7 @@
 
 from itertools import chain
 
+from django.conf import settings
 from django.db.models import (CharField, TextField, DateTimeField, PositiveIntegerField,
         ForeignKey, ManyToManyField, PROTECT, CASCADE)
 from django.urls import reverse
@@ -28,16 +29,17 @@ from django.utils.translation import ugettext_lazy as _
 from creme.creme_core.models import CremeEntity, Relation
 
 from ..constants import COMPLETED_PK, CANCELED_PK, REL_OBJ_LINKED_2_PTASK, REL_SUB_PART_AS_RESOURCE
-from .project import Project
+# from .project import Project
 from .taskstatus import TaskStatus
 
 
 class AbstractProjectTask(CremeEntity):
     title        = CharField(_('Title'), max_length=100)
-    linked_project = ForeignKey(Project, on_delete=CASCADE,  # TODO: settings.PROJECTS_PROJECT_MODEL ?
+    # linked_project = ForeignKey(Project, on_delete=CASCADE,
+    linked_project = ForeignKey(settings.PROJECTS_PROJECT_MODEL, on_delete=CASCADE,
                                 verbose_name=_('Project'), related_name='tasks_set', editable=False,
                                )
-    order        = PositiveIntegerField(_('Order'), blank=True, null=True, editable=False)  # TODO: null = False ?
+    order        = PositiveIntegerField(_('Order'), blank=True, null=True, editable=False)  # TODO: null = False ? remove blank
     parent_tasks = ManyToManyField('self', symmetrical=False,
                                    related_name='children_set', editable=False,  # TODO: rename children ?
                                   )
