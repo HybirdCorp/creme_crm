736548d47c41af257603c89444f2d70d92187746
Guillaume Englert <genglert at hybird.org@localhost>
2018-11-02 17:18:33 +0100

    refs #89 Add a generic view to help the disabling of an URL.

---
+++ creme/creme_core/views/generic/placeholder.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+#    Creme is a free/open-source Customer Relationship Management software
+#    Copyright (C) 2018  Hybird
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+from django.utils.decorators import method_decorator
+from django.views.generic.base import View
+
+from creme.creme_core.auth.decorators import login_required
+from creme.creme_core.core.exceptions import ConflictError
+
+
+class ErrorView(View):
+    error_class = ConflictError
+    message = 'These view has been disabled'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        raise self.error_class(self.message)
