5617539460e6f822b5315a2f0bcdcae003e3e6c8
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2018-11-06 18:11:38 +0100

    refs #4 #12 Remove some deprecated code.
    API breaking:
     - The templatetag 'creme_listview.get_listview_columns_header' does not inject the variable "can_merge_entities" in the context (it was deprecated).
     - These constants of 'creme_core.constants' have been removed (they were deprecated):
        - SETTING_BLOCK_DEFAULT_STATE_IS_OPEN
        - SETTING_BLOCK_DEFAULT_STATE_SHOW_EMPTY_FIELDS

--- creme/creme_core/templatetags/creme_listview.py
+++ creme/creme_core/templatetags/creme_listview.py
@@ -34,7 +34,7 @@ from ..core.enumerable import enumerable_registry, Enumerator
 from ..core.paginator import FlowPaginator
 from ..gui.bulk_update import bulk_update_registry
 from ..gui.listview import NULL_FK
-from ..gui.merge import merge_form_registry
+# from ..gui.merge import merge_form_registry
 from ..gui.pager import PagerContext
 from ..models import CustomField
 # from ..models.fields import EntityCTypeForeignKey
@@ -255,7 +255,7 @@ def get_listview_columns_header(context):
         cell.widget_ctx = widget_ctx
 
     context['NULL_FK'] = NULL_FK
-    context['can_merge_entities'] = merge_form_registry.get(context['model']) is not None  # DEPRECATED
+    # context['can_merge_entities'] = merge_form_registry.get(context['model']) is not None
 
     return context
 
