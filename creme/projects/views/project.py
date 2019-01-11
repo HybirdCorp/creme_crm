96e9ba251dc0f6c8e82c765473a6c6f829da2203
Guillaume Englert <genglert at hybird.org@localhost>
2019-01-11 18:55:26 +0100

    refs #51 Remove the deprecated code fom 'projects'.
    API breaking:
     - In 'views.project' :
       - abstract_add_project()
       - abstract_edit_project()
       - abstract_view_project()
       - add()
       - edit()
       - detailview()
     - In 'views.task' :
       - abstract_add_ptask()
       - abstract_edit_ptask()
       - abstract_edit_ptask_popup()
       - abstract_view_ptask()
       - add()
       - detailview()
       - edit()
       - edit_popup()
       - abstract_edit_activity()
       - edit_activity()

--- creme/projects/views/project.py
+++ creme/projects/views/project.py
@@ -18,13 +18,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-import warnings
+# import warnings
 
 from django.db.transaction import atomic
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 
-from creme.creme_core.auth import build_creation_perm as cperm
+# from creme.creme_core.auth import build_creation_perm as cperm
 from creme.creme_core.auth.decorators import login_required, permission_required
 from creme.creme_core.views.decorators import POST_only
 from creme.creme_core.views import generic
@@ -40,49 +40,49 @@ Project = get_project_model()
 # Function views --------------------------------------------------------------
 
 
-def abstract_add_project(request, form=project_forms.ProjectCreateForm,
-                         submit_label=Project.save_label,
-                        ):
-    warnings.warn('project.views.project.abstract_add_project() is deprecated ; '
-                  'use the class-based view ProjectCreation instead.',
-                  DeprecationWarning
-                 )
-    return generic.add_entity(request, form,
-                              extra_initial={'status': ProjectStatus.objects.first()},
-                              extra_template_dict={'submit_label': submit_label},
-                             )
+# def abstract_add_project(request, form=project_forms.ProjectCreateForm,
+#                          submit_label=Project.save_label,
+#                         ):
+#     warnings.warn('project.views.project.abstract_add_project() is deprecated ; '
+#                   'use the class-based view ProjectCreation instead.',
+#                   DeprecationWarning
+#                  )
+#     return generic.add_entity(request, form,
+#                               extra_initial={'status': ProjectStatus.objects.first()},
+#                               extra_template_dict={'submit_label': submit_label},
+#                              )
 
 
-def abstract_edit_project(request, project_id, form=project_forms.ProjectEditForm):
-    warnings.warn('project.views.project.abstract_edit_project() is deprecated ; '
-                  'use the class-based view ProjectEdition instead.',
-                  DeprecationWarning
-                 )
-    return generic.edit_entity(request, project_id, Project, form)
+# def abstract_edit_project(request, project_id, form=project_forms.ProjectEditForm):
+#     warnings.warn('project.views.project.abstract_edit_project() is deprecated ; '
+#                   'use the class-based view ProjectEdition instead.',
+#                   DeprecationWarning
+#                  )
+#     return generic.edit_entity(request, project_id, Project, form)
 
 
-def abstract_view_project(request, project_id,
-                          template='projects/view_project.html',
-                         ):
-    warnings.warn('project.views.project.abstract_view_project() is deprecated ; '
-                  'use the class-based view ProjectDetail instead.',
-                  DeprecationWarning
-                 )
-    return generic.view_entity(request, project_id, Project, template=template)
+# def abstract_view_project(request, project_id,
+#                           template='projects/view_project.html',
+#                          ):
+#     warnings.warn('project.views.project.abstract_view_project() is deprecated ; '
+#                   'use the class-based view ProjectDetail instead.',
+#                   DeprecationWarning
+#                  )
+#     return generic.view_entity(request, project_id, Project, template=template)
 
 
-@login_required
-@permission_required(('projects', cperm(Project)))
-def add(request):
-    warnings.warn('project.views.project.add() is deprecated.', DeprecationWarning)
-    return abstract_add_project(request)
+# @login_required
+# @permission_required(('projects', cperm(Project)))
+# def add(request):
+#     warnings.warn('project.views.project.add() is deprecated.', DeprecationWarning)
+#     return abstract_add_project(request)
 
 
-@login_required
-@permission_required('projects')
-def edit(request, project_id):
-    warnings.warn('project.views.project.edit() is deprecated.', DeprecationWarning)
-    return abstract_edit_project(request, project_id)
+# @login_required
+# @permission_required('projects')
+# def edit(request, project_id):
+#     warnings.warn('project.views.project.edit() is deprecated.', DeprecationWarning)
+#     return abstract_edit_project(request, project_id)
 
 
 @login_required
@@ -91,11 +91,11 @@ def listview(request):
     return generic.list_view(request, Project, hf_pk=DEFAULT_HFILTER_PROJECT)
 
 
-@login_required
-@permission_required('projects')
-def detailview(request, project_id):
-    warnings.warn('project.views.project.detailview() is deprecated.', DeprecationWarning)
-    return abstract_view_project(request, project_id)
+# @login_required
+# @permission_required('projects')
+# def detailview(request, project_id):
+#     warnings.warn('project.views.project.detailview() is deprecated.', DeprecationWarning)
+#     return abstract_view_project(request, project_id)
 
 
 @login_required
