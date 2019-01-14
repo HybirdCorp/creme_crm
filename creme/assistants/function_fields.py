401f1aa5caf39bc1a6305dbad5f5e0a89c5a441a
Guillaume Englert <genglert at hybird.org@localhost>
2019-01-14 14:13:38 +0100

    refs #63 The FunctionFields now store cached data in the FunctionField instances, not the entities any more.
    API breaking:
     - The method 'creme_core.core.function_field.FunctionField.populate_entities()' is now a regular method (it was a 'classmethod').
     - In the class 'billing.function_fields._BaseTotalFunctionField' (& child classes), the class-attribute "cache_attr" has been removed.

--- creme/assistants/function_fields.py
+++ creme/assistants/function_fields.py
@@ -28,89 +28,160 @@ from creme.creme_core.core.function_field import (FunctionField,
 from .models import Alert, Memo, ToDo
 
 
-class AlertsField(FunctionField):
+class _CachedFunctionField(FunctionField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._cache = defaultdict(list)
+
+
+# class AlertsField(FunctionField):
+class AlertsField(_CachedFunctionField):
     name         = 'assistants-get_alerts'
     verbose_name = _('Alerts')
     result_type  = FunctionFieldResultsList
 
     def __call__(self, entity, user):
-        cache = getattr(entity, '_alerts_cache', None)
+        # cache = getattr(entity, '_alerts_cache', None)
+        #
+        # if cache is None:
+        #     cache = entity._alerts_cache = list(Alert.objects
+        #                                              .filter(entity_id=entity.id, is_validated=False)
+        #                                              .order_by('trigger_date')
+        #                                              .values_list('title', flat=True)
+        #                                        )
+        #
+        # return FunctionFieldResultsList(FunctionFieldResult(title) for title in cache)
+        cache = self._cache
+        e_id = entity.id
+        cached = cache.get(e_id)
 
-        if cache is None:
-            cache = entity._alerts_cache = list(Alert.objects
-                                                     .filter(entity_id=entity.id, is_validated=False)
-                                                     .order_by('trigger_date')
-                                                     .values_list('title', flat=True)
-                                               )
+        if cached is None:
+            cached = cache[e_id] = list(Alert.objects
+                                             .filter(entity_id=e_id, is_validated=False)
+                                             .order_by('trigger_date')
+                                             .values_list('title', flat=True)
+                                       )
 
-        return FunctionFieldResultsList(FunctionFieldResult(title) for title in cache)
+        return FunctionFieldResultsList(FunctionFieldResult(title) for title in cached)
 
-    @classmethod
-    def populate_entities(cls, entities, user):
-        alerts_map = defaultdict(list)
+    # @classmethod
+    # def populate_entities(cls, entities, user):
+    #     alerts_map = defaultdict(list)
+    #
+    #     for title, e_id in Alert.objects.filter(entity_id__in=[e.id for e in entities], is_validated=False) \
+    #                                     .order_by('trigger_date') \
+    #                                     .values_list('title', 'entity_id'):
+    #         alerts_map[e_id].append(title)
+    #
+    #     for entity in entities:
+    #         entity._alerts_cache = alerts_map[entity.id]
+    def populate_entities(self, entities, user):
+        cache = self._cache
 
-        for title, e_id in Alert.objects.filter(entity_id__in=[e.id for e in entities], is_validated=False) \
-                                        .order_by('trigger_date') \
-                                        .values_list('title', 'entity_id'):
-            alerts_map[e_id].append(title)
+        for title, e_id in Alert.objects \
+                                .filter(entity_id__in=[e.id for e in entities if e.id not in cache],
+                                        is_validated=False,
+                                       ) \
+                                .order_by('trigger_date') \
+                                .values_list('title', 'entity_id'):
+            cache[e_id].append(title)
 
-        for entity in entities:
-            entity._alerts_cache = alerts_map[entity.id]
 
-
-class MemosField(FunctionField):
+# class MemosField(FunctionField):
+class MemosField(_CachedFunctionField):
     name         = 'assistants-get_memos'
     verbose_name = _('Memos')
     result_type  = FunctionFieldResultsList
 
     def __call__(self, entity, user):
-        cache = getattr(entity, '_memos_cache', None)
+        # cache = getattr(entity, '_memos_cache', None)
+        #
+        # if cache is None:
+        #     cache = entity._memos_cache = list(Memo.objects.filter(entity_id=entity.id)
+        #                                                    .order_by('-creation_date')
+        #                                                    .values_list('content', flat=True)
+        #                                       )
+        #
+        # return FunctionFieldResultsList(FunctionFieldResult(content) for content in cache)
+        cache = self._cache
+        e_id = entity.id
+        cached = cache.get(e_id)
 
-        if cache is None:
-            cache = entity._memos_cache = list(Memo.objects.filter(entity_id=entity.id)
-                                                           .order_by('-creation_date')
-                                                           .values_list('content', flat=True)
-                                              )
+        if cached is None:
+            cached = cache[e_id] = list(Memo.objects.filter(entity_id=entity.id)
+                                                    .order_by('-creation_date')
+                                                    .values_list('content', flat=True)
+                                       )
 
-        return FunctionFieldResultsList(FunctionFieldResult(content) for content in cache)
+        return FunctionFieldResultsList(FunctionFieldResult(content) for content in cached)
 
-    @classmethod
-    def populate_entities(cls, entities, user):
-        memos_map = defaultdict(list)
+    # @classmethod
+    # def populate_entities(cls, entities, user):
+    #     memos_map = defaultdict(list)
+    #
+    #     for content, e_id in Memo.objects.filter(entity_id__in=[e.id for e in entities]) \
+    #                                      .order_by('-creation_date') \
+    #                                      .values_list('content', 'entity_id'):
+    #         memos_map[e_id].append(content)
+    #
+    #     for entity in entities:
+    #         entity._memos_cache = memos_map[entity.id]
+    def populate_entities(self, entities, user):
+        cache = self._cache
 
-        for content, e_id in Memo.objects.filter(entity_id__in=[e.id for e in entities]) \
-                                         .order_by('-creation_date') \
-                                         .values_list('content', 'entity_id'):
-            memos_map[e_id].append(content)
-
-        for entity in entities:
-            entity._memos_cache = memos_map[entity.id]
+        for content, e_id in Memo.objects \
+                                 .filter(entity_id__in=[e.id for e in entities if e.id not in cache]) \
+                                 .order_by('-creation_date') \
+                                 .values_list('content', 'entity_id'):
+            cache[e_id].append(content)
 
 
-class TodosField(FunctionField):
+# class TodosField(FunctionField):
+class TodosField(_CachedFunctionField):
     name         = 'assistants-get_todos'
     verbose_name = _('Todos')
     result_type  = FunctionFieldResultsList
 
     def __call__(self, entity, user):
-        cache = getattr(entity, '_todos_cache', None)
+        # cache = getattr(entity, '_todos_cache', None)
+        #
+        # if cache is None:
+        #     cache = entity._todos_cache = list(ToDo.objects.filter(entity_id=entity.id, is_ok=False)
+        #                                                    .order_by('-creation_date')
+        #                                                    .values_list('title', flat=True)
+        #                                       )
+        #
+        # return FunctionFieldResultsList(FunctionFieldResult(title) for title in cache)
+        cache = self._cache
+        e_id = entity.id
+        cached = cache.get(e_id)
 
-        if cache is None:
-            cache = entity._todos_cache = list(ToDo.objects.filter(entity_id=entity.id, is_ok=False)
-                                                           .order_by('-creation_date')
-                                                           .values_list('title', flat=True)
-                                              )
+        if cached is None:
+            cached = cache[e_id] = list(ToDo.objects.filter(entity_id=entity.id, is_ok=False)
+                                                    .order_by('-creation_date')
+                                                    .values_list('title', flat=True)
+                                       )
 
-        return FunctionFieldResultsList(FunctionFieldResult(title) for title in cache)
+        return FunctionFieldResultsList(FunctionFieldResult(title) for title in cached)
 
-    @classmethod
-    def populate_entities(cls, entities, user):
-        todos_map = defaultdict(list)
+    # @classmethod
+    # def populate_entities(cls, entities, user):
+    #     todos_map = defaultdict(list)
+    #
+    #     for title, e_id in ToDo.objects.filter(entity_id__in=[e.id for e in entities], is_ok=False) \
+    #                                    .order_by('-creation_date') \
+    #                                    .values_list('title', 'entity_id'):
+    #         todos_map[e_id].append(title)
+    #
+    #     for entity in entities:
+    #         entity._todos_cache = todos_map[entity.id]
+    def populate_entities(self, entities, user):
+        cache = self._cache
 
-        for title, e_id in ToDo.objects.filter(entity_id__in=[e.id for e in entities], is_ok=False) \
-                                       .order_by('-creation_date') \
-                                       .values_list('title', 'entity_id'):
-            todos_map[e_id].append(title)
-
-        for entity in entities:
-            entity._todos_cache = todos_map[entity.id]
+        for title, e_id in ToDo.objects \
+                               .filter(entity_id__in=[e.id for e in entities if e.id not in cache],
+                                       is_ok=False,
+                                      ) \
+                               .order_by('-creation_date') \
+                               .values_list('title', 'entity_id'):
+            cache[e_id].append(title)
