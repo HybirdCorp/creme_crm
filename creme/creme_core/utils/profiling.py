259b8c5145aa8de9651c6cd8d7075026ea0828ba
Guillaume Englert <genglert at hybird.org@localhost>
2019-02-14 15:50:14 +0100

    refs #77 remove a useless future import.

--- creme/creme_core/utils/profiling.py
+++ creme/creme_core/utils/profiling.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #
-# Copyright (c) 2016-2018 Hybird
+# Copyright (c) 2016-2019 Hybird
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,8 +22,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 ################################################################################
-
-from __future__ import print_function
 
 from django.db import DEFAULT_DB_ALIAS, connections
 from django.test.utils import CaptureQueriesContext as DjangoCaptureQueriesContext
