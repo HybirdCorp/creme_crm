7def3ccb5090d2e1eb96b9f9a087a6b3ccc6d2e4
Guillaume Englert <genglert at hybird.org@localhost>
2019-04-09 10:05:17 +0200

    refs #30 Fix 2 logging messages in CremeUser.

--- creme/creme_core/models/auth.py
+++ creme/creme_core/models/auth.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2018  Hybird
+#    Copyright (C) 2009-2019  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -663,10 +663,10 @@ class CremeUser(AbstractBaseUser):
             creds = creds_map.get(self.id)
 
         if creds is None:
-            logger.debug('UserProfile._get_credentials(): Cache MISS for id=%s user=%s', entity.id, self)
+            logger.debug('CremeUser._get_credentials(): Cache MISS for id=%s user=%s', entity.id, self)
             creds_map[self.id] = creds = EntityCredentials(self, entity)
         else:
-            logger.debug('UserProfile._get_credentials(): Cache HIT for id=%s user=%s', entity.id, self)
+            logger.debug('CremeUser._get_credentials(): Cache HIT for id=%s user=%s', entity.id, self)
 
         return creds
 
