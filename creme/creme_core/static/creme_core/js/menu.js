9d324c90b8469a8f6f61260447b08021cce40f51
Florian Fabre <ffabre@hybird.org>
2019-04-30 10:53:38 +0200

    refs #98 Refactor main menu  - Add creme.menu.MenuController  - Add tests API Breaking  - [JS] Remove creme.menu.bindEvents  - [JS] Remove creme.menu.openQuickForm  - [JS] Remove creme.menu.openCreateAnyDialog

--- creme/creme_core/static/creme_core/js/menu.js
+++ creme/creme_core/static/creme_core/js/menu.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
     Creme is a free/open-source Customer Relationship Management software
-    Copyright (C) 2009-2018  Hybird
+    Copyright (C) 2009-2019  Hybird
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -26,6 +26,143 @@
 
 creme.menu = {};
 
+creme.menu.MenuController = creme.component.Component.sub({
+    isBound: function() {
+        return Object.isNone(this._element) === false;
+    },
+
+    bind: function(element) {
+        if (this.isBound()) {
+            throw new Error('MenuController is already bound');
+        }
+
+        this._element = element;
+
+        this._initMenuItems(element);
+        this._initQuickFormItems(element);
+        this._initAnyFormItems(element);
+
+        return this;
+    },
+
+    closeMenu: function() {
+        // Hide the current open menu (since the quick-forms are triggered in the menu)
+        $('.ui-creme-navigation-activated', this._element).removeClass('ui-creme-navigation-activated');
+        return this;
+    },
+
+    _initMenuItems: function(element) {
+        var menu = element.find('.ui-creme-navigation');
+        var items = menu.children('li');
+
+        // Activate menus on hover events
+        items.hover(function(e) {
+            $(this).addClass('ui-creme-navigation-activated');
+        }, function(e) {
+            $(this).removeClass('ui-creme-navigation-activated');
+        });
+
+        // Activate menus when clicked directly (for devices without hover)
+        items.click(function(e) { // possibly limit this to touch press events for tablets, or maybe just disable for desktop ?
+            if (e.target !== this) {
+                return; // when clicking on sub-menu entries (<a>s inside <li>s inside <ul>s inside the <li> menu) we don't want to do anything
+            }
+
+            var currentActivatedItem = menu.children('li.ui-creme-navigation-activated');
+            var itemToActivate = $(e.currentTarget);
+            var isOtherItemActive = currentActivatedItem.index() !== itemToActivate.index();
+
+            if (isOtherItemActive) {
+                currentActivatedItem.removeClass('ui-creme-navigation-activated');
+                itemToActivate.addClass('ui-creme-navigation-activated');
+            }
+        });
+    },
+
+    _initQuickFormItems: function(element) {
+        var openQuickFormDialog = this._openQuickFormDialog.bind(this);
+
+        $('.quickform-menu-link', element).on('click', function(e) {
+            e.preventDefault();
+            openQuickFormDialog($(this));
+        });
+    },
+
+    _initAnyFormItems: function(element) {
+        var openCreateAnyDialog = this._openCreateAnyDialog.bind(this);
+
+        $('.anyform-menu-link', element).on('click', function(e) {
+            e.preventDefault();
+            openCreateAnyDialog($(this));
+        });
+    },
+
+    _openPopup: function(dialog) {
+        // ...or if there's a need to close all popups: $('.ui-dialog .ui-dialog-content').dialog('close');
+        if (this._activePopup) {
+            this._activePopup.close();
+            this._activePopup = undefined;
+        }
+
+        this._activePopup = dialog.open();
+    },
+
+    _openQuickFormDialog: function(item) {
+        this.closeMenu();
+        this._openPopup(creme.dialogs.form(item.attr('data-href'), {reloadOnSuccess: true}));
+    },
+
+    _openCreateAnyDialog: function(item) {
+        this.closeMenu();
+
+        // TODO : use cache ?
+        var groupedLinks = JSON.parse(item.attr('data-grouped-links'));
+        var maxColumnCount = 1;
+
+        groupedLinks.forEach(function(item) {
+            maxColumnCount = Math.max(maxColumnCount, item.length);
+        });
+
+        var renderLinkGroupRow = function(item) {
+            return (
+                '<div class="create-group-container create-group-container-${count}-columned">' +
+                    '${groups}' +
+                '</div>').template({
+                    count: item.length,
+                    groups: item.map(renderLinkGroup).join('')
+                });
+        };
+
+        var renderLinkGroup = function(item) {
+            return (
+                '<div class="create-group">' +
+                    '<div class="create-group-title">${title}</div>' +
+                    '${links}' +
+                '</div>').template({
+                    title: item.label,
+                    links: item.links.map(renderLink).join('')
+                });
+        };
+
+        var renderLink = function(link) {
+            if (link.url) {
+                return '<a href="${url}" class="create-group-entry">${label}</a>'.template(link);
+            } else {
+                return '<span class="create-group-entry forbidden">${label}</span>'.template(link);
+            }
+        };
+
+        var html = '<div class="create-all-form">${rows}</div>'.template({
+            rows: groupedLinks.map(renderLinkGroupRow).join('')
+        });
+
+        this._openPopup(creme.dialogs.html(html, {
+                                              title: gettext('Chose the type of entity to create'),
+                                              width: Math.max(550, maxColumnCount * 200)
+                                           }));
+    }
+});
+/*
 creme.menu.bindEvents = function() {
     var menu = $('.ui-creme-navigation');
     var items = menu.children('li');
@@ -119,4 +256,5 @@ creme.menu.openCreateAnyDialog = function(a_tag) {
                                                  }
                                                 ).open();
 };
+*/
 }(jQuery));
---
+++ creme/creme_core/static/creme_core/js/tests/views/menu.js
@@ -0,0 +1,359 @@
+/* global QUnitWidgetMixin */
+
+(function($) {
+var ANYFORM_GROUPED_LINKS = [
+    [{
+        "label": "Group A",
+        "links": [{
+            "label": "A1",
+            "url": "mock/any/A1"
+        }, {
+            "label": "A2",
+            "url": "mock/any/A2"
+        }, {
+            "label": "A3 (disabled)"
+        }]
+    }, {
+        "label": "Group B",
+        "links": [{
+            "label": "B1",
+            "url": "mock/any/B1"
+        }]
+    }],
+    [{
+        "label": "Group C",
+        "links": [{
+            "label": "C1",
+            "url": "mock/any/C1"
+        }, {
+            "label": "C2",
+            "url": "mock/any/C2"
+        }, {
+            "label": "C3",
+            "url": "mock/any/C3"
+        }]
+    }]
+];
+
+QUnit.module("creme.menu.js", new QUnitMixin(QUnitEventMixin,
+                                             QUnitAjaxMixin,
+                                             QUnitDialogMixin,
+                                             QUnitWidgetMixin, {
+    beforeEach: function() {
+        var backend = this.backend;
+
+        this.setMockBackendGET({
+            'mock/A/quickform': backend.response(200, '<form action="mock/A/quickform/"></form>'),
+            'mock/B/quickform': backend.response(200, '<form action="mock/B/quickform/"></form>')
+        });
+
+        this.setMockBackendPOST({
+            'mock/A/quickform': backend.response(200, ''),
+            'mock/B/quickform': backend.response(200, '')
+        });
+    },
+
+    createMenuHtml: function(options) {
+        options = $.extend({
+            items: []
+        }, options || {});
+
+        var renderItem = function(item) {
+            item = $.extend({
+                level: 0,
+                items: [],
+                id: 'id',
+                content: '<span></span>'
+            }, item || {});
+
+            var renderSubItem = function(item) {
+                return renderItem($.extend({}, item, {level: (item.level || 0) + 1}));
+            };
+
+            return (
+               '<li class="ui-creme-navigation-item-level${level} ui-creme-navigation-item-${id}">' +
+                  '${content}' +
+                  '${items}' +
+               '</li>'
+            ).template({
+                content: item.content || '<span></span>',
+                level: item.level,
+                id: item.id,
+                items: item.items ? '<ul>' + item.items.map(renderSubItem).join('') + '</ul>' : ''
+            });
+        };
+
+        return (
+            '<div class="header-menu">' +
+                 '<ul class="ui-creme-navigation">${items}</ul>' +
+            '</div>'
+        ).template({
+            items: options.items.map(renderItem).join('')
+        });
+    },
+
+    createRegularMenuHtml: function() {
+        return this.createMenuHtml({
+            items: [{
+                label: 'menu A',
+                id: 'A',
+                items: [
+                    {id: 'A1', content: '<a href="mock/menu/A/A1">item A1</a>'},
+                    {id: 'A2', content: '<a href="mock/menu/A/A2">item A2</a>'},
+                    {id: 'A3', content: '<span>item A3 (disabled)</span>'}
+                ]
+            }, {
+                label: 'menu B',
+                id: 'B',
+                items: [
+                    {id: 'B1', content: '<a href="mock/menu/B/B1">item B1</a>'},
+                    {id: 'B2', content: '<a href="mock/menu/B/B2">item B2</a>'}
+                ]
+            }]
+        });
+    },
+
+    createQuickFormMenuHtml: function() {
+        return this.createMenuHtml({
+            items: [{
+                label: 'menu A',
+                id: 'A',
+                items: [
+                    {id: 'quickA', content: '<a class="quickform-menu-link" data-href="mock/A/quickform">quickform A</a>'},
+                    {id: 'quickB', content: '<a class="quickform-menu-link" data-href="mock/B/quickform">quickform B</a>'}
+                ]
+            }]
+        });
+    },
+
+    createAnyFormMenuHtml: function() {
+        return this.createMenuHtml({
+            items: [{
+                label: 'menu A',
+                id: 'A',
+                items: [
+                    {id: 'anyA', content: '<a class="anyform-menu-link" data-grouped-links="' + $.toJSON([]) + '">anyform A</a>'},
+                    {id: 'anyB', content: '<a class="anyform-menu-link" data-grouped-links="' + $.toJSON(ANYFORM_GROUPED_LINKS).replace(/"/gi, '&quot;') + '">anyform B</a>'}
+                ]
+            }]
+        });
+    }
+}));
+
+QUnit.test('creme.menu.MenuController (bind)', function(assert) {
+    var element = $(this.createRegularMenuHtml());
+    var controller = new creme.menu.MenuController();
+
+    equal(false, controller.isBound());
+
+    controller.bind(element);
+
+    equal(true, controller.isBound());
+    this.assertRaises(function() {
+        controller.bind(element);
+    }, Error, 'Error: MenuController is already bound');
+});
+
+QUnit.test('creme.menu.MenuController (main menu, hover)', function(assert) {
+    var element = $(this.createRegularMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+
+    equal(true, controller.isBound());
+
+    var menuA = element.find('li.ui-creme-navigation-item-A');
+    var menuB = element.find('li.ui-creme-navigation-item-B');
+
+    equal(false, menuA.is('.ui-creme-navigation-activated'));
+    equal(false, menuB.is('.ui-creme-navigation-activated'));
+
+    menuA.mouseenter();
+
+    equal(true, menuA.is('.ui-creme-navigation-activated'));
+    equal(false, menuB.is('.ui-creme-navigation-activated'));
+
+    menuA.mouseleave();
+    menuB.mouseenter();
+
+    equal(false, menuA.is('.ui-creme-navigation-activated'));
+    equal(true, menuB.is('.ui-creme-navigation-activated'));
+});
+
+QUnit.test('creme.menu.MenuController (main menu, click)', function(assert) {
+    var element = $(this.createRegularMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+
+    equal(true, controller.isBound());
+
+    var menuA = element.find('.ui-creme-navigation-item-A');
+    var menuB = element.find('.ui-creme-navigation-item-B');
+
+    equal(false, menuA.is('li.ui-creme-navigation-activated'));
+    equal(false, menuB.is('li.ui-creme-navigation-activated'));
+
+    menuA.click();
+
+    equal(true, menuA.is('.ui-creme-navigation-activated'));
+    equal(false, menuB.is('.ui-creme-navigation-activated'));
+
+    menuB.click();
+
+    equal(false, menuA.is('.ui-creme-navigation-activated'));
+    equal(true, menuB.is('.ui-creme-navigation-activated'));
+});
+
+QUnit.test('creme.menu.MenuController (submenu quickform)', function(assert) {
+    var element = $(this.createQuickFormMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+    var quickA = element.find('.ui-creme-navigation-item-quickA a');
+
+    equal(true, controller.isBound());
+
+    this.assertClosedDialog();
+    deepEqual([], this.mockBackendUrlCalls('mock/A/quickform'));
+
+    quickA.click();
+
+    this.assertOpenedDialog();
+    deepEqual([['GET', {}]], this.mockBackendUrlCalls('mock/A/quickform'));
+});
+
+QUnit.test('creme.menu.MenuController (quickform, close opened submenu)', function(assert) {
+    var element = $(this.createQuickFormMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+
+    var menuA = element.find('.ui-creme-navigation-item-A');
+    var quickA = element.find('.ui-creme-navigation-item-quickA a');
+
+    equal(true, controller.isBound());
+    equal(false, menuA.is('li.ui-creme-navigation-activated'));
+
+    menuA.mouseenter();
+
+    equal(true, menuA.is('li.ui-creme-navigation-activated'));
+
+    this.assertClosedDialog();
+
+    quickA.click();
+
+    this.assertOpenedDialog();
+    equal(false, menuA.is('li.ui-creme-navigation-activated'));
+});
+
+QUnit.test('creme.menu.MenuController (quickform, close opened dialog)', function(assert) {
+    var element = $(this.createQuickFormMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+
+    var quickA = element.find('.ui-creme-navigation-item-quickA a');
+    var quickB = element.find('.ui-creme-navigation-item-quickB a');
+
+    equal(true, controller.isBound());
+
+    quickA.click();
+
+    var dialog = this.assertOpenedDialog();
+    equal('mock/A/quickform/', dialog.find('form').attr('action'));
+
+    deepEqual([['mock/A/quickform', 'GET', {}]], this.mockBackendUrlCalls());
+
+    quickB.click();
+
+    dialog = this.assertOpenedDialog();
+    equal('mock/B/quickform/', dialog.find('form').attr('action'));
+
+    deepEqual([
+        ['mock/A/quickform', 'GET', {}],
+        ['mock/B/quickform', 'GET', {}]
+    ], this.mockBackendUrlCalls());
+});
+
+QUnit.test('creme.menu.MenuController (submenu anyform, empty)', function(assert) {
+    var element = $(this.createAnyFormMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+
+    var anyA = element.find('.ui-creme-navigation-item-anyA a');
+
+    equal(true, controller.isBound());
+
+    anyA.click();
+
+    var dialog = this.assertOpenedDialog();
+    this.equalHtml('<div class="create-all-form">', dialog.find('.ui-creme-dialog-frame'));
+});
+
+QUnit.test('creme.menu.MenuController (submenu anyform)', function(assert) {
+    var element = $(this.createAnyFormMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+
+    var anyB = element.find('.ui-creme-navigation-item-anyB a');
+
+    equal(true, controller.isBound());
+
+    anyB.click();
+
+    var dialog = this.assertOpenedDialog();
+    deepEqual([], this.mockBackendUrlCalls());   // rendered in js
+
+    this.equalHtml(
+         '<div class="create-all-form">' +
+            '<div class="create-group-container create-group-container-2-columned">' +
+                '<div class="create-group">' +
+                    '<div class="create-group-title">Group A</div>' +
+                    '<a href="mock/any/A1" class="create-group-entry">A1</a>' +
+                    '<a href="mock/any/A2" class="create-group-entry">A2</a>' +
+                    '<span class="create-group-entry forbidden">A3 (disabled)</span>' +
+                '</div>' +
+                '<div class="create-group">' +
+                    '<div class="create-group-title">Group B</div>' +
+                    '<a href="mock/any/B1" class="create-group-entry">B1</a>' +
+                '</div>' +
+            '</div>' +
+            '<div class="create-group-container create-group-container-1-columned">' +
+                '<div class="create-group">' +
+                    '<div class="create-group-title">Group C</div>' +
+                    '<a href="mock/any/C1" class="create-group-entry">C1</a>' +
+                    '<a href="mock/any/C2" class="create-group-entry">C2</a>' +
+                    '<a href="mock/any/C3" class="create-group-entry">C3</a>' +
+                '</div>' +
+            '</div>' +
+        '</div>', dialog.find('.ui-creme-dialog-frame'));
+});
+
+QUnit.test('creme.menu.MenuController (submenu anyform, close opened submenu)', function(assert) {
+    var element = $(this.createAnyFormMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+
+    var menuA = element.find('.ui-creme-navigation-item-A');
+    var anyA = element.find('.ui-creme-navigation-item-anyA a');
+
+    equal(true, controller.isBound());
+    equal(false, menuA.is('li.ui-creme-navigation-activated'));
+
+    menuA.mouseenter();
+
+    equal(true, menuA.is('li.ui-creme-navigation-activated'));
+
+    anyA.click();
+
+    this.assertOpenedDialog();
+    equal(false, menuA.is('li.ui-creme-navigation-activated'));
+});
+
+QUnit.test('creme.menu.MenuController (submenu anyform, close opened dialog)', function(assert) {
+    var element = $(this.createAnyFormMenuHtml());
+    var controller = new creme.menu.MenuController().bind(element);
+
+    var anyA = element.find('.ui-creme-navigation-item-anyA a');
+    var anyB = element.find('.ui-creme-navigation-item-anyB a');
+
+    equal(true, controller.isBound());
+
+    anyA.click();
+
+    this.assertOpenedDialog();
+
+    anyB.click();
+
+    this.assertOpenedDialog();
+});
+
+}(jQuery));
