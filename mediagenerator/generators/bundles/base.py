3c8fe3deb0fa3feb2054856c1a7d478a881add55
Florian Fabre <ffabre@hybird.org>
2019-09-02 15:24:27 +0200

    refs #33 #114 Fix "generatemedia" command compatibility issue with python 3.5

--- mediagenerator/generators/bundles/base.py
+++ mediagenerator/generators/bundles/base.py
@@ -1,5 +1,7 @@
 from hashlib import sha1
+
 import os
+import sys
 
 from django.utils.encoding import smart_str
 
@@ -204,3 +206,31 @@ class RawFileFilter(FileFilter):
             hash = self.hash
 
         yield self.name, hash
+
+
+class SubProcessFilter(Filter):
+    def run_process(self, command, input=None):
+        # We import this here, so App Engine Helper users don't get import errors.
+        from subprocess import Popen, PIPE
+
+        # universal_newlines enables text mode for stdin. That cause an issue
+        # in windows which use cp1252 as default for western europe.
+        #
+        # The "encoding" argument in python 3.6 fix it by forcing the
+        # right encoding to io.TextIOWrapper (see Popen code).
+        #
+        # In python 3.5 removing universal_newlines and encode input as utf-8
+        # bytes fix the problem in a different way. 
+        if sys.version_info < (3, 6):
+            cmd = Popen(command,
+                        stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            output, error = cmd.communicate(input.encode('utf-8'))
+        else:
+            cmd = Popen(command,
+                        stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding='utf-8',
+                        universal_newlines=True)
+            output, error = cmd.communicate(input)
+
+        assert cmd.wait() == 0, 'Command returned bad result:\n{}'.format(error)
+
+        return output
