f9e74e1855873d1310a1a1ec9bd238e9269e6e96
Guillaume Englert <genglert at hybird.org@localhost>
2020-01-06 16:01:01 +0100

    refs #26 Remove the deprecated code from 'crudity'.
    API breaking:
     - The filter "|truncate" (from the lib "crudity_tags") has been removed.

--- creme/crudity/templatetags/crudity_tags.py
+++ creme/crudity/templatetags/crudity_tags.py
@@ -1,43 +1,25 @@
 # -*- coding: utf-8 -*-
 
-################################################################################
-#    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2019  Hybird
+# import warnings
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# from django import template
+# from django.template.defaultfilters import truncatewords
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# register = template.Library()
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
-
-import warnings
-
-from django import template
-from django.template.defaultfilters import truncatewords
-
-register = template.Library()
-
-
-@register.filter(name='truncate')
-def truncate(word, truncate_at):
-    warnings.warn('The filter "|truncate" (crudity_tags) is deprecated ;'
-                  'use "|truncatewords" and "|truncatewords" instead.',
-                  DeprecationWarning
-                 )
-
-    words = truncatewords(word, truncate_at)
-    word = str(word)
-    truncated = word[:truncate_at]
-
-    if len(words.split()) == 1 and not len(truncated) == len(word):
-        words = '{}...'.format(truncated)
-
-    return words
+#
+# @register.filter(name='truncate')
+# def truncate(word, truncate_at):
+#     warnings.warn('The filter "|truncate" (crudity_tags) is deprecated ;'
+#                   'use "|truncatewords" and "|truncatewords" instead.',
+#                   DeprecationWarning
+#                  )
+#
+#     words = truncatewords(word, truncate_at)
+#     word = str(word)
+#     truncated = word[:truncate_at]
+#
+#     if len(words.split()) == 1 and not len(truncated) == len(word):
+#         words = '{}...'.format(truncated)
+#
+#     return words
