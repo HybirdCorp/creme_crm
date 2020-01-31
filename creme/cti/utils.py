421fa4f2e489aaedc863c830975a8bddfd9af73e
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2020-01-31 19:31:29 +0100

    refs #73 Small improvements in 'cti'.
    API breaking
     - The constant 'cti.views.RESPOND_TO_A_CALL_MODELS' has been removed.

--- creme/cti/utils.py
+++ creme/cti/utils.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2019  Hybird
+#    Copyright (C) 2009-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -28,7 +28,7 @@ from creme.creme_core.templatetags.creme_widgets import get_icon_size_px, get_ic
 from creme.creme_core.utils.media import get_current_theme
 
 
-def print_phone(entity, fval, user, field):  # TODO: rename  print_phone_html ?
+def print_phone(entity, fval, user, field) -> str:  # TODO: rename  print_phone_html ?
     if not fval:
         return simple_print_html(entity, fval, user, field)
 
