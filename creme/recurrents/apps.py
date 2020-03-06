603da9b4ed104c8e53d98b806907a187106fb3c5
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2020-03-06 16:05:23 +0100

    refs #98 #86 Fix a crash when 'recurrents' is installed but not 'billing'.

--- creme/recurrents/apps.py
+++ creme/recurrents/apps.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2015-2019  Hybird
+#    Copyright (C) 2015-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -47,7 +47,10 @@ class RecurrentsConfig(CremeAppConfig):
 
     def register_menu(self, creme_menu):
         RGenerator = self.RecurrentGenerator
-        creme_menu.get('features', 'management') \
+        creme_menu.get('features') \
+                  .get_or_create(creme_menu.ContainerItem, 'management', priority=50,
+                                 defaults={'label': _('Management')},
+                                ) \
                   .add(creme_menu.URLItem.list_view('recurrents-generators', model=RGenerator), priority=100)
         creme_menu.get('creation', 'any_forms') \
                   .get_or_create_group('management', _('Management'), priority=50) \
