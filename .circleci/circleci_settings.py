016ea86d7a585c9183e42fc29e2986b678a007e3
Fabre Florian <ffabre@hybird.org>
2020-07-02 12:23:20 +0200

    Refs #41 Add django_extensions in dev requirements. Add settings info & shell targets in Makefile

--- .circleci/circleci_settings.py
+++ .circleci/circleci_settings.py
@@ -1,6 +1,10 @@
-from .settings import INSTALLED_CREME_APPS
+from .settings import INSTALLED_CREME_APPS, INSTALLED_DJANGO_APPS
 
 SECRET_KEY = "CircleCi-Secret-Key"
+
+INSTALLED_DJANGO_APPS.extend([
+    'django_extensions',
+])
 
 INSTALLED_CREME_APPS.extend([
     # 'creme.sms',  # Work In Progress
