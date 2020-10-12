e9e4f125f7634e3f964a40fa3dc9a0e5d23a28c4
Guillaume Englert <genglert@hybird.org>
2020-10-12 19:37:42 +0200

    In 'Folder', some business logic has been moved from forms to model.  - The copy of the parent category.  - The checking of loop of Folders.

--- creme/documents/forms/folder.py
+++ creme/documents/forms/folder.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2019  Hybird
+#    Copyright (C) 2009-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,9 +18,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+# from django.forms.utils import ValidationError
+# from django.utils.translation import gettext_lazy as _
 from django.db.models.query_utils import Q
-from django.forms.utils import ValidationError
-from django.utils.translation import gettext_lazy as _
 
 from creme.creme_core.forms import CremeEntityForm
 from creme.creme_core.forms.bulk import BulkDefaultEditForm
@@ -33,22 +33,22 @@ Folder = get_folder_model()
 class _FolderForm(CremeEntityForm):
     class Meta(CremeEntityForm.Meta):
         model = Folder
-        help_texts = {
-            'category': _("The parent's category will be copied if you do not select one."),
-        }
+        # help_texts = {
+        #     'category': _("The parent's category will be copied if you do not select one."),
+        # }
 
-    def save(self, *args, **kwargs):
-        instance = self.instance
-        if not instance.category and instance.parent_folder:
-            instance.category = instance.parent_folder.category
-
-        return super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     instance = self.instance
+    #     if not instance.category and instance.parent_folder:
+    #         instance.category = instance.parent_folder.category
+    #
+    #     return super().save(*args, **kwargs)
 
 
 class FolderForm(_FolderForm):
-    error_messages = {
-        'loop': _('This folder is one of the child folders of «%(folder)s»'),
-    }
+    # error_messages = {
+    #     'loop': _('This folder is one of the child folders of «%(folder)s»'),
+    # }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -57,16 +57,17 @@ class FolderForm(_FolderForm):
             # TODO: remove direct children too ??
             self.fields['parent_folder'].q_filter = ~Q(id=pk)
 
-    def clean_parent_folder(self):
-        parent_folder = self.cleaned_data['parent_folder']
-        folder = self.instance
-
-        if folder.pk and parent_folder and folder.already_in_children(parent_folder.id):
-            raise ValidationError(self.error_messages['loop'],
-                                  params={'folder': folder}, code='loop',
-                                 )
-
-        return parent_folder
+    # def clean_parent_folder(self):
+    #     parent_folder = self.cleaned_data['parent_folder']
+    #     folder = self.instance
+    #
+    #     if folder.pk and parent_folder and folder.already_in_children(parent_folder.id):
+    #         raise ValidationError(
+    #             self.error_messages['loop'],
+    #             params={'folder': folder}, code='loop',
+    #         )
+    #
+    #     return parent_folder
 
 
 class ChildFolderForm(_FolderForm):
@@ -79,10 +80,10 @@ class ChildFolderForm(_FolderForm):
 
 
 class ParentFolderBulkForm(BulkDefaultEditForm):
-    error_messages = {
-        'itself': _('«%(folder)s» cannot be its own parent'),
-        'loop': _('This folder is one of the child folders of «%(folder)s»'),
-    }
+    # error_messages = {
+    #     'itself': _('«%(folder)s» cannot be its own parent'),
+    #     'loop': _('This folder is one of the child folders of «%(folder)s»'),
+    # }
 
     def __init__(self, model, field, user, entities, is_bulk=False, **kwargs):
         super().__init__(model, field, user, entities, is_bulk=is_bulk, **kwargs)
@@ -91,26 +92,28 @@ class ParentFolderBulkForm(BulkDefaultEditForm):
             # TODO: like above -> remove direct children too ??
             self.fields['field_value'].q_filter = ~Q(id__in=[entities[0].id])
 
-    def _bulk_clean_entity(self, entity, values):
-        parent_folder = values.get('parent_folder')
-
-        if parent_folder:
-            if parent_folder == entity:
-                raise ValidationError(self.error_messages['itself'],
-                                      params={'folder': entity},
-                                      code='itself',
-                                     )
-
-            if entity.already_in_children(parent_folder.id):
-                raise ValidationError(self.error_messages['loop'],
-                                      params={'folder': entity},
-                                      code='loop',
-                                     )
-
-            if not entity.category:
-                entity.category = parent_folder.category
-
-        return super()._bulk_clean_entity(entity, values)
+    # def _bulk_clean_entity(self, entity, values):
+    #     parent_folder = values.get('parent_folder')
+    #
+    #     if parent_folder:
+    #         if parent_folder == entity:
+    #             raise ValidationError(
+    #                 self.error_messages['itself'],
+    #                 params={'folder': entity},
+    #                 code='itself',
+    #             )
+    #
+    #         if entity.already_in_children(parent_folder.id):
+    #             raise ValidationError(
+    #                 self.error_messages['loop'],
+    #                 params={'folder': entity},
+    #                 code='loop',
+    #             )
+    #
+    #         if not entity.category:
+    #             entity.category = parent_folder.category
+    #
+    #     return super()._bulk_clean_entity(entity, values)
 
 
 def get_merge_form_builder():
