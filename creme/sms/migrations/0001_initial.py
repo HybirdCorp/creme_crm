c454c9f05be06ca01bd97a609045977ab8be70f7
Guillaume Englert <genglert@hybird.org>
2020-10-12 10:54:40 +0200

    In 'AbstractMessageTemplate', the validation of subject/body is now done in model, instead of forms. Help text too. So the inner-edition of these fields has been re-activated.

--- creme/sms/migrations/0001_initial.py
+++ creme/sms/migrations/0001_initial.py
@@ -16,13 +16,27 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MessageTemplate',
             fields=[
-                ('cremeentity_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                                                         to='creme_core.CremeEntity', on_delete=CASCADE,
-                                                        )
+                (
+                    'cremeentity_ptr',
+                    models.OneToOneField(
+                        parent_link=True, auto_created=True, primary_key=True, serialize=False,
+                        to='creme_core.CremeEntity', on_delete=CASCADE,
+                    ),
                 ),
                 ('name', models.CharField(max_length=100, verbose_name='Name')),
                 ('subject', models.CharField(max_length=100, verbose_name='Subject')),
-                ('body', models.TextField(verbose_name='Body')),
+                # ('body', models.TextField(verbose_name='Body')),
+                (
+                    'body',
+                    models.TextField(
+                        verbose_name='Message body',
+                        help_text=(
+                            'Message with a maximum of 160 characters.\n'
+                            'Beware, the header matters (+ 3 characters) and the '
+                            'following characters count double: ^ { } \\ [ ~ ] | €'
+                        ),
+                    ),
+                ),
             ],
             options={
                 'swappable': 'SMS_TEMPLATE_MODEL',
@@ -35,12 +49,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MessagingList',
             fields=[
-                ('cremeentity_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                                                         to='creme_core.CremeEntity', on_delete=CASCADE,
-                                                        )
+                (
+                    'cremeentity_ptr',
+                    models.OneToOneField(
+                        parent_link=True, auto_created=True, primary_key=True, serialize=False,
+                        to='creme_core.CremeEntity', on_delete=CASCADE,
+                    ),
                 ),
                 ('name', models.CharField(max_length=80, verbose_name='Name of the messaging list')),
-                ('contacts', models.ManyToManyField(to=settings.PERSONS_CONTACT_MODEL, verbose_name='Contacts recipients', editable=False,)),
+                (
+                    'contacts',
+                    models.ManyToManyField(
+                        to=settings.PERSONS_CONTACT_MODEL,
+                        verbose_name='Contacts recipients', editable=False,
+                    )
+                ),
             ],
             options={
                 'swappable': 'SMS_MLIST_MODEL',
@@ -55,7 +78,13 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('phone', models.CharField(max_length=100, verbose_name='Number', blank=True)),
-                ('messaging_list', models.ForeignKey(verbose_name='Related messaging list', to=settings.SMS_MLIST_MODEL, on_delete=CASCADE)),
+                (
+                    'messaging_list',
+                    models.ForeignKey(
+                        to=settings.SMS_MLIST_MODEL,  on_delete=CASCADE,
+                        verbose_name='Related messaging list',
+                    )
+                ),
             ],
             options={
                 'ordering': ('phone',),
@@ -81,12 +110,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SMSCampaign',
             fields=[
-                ('cremeentity_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                                                         to='creme_core.CremeEntity', on_delete=CASCADE,
-                                                        )
+                (
+                    'cremeentity_ptr',
+                    models.OneToOneField(
+                        parent_link=True, auto_created=True, primary_key=True, serialize=False,
+                        to='creme_core.CremeEntity', on_delete=CASCADE,
+                    ),
                 ),
                 ('name', models.CharField(max_length=100, verbose_name='Name of the campaign')),
-                ('lists', models.ManyToManyField(to=settings.SMS_MLIST_MODEL, verbose_name='Related messaging lists', blank=True)),
+                (
+                    'lists',
+                    models.ManyToManyField(
+                        to=settings.SMS_MLIST_MODEL, blank=True,
+                        verbose_name='Related messaging lists',
+                    )
+                ),
             ],
             options={
                 'swappable': 'SMS_CAMPAIGN_MODEL',
@@ -102,8 +140,20 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('date', models.DateField(verbose_name='Date', editable=False)),
                 ('content', models.TextField(max_length=160, verbose_name='Generated message', editable=False)),
-                ('campaign', models.ForeignKey(related_name='sendings', verbose_name='Related campaign', to=settings.SMS_CAMPAIGN_MODEL, on_delete=CASCADE, editable=False)),
-                ('template', models.ForeignKey(verbose_name='Message template', to=settings.SMS_TEMPLATE_MODEL, on_delete=CASCADE, editable=False)),
+                (
+                    'campaign',
+                    models.ForeignKey(
+                        to=settings.SMS_CAMPAIGN_MODEL, on_delete=CASCADE, editable=False,
+                        related_name='sendings', verbose_name='Related campaign',
+                    )
+                ),
+                (
+                    'template',
+                    models.ForeignKey(
+                        to=settings.SMS_TEMPLATE_MODEL, on_delete=CASCADE,
+                        verbose_name='Message template', editable=False,
+                    )
+                ),
             ],
             options={
                 'verbose_name': 'Sending',
@@ -118,7 +168,13 @@ class Migration(migrations.Migration):
                 ('phone', models.CharField(max_length=100, verbose_name='Number')),
                 ('status', models.CharField(max_length=10, verbose_name='State')),
                 ('status_message', models.CharField(max_length=100, verbose_name='Full state', blank=True)),
-                ('sending', models.ForeignKey(related_name='messages', verbose_name='Sending', to='sms.Sending', on_delete=CASCADE)),
+                (
+                    'sending',
+                    models.ForeignKey(
+                        to='sms.Sending',  on_delete=CASCADE,
+                        verbose_name='Sending', related_name='messages',
+                    )
+                ),
             ],
             options={
                 'verbose_name': 'Message',
--- creme/sms/models/template.py
+++ creme/sms/models/template.py
@@ -18,18 +18,40 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+from django.core.exceptions import ValidationError
 from django.db.models import CharField, TextField
 from django.urls import reverse
+from django.utils.functional import lazy
+from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import pgettext_lazy
 
 from creme.creme_core.models import CremeEntity
 
+from ..encoding import SMS_MAX_LENGTH, gsm_encoded_content
+
+SPECIAL_CHARS = '^ { } \\ [ ~ ] | €'  # TODO: given by the backend ?
+
 
 class AbstractMessageTemplate(CremeEntity):
-    name    = CharField(_('Name'), max_length=100)
+    name = CharField(_('Name'), max_length=100)
     subject = CharField(_('Subject'), max_length=100)
-    body    = TextField(_('Body'))
+    body = TextField(
+        # _('Body'),
+        pgettext_lazy('sms', 'Message body'),
+        help_text=lazy(
+            (lambda: gettext(
+                'Message with a maximum of 160 characters.\n'
+                'Beware, the header matters (+ 3 characters) '
+                'and the following characters count double: {}'
+            ).format(SPECIAL_CHARS)),
+            str
+        )()
+    )
+
+    error_messages = {
+        'too_long': _('Message is too long (%(length)s > %(max_length)s)'),
+    }
 
     creation_label = pgettext_lazy('sms-template', 'Create a template')
     save_label     = pgettext_lazy('sms-template', 'Save the template')
@@ -45,6 +67,23 @@ class AbstractMessageTemplate(CremeEntity):
     def __str__(self):
         return self.name
 
+    def clean(self):
+        # super().clean()
+        subject = self.subject
+        # TODO: factorise
+        content = f'{subject} : {self.body}' if subject else self.body
+        encoded_length = len(gsm_encoded_content(content))
+
+        if encoded_length > SMS_MAX_LENGTH:
+            raise ValidationError(
+                self.error_messages['too_long'],
+                params={
+                    'length':     encoded_length,
+                    'max_length': SMS_MAX_LENGTH,
+                },
+                code='too_long',
+            )
+
     def get_absolute_url(self):
         return reverse('sms__view_template', args=(self.id,))
 
@@ -59,8 +98,8 @@ class AbstractMessageTemplate(CremeEntity):
     def get_lv_absolute_url():
         return reverse('sms__list_templates')
 
-    def resolve(self, date):
-        return self.subject + ' : ' + self.body
+    # def resolve(self, date):
+    #     return self.subject + ' : ' + self.body
 
 
 class MessageTemplate(AbstractMessageTemplate):
