b3c335654a2dc415195667417ba21b6c46d5c9b5
Fabre Florian <ffabre@hybird.org>
2020-10-27 06:41:59 +0100

    make gettext-collect & Fix some translations

--- creme/mobile/setting_keys.py
+++ creme/mobile/setting_keys.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from django.utils.translation import gettext as _
+from django.utils.text import format_lazy
+from django.utils.translation import gettext_lazy as _
 
 from creme.creme_core.core.setting_key import SettingKey
 
 LOCATION_MAP_URL = SettingKey(
     id='mobile-location_map_url',
-    description='\n'.join((
-        _("Url pattern to map & geolocation services.\n"
-          "Use {search} placeholder for the address and if geolocation is enabled, "
-          "{lat} & {lng} coordinates can be used too."
-         ),
-        ' − https://www.openstreetmap.org/search?query={search}',
-        ' − https://www.openstreetmap.org#map=18/{lat}/{lng}',
-        ' − https://www.google.com/maps/?q={search}',
-        ' − https://maps.google.com/maps/place/{lat},{lng}',
+    description=format_lazy((
+        '{title}\n'
+        ' − https://www.openstreetmap.org/search?query={{search}}\n'
+        ' − https://www.openstreetmap.org#map=18/{{lat}}/{{lng}}\n'
+        ' − https://www.google.com/maps/?q={{search}}\n'
+        ' − https://maps.google.com/maps/place/{{lat}},{{lng}}'
+    ), title=_(
+        "Url pattern to map & geolocation services.\n"
+        "Use {search} placeholder for the address and if geolocation is enabled, "
+        "{lat} & {lng} coordinates can be used too."
     )),
     app_label='mobile',
     type=SettingKey.STRING,
