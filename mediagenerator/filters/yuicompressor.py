570d730a8f2d9b7655b51b77c59a8616c4237ed7
Fabre Florian <ffabre@hybird.org>
2020-12-11 17:14:32 +0100

    Improve mediagenerator error output.

--- mediagenerator/filters/yuicompressor.py
+++ mediagenerator/filters/yuicompressor.py
@@ -1,3 +1,5 @@
+import re
+
 from django.conf import settings
 
 from mediagenerator.generators.bundles.base import SubProcessFilter
@@ -10,18 +12,41 @@ class YUICompressor(SubProcessFilter):
             f'YUICompressor only supports compilation to css and js. '
             f'The parent filter expects "{self.filetype}".')
 
+    def parse_errors(self, e):
+        pattern = re.compile(
+            r'^\[ERROR\]\s+(?P<line>[0-9]+):(?P<col>[0-9]+):(?P<message>.+)\n',
+            re.MULTILINE
+        )
+        errors = pattern.findall(str(e))
+
+        # The last error is the java exception raised by compressor, so ignore it
+        return [
+            (int(index), int(col), message) for index, col, message in errors[:-1]
+        ]
+
     def get_output(self, variation):
-        for input in self.get_input(variation):
+        for source in self.get_input(variation):
             try:
                 compressor = settings.YUICOMPRESSOR_PATH
                 yield self.run_process([
-                    'java', '-jar', compressor, '--charset', 'utf-8', '--type', self.filetype
-                ], input=input)
+                    'java', '-jar', compressor,
+                    '--charset', 'utf-8',
+                    '--type', self.filetype
+                ], input=source)
+            except SubProcessFilter.ProcessError as e:
+                errors = self.parse_errors(e.stderr)
+
+                if errors:
+                    message = self.format_lint_errors(errors, source)
+                else:
+                    f"The YUI Compressor has returned an error. {e.stderr}"
+
+                raise ValueError(message)
             except Exception as e:
                 raise ValueError(
                     "Failed to execute Java VM or yuicompressor. "
                     "Please make sure that you have installed Java "
                     "and that it's in your PATH and that you've configured "
                     "YUICOMPRESSOR_PATH in your settings correctly.\n"
-                    "Error was: {}".format(e)
+                    "{}".format(e)
                 )
