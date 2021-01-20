8395206cad1cca6aa7aa2b1fd7db66fb20b26fb7
Guillaume Englert <genglert@hybird.org>
2021-01-20 15:26:15 +0100

    Remove old commented code in "recurrents".

--- creme/recurrents/forms/recurrentgenerator.py
+++ creme/recurrents/forms/recurrentgenerator.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2020  Hybird
+#    Copyright (C) 2009-2021  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -47,8 +47,6 @@ class RecurrentGeneratorCreateForm(RecurrentGeneratorEditForm):
 
     def __init__(self, *args, **kwargs):
         warnings.warn('RecurrentGeneratorCreateForm is deprecated.', DeprecationWarning)
-
-        # from ..registry import recurrent_registry
 
         super().__init__(*args, **kwargs)
 
