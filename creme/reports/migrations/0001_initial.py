8da862a0325bfaafc0e16d9ea857b340a1b82795
Guillaume Englert <genglert@hybird.org>
2021-01-22 15:37:11 +0100

    Squash the migrations of 'reports'.

--- creme/reports/migrations/0001_initial.py
+++ creme/reports/migrations/0001_initial.py
@@ -12,7 +12,13 @@ EF_USER = 1
 class Migration(migrations.Migration):
     # replaces = [
     #     ('reports', '0001_initial'),
-    #     ('reports', '0006_v2_1__reportgraph_asc'),
+    #     ('reports', '0007_v2_2__abscissa_fields_rework01'),
+    #     ('reports', '0008_v2_2__abscissa_fields_rework02'),
+    #     ('reports', '0009_v2_2__abscissa_fields_rework03'),
+    #     ('reports', '0010_v2_2__ordinate_field_rework01'),
+    #     ('reports', '0011_v2_2__ordinate_field_rework02'),
+    #     ('reports', '0012_v2_2__ordinate_field_rework03'),
+    #     ('reports', '0013_v2_2__instance_bricks_data'),
     # ]
 
     initial = True
@@ -25,16 +31,25 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Report',
             fields=[
-                ('cremeentity_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                                                         to='creme_core.CremeEntity', on_delete=CASCADE,
-                                                        )
+                (
+                    'cremeentity_ptr',
+                    models.OneToOneField(
+                        parent_link=True, auto_created=True, primary_key=True, serialize=False,
+                        to='creme_core.CremeEntity', on_delete=CASCADE,
+                    )
                 ),
                 ('name', models.CharField(max_length=100, verbose_name='Name of the report')),
-                ('ct', creme_fields.EntityCTypeForeignKey(verbose_name='Entity type', to='contenttypes.ContentType')),
-                ('filter', models.ForeignKey(on_delete=PROTECT, verbose_name='Filter', blank=True,
-                                             to='creme_core.EntityFilter', null=True,
-                                             limit_choices_to={'filter_type': EF_USER},
-                                            )
+                (
+                    'ct',
+                    creme_fields.EntityCTypeForeignKey(verbose_name='Entity type', to='contenttypes.ContentType')
+                ),
+                (
+                    'filter',
+                    models.ForeignKey(
+                        on_delete=PROTECT, verbose_name='Filter', blank=True,
+                        to='creme_core.EntityFilter', null=True,
+                        limit_choices_to={'filter_type': EF_USER},
+                    )
                 ),
             ],
             options={
@@ -53,8 +68,14 @@ class Migration(migrations.Migration):
                 ('order', models.PositiveIntegerField()),
                 ('type', models.PositiveSmallIntegerField()),
                 ('selected', models.BooleanField(default=False)),
-                ('report', models.ForeignKey(related_name='fields', to=settings.REPORTS_REPORT_MODEL, on_delete=CASCADE)),
-                ('sub_report', models.ForeignKey(blank=True, to=settings.REPORTS_REPORT_MODEL, null=True, on_delete=CASCADE,)),
+                (
+                    'report',
+                    models.ForeignKey(related_name='fields', to=settings.REPORTS_REPORT_MODEL, on_delete=CASCADE)
+                ),
+                (
+                    'sub_report',
+                    models.ForeignKey(blank=True, to=settings.REPORTS_REPORT_MODEL, null=True, on_delete=CASCADE)
+                ),
             ],
             options={
                 'ordering': ('order',),
@@ -66,30 +87,60 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ReportGraph',
             fields=[
-                ('cremeentity_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                                                         to='creme_core.CremeEntity', on_delete=CASCADE,
-                                                        )
+                (
+                    'cremeentity_ptr',
+                    models.OneToOneField(
+                        parent_link=True, auto_created=True, primary_key=True, serialize=False,
+                        to='creme_core.CremeEntity', on_delete=CASCADE,
+                    )
                 ),
                 ('name', models.CharField(max_length=100, verbose_name='Name of the graph')),
-                # ('abscissa', models.CharField(verbose_name='X axis', max_length=100, editable=False)),
-                ('abscissa', models.CharField(verbose_name='X axis (field)', max_length=100, editable=False)),
-                ('ordinate', models.CharField(verbose_name='Y axis', max_length=100, editable=False)),
-                # ('type', models.PositiveIntegerField(verbose_name='Grouping', editable=False,
-                ('type', models.PositiveIntegerField(verbose_name='X axis (grouping)', editable=False,
-                                                     choices=[(1, 'By days'), (2, 'By months'), (3, 'By years'), (4, 'By X days'),
-                                                              (5, 'By values'), (6, 'By values (of related entities)'),
-                                                              (11, 'By days (custom field)'), (12, 'By months (custom field)'),
-                                                              (13, 'By years (custom field)'), (14, 'By X days (custom field)'),
-                                                              (15, 'By values (of custom choices)'),
-                                                             ],
-                                                    )
+                (
+                    # 'abscissa',
+                    'abscissa_cell_value',
+                    models.CharField(verbose_name='X axis (field)', max_length=100, editable=False)
                 ),
-                # ('days', models.PositiveIntegerField(null=True, verbose_name='Days', blank=True)),
-                ('days', models.PositiveIntegerField(null=True, verbose_name='Days', blank=True, editable=False)),
-                ('is_count', models.BooleanField(default=False, verbose_name='Make a count instead of aggregate?')),
+                (
+                    'abscissa_parameter',
+                    models.TextField(verbose_name='X axis parameter', editable=False, null=True)
+                ),
+                # ('ordinate', models.CharField(verbose_name='Y axis', max_length=100, editable=False)),
+                (
+                    'ordinate_type',
+                    models.CharField(
+                        verbose_name='Y axis (type)', editable=False, max_length=100, default='',
+                        choices=[
+                            ('count', 'Count'), ('avg', 'Average'), ('max', 'Maximum'),
+                            ('min', 'Minimum'), ('sum', 'Sum'),
+                        ],
+                    )
+                ),
+                (
+                    'ordinate_cell_key',
+                    models.CharField(verbose_name='Y axis (field)', editable=False, max_length=100, default='')
+                ),
+                (
+                    # 'type',
+                    'abscissa_type',
+                    models.PositiveIntegerField(
+                        verbose_name='X axis (grouping)', editable=False,
+                        choices=[
+                            (1, 'By days'), (2, 'By months'), (3, 'By years'), (4, 'By X days'),
+                            (5, 'By values'), (6, 'By values (of related entities)'),
+                            (11, 'By days (custom field)'), (12, 'By months (custom field)'),
+                            (13, 'By years (custom field)'), (14, 'By X days (custom field)'),
+                            (15, 'By values (of custom choices)'),
+                        ],
+                    )
+                ),
+                # ('days', models.PositiveIntegerField(null=True, verbose_name='Days', blank=True, editable=False)),
+                # ('is_count', models.BooleanField(default=False, verbose_name='Make a count instead of aggregate?')),
                 ('chart', models.CharField(max_length=100, null=True, verbose_name='Chart type')),
                 ('asc', models.BooleanField(default=True, editable=False, verbose_name='ASC order')),
-                ('linked_report', models.ForeignKey(editable=False, to=settings.REPORTS_REPORT_MODEL, on_delete=CASCADE)),
+                (
+                    'linked_report',
+                    models.ForeignKey(editable=False, to=settings.REPORTS_REPORT_MODEL, on_delete=CASCADE)
+                ),
             ],
             options={
                 'swappable': 'REPORTS_GRAPH_MODEL',
@@ -106,15 +157,20 @@ class Migration(migrations.Migration):
             migrations.CreateModel(
                 name='FakeReportsFolder',
                 fields=[
-                    ('cremeentity_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                                                             to='creme_core.CremeEntity', on_delete=CASCADE,
-                                                            )
+                    (
+                        'cremeentity_ptr',
+                        models.OneToOneField(
+                            parent_link=True, auto_created=True, primary_key=True, serialize=False,
+                            to='creme_core.CremeEntity', on_delete=CASCADE,
+                        )
                     ),
                     ('title', models.CharField(unique=True, max_length=100, verbose_name='Title')),
-                    # ('description', models.TextField(null=True, verbose_name='Description', blank=True)),
-                    ('parent', models.ForeignKey(on_delete=PROTECT, verbose_name='Parent folder',
-                                                 to='reports.FakeReportsFolder', null=True,
-                                                )
+                    (
+                        'parent',
+                        models.ForeignKey(
+                            on_delete=PROTECT, verbose_name='Parent folder',
+                            to='reports.FakeReportsFolder', null=True,
+                        )
                     ),
                 ],
                 options={
@@ -127,13 +183,20 @@ class Migration(migrations.Migration):
             migrations.CreateModel(
                 name='FakeReportsDocument',
                 fields=[
-                    ('cremeentity_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                                                             to='creme_core.CremeEntity', on_delete=CASCADE,
-                                                            )
+                    (
+                        'cremeentity_ptr',
+                        models.OneToOneField(
+                            parent_link=True, auto_created=True, primary_key=True, serialize=False,
+                            to='creme_core.CremeEntity', on_delete=CASCADE,
+                        )
                     ),
                     ('title', models.CharField(max_length=100, verbose_name='Title')),
-                    # ('description', models.TextField(null=True, verbose_name='Description', blank=True)),
-                    ('linked_folder', models.ForeignKey(on_delete=PROTECT, verbose_name='Folder', to='reports.FakeReportsFolder')),
+                    (
+                        'linked_folder',
+                        models.ForeignKey(
+                            on_delete=PROTECT, verbose_name='Folder', to='reports.FakeReportsFolder',
+                        )
+                    ),
                 ],
                 options={
                     'ordering': ('title',),
@@ -145,9 +208,12 @@ class Migration(migrations.Migration):
             migrations.CreateModel(
                 name='Guild',
                 fields=[
-                    ('cremeentity_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                                                             to='creme_core.CremeEntity', on_delete=CASCADE,
-                                                            )
+                    (
+                        'cremeentity_ptr',
+                        models.OneToOneField(
+                            parent_link=True, auto_created=True, primary_key=True, serialize=False,
+                            to='creme_core.CremeEntity', on_delete=CASCADE,
+                        )
                     ),
                     ('name', models.CharField(max_length=100, verbose_name='Name')),
                     ('members', models.ManyToManyField(to='creme_core.FakeContact', verbose_name='Members')),
