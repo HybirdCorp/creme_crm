f7a1ecf8843cafbd7d1891229e855ba7957d7e93
Guillaume Englert <genglert@hybird.org>
2021-01-22 22:42:37 +0100

    Remove the deprecated code of 'opportunities'.

--- creme/opportunities/forms/opportunity.py
+++ creme/opportunities/forms/opportunity.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2020  Hybird
+#    Copyright (C) 2009-2021  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,9 +18,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-import warnings
-
-from django.forms import Form, ModelChoiceField
+# import warnings
+from django.forms import ModelChoiceField  # Form
 from django.utils.translation import gettext_lazy as _
 
 from creme import persons
@@ -34,66 +33,65 @@ Opportunity = get_opportunity_model()
 Organisation = persons.get_organisation_model()
 Contact = persons.get_contact_model()
 
-
-class OpportunityForm(CremeEntityForm):
-    class Meta(CremeEntityForm.Meta):
-        model = Opportunity
-
-    def __init__(self, *args, **kwargs):
-        warnings.warn('OpportunityForm is deprecated.', DeprecationWarning)
-        super().__init__(*args, **kwargs)
-
-
-class TargetMixin(Form):
-    target = GenericEntityField(
-        label=_('Target organisation / contact'),
-        models=[Organisation, Contact],
-    )
-
-    def clean_target(self):
-        warnings.warn('TargetMixin is deprecated.', DeprecationWarning)
-
-        self.instance.target = target = self.cleaned_data['target']
-
-        return target
+# class OpportunityForm(CremeEntityForm):
+#     class Meta(CremeEntityForm.Meta):
+#         model = Opportunity
+#
+#     def __init__(self, *args, **kwargs):
+#         warnings.warn('OpportunityForm is deprecated.', DeprecationWarning)
+#         super().__init__(*args, **kwargs)
 
 
-class EmitterMixin(Form):
-    emitter = ModelChoiceField(
-        label=_('Concerned organisation'),
-        queryset=Organisation.objects.filter_managed_by_creme(),
-        empty_label=None,
-    )
-
-    def clean_emitter(self):
-        warnings.warn('EmitterMixin is deprecated.', DeprecationWarning)
-
-        self.instance.emitter = emitter = validate_linkable_entity(
-            entity=self.cleaned_data['emitter'],
-            user=self.user,
-        )
-
-        return emitter
+# class TargetMixin(Form):
+#     target = GenericEntityField(
+#         label=_('Target organisation / contact'),
+#         models=[Organisation, Contact],
+#     )
+#
+#     def clean_target(self):
+#         warnings.warn('TargetMixin is deprecated.', DeprecationWarning)
+#
+#         self.instance.target = target = self.cleaned_data['target']
+#
+#         return target
 
 
-class OpportunityEditionForm(TargetMixin, OpportunityForm):
-    def __init__(self, *args, **kwargs):
-        warnings.warn('OpportunityEditionForm is deprecated.', DeprecationWarning)
-        super().__init__(*args, **kwargs)
-        self.fields['target'].initial = self.instance.target
+# class EmitterMixin(Form):
+#     emitter = ModelChoiceField(
+#         label=_('Concerned organisation'),
+#         queryset=Organisation.objects.filter_managed_by_creme(),
+#         empty_label=None,
+#     )
+#
+#     def clean_emitter(self):
+#         warnings.warn('EmitterMixin is deprecated.', DeprecationWarning)
+#
+#         self.instance.emitter = emitter = validate_linkable_entity(
+#             entity=self.cleaned_data['emitter'],
+#             user=self.user,
+#         )
+#
+#         return emitter
 
 
-class OpportunityCreationForm(TargetMixin, EmitterMixin, OpportunityForm):
-    def __init__(self, *args, **kwargs):
-        warnings.warn('OpportunityCreationForm is deprecated.', DeprecationWarning)
-        super().__init__(*args, **kwargs)
+# class OpportunityEditionForm(TargetMixin, OpportunityForm):
+#     def __init__(self, *args, **kwargs):
+#         warnings.warn('OpportunityEditionForm is deprecated.', DeprecationWarning)
+#         super().__init__(*args, **kwargs)
+#         self.fields['target'].initial = self.instance.target
 
 
-class TargetedOpportunityCreationForm(EmitterMixin, OpportunityForm):
-    def __init__(self, target, *args, **kwargs):
-        warnings.warn('TargetedOpportunityCreationForm is deprecated.', DeprecationWarning)
-        super().__init__(*args, **kwargs)
-        self.instance.target = target
+# class OpportunityCreationForm(TargetMixin, EmitterMixin, OpportunityForm):
+#     def __init__(self, *args, **kwargs):
+#         warnings.warn('OpportunityCreationForm is deprecated.', DeprecationWarning)
+#         super().__init__(*args, **kwargs)
+
+
+# class TargetedOpportunityCreationForm(EmitterMixin, OpportunityForm):
+#     def __init__(self, target, *args, **kwargs):
+#         warnings.warn('TargetedOpportunityCreationForm is deprecated.', DeprecationWarning)
+#         super().__init__(*args, **kwargs)
+#         self.instance.target = target
 
 
 class OpportunitySubCell(CustomFormExtraSubCell):
