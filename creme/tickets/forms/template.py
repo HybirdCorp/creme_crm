36b3fb5c09566c7b62037177c3771d1802caada5
Guillaume Englert <genglert@hybird.org>
2021-01-25 14:32:11 +0100

    Remove the deprecated code of 'tickets'.

--- creme/tickets/forms/template.py
+++ creme/tickets/forms/template.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2020  Hybird
+#    Copyright (C) 2009-2021  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,26 +18,23 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-import warnings
-
+# import warnings
+# from .. import get_tickettemplate_model
 from creme.creme_core.forms import CremeEntityForm
 
-from .. import get_tickettemplate_model
-
-
-class TicketTemplateForm(CremeEntityForm):
-    class Meta(CremeEntityForm.Meta):
-        model = get_tickettemplate_model()
-
-    def __init__(self, *args, **kwargs):
-        warnings.warn('TicketTemplateForm is deprecated.', DeprecationWarning)
-        super().__init__(*args, **kwargs)
-
-
-class TicketTemplateRecurrentsForm(TicketTemplateForm):
-    def __init__(self, ct, *args, **kwargs):  # 'ct' arg => see RecurrentGeneratorWizard
-        warnings.warn('TicketTemplateRecurrentsForm is deprecated.', DeprecationWarning)
-        super().__init__(*args, **kwargs)
+# class TicketTemplateForm(CremeEntityForm):
+#     class Meta(CremeEntityForm.Meta):
+#         model = get_tickettemplate_model()
+#
+#     def __init__(self, *args, **kwargs):
+#         warnings.warn('TicketTemplateForm is deprecated.', DeprecationWarning)
+#         super().__init__(*args, **kwargs)
+#
+#
+# class TicketTemplateRecurrentsForm(TicketTemplateForm):
+#     def __init__(self, ct, *args, **kwargs):  # 'ct' arg => see RecurrentGeneratorWizard
+#         warnings.warn('TicketTemplateRecurrentsForm is deprecated.', DeprecationWarning)
+#         super().__init__(*args, **kwargs)
 
 
 class BaseTemplateCustomForm(CremeEntityForm):
