87aa6fe46907aaaba7e87c35eab5610e0757fc5d
Guillaume Englert <genglert@hybird.org>
2021-01-25 11:34:37 +0100

    Remove the deprecated code of 'polls'.

--- creme/polls/forms/campaign.py
+++ creme/polls/forms/campaign.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2013-2020  Hybird
+#    Copyright (C) 2013-2021  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,17 +18,17 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-import warnings
-
-from creme.creme_core.forms import CremeEntityForm
-
-from .. import get_pollcampaign_model
-
-
-class PollCampaignForm(CremeEntityForm):
-    class Meta(CremeEntityForm.Meta):
-        model = get_pollcampaign_model()
-
-    def __init__(self, *args, **kwargs):
-        warnings.warn('PollCampaignForm is deprecated.', DeprecationWarning)
-        super().__init__(*args, **kwargs)
+# import warnings
+#
+# from creme.creme_core.forms import CremeEntityForm
+#
+# from .. import get_pollcampaign_model
+#
+#
+# class PollCampaignForm(CremeEntityForm):
+#     class Meta(CremeEntityForm.Meta):
+#         model = get_pollcampaign_model()
+#
+#     def __init__(self, *args, **kwargs):
+#         warnings.warn('PollCampaignForm is deprecated.', DeprecationWarning)
+#         super().__init__(*args, **kwargs)
