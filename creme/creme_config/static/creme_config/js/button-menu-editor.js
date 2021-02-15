ae6ebb3e8343ea1cf50cdbc053d92e1025f0f0aa
Fabre Florian <ffabre@hybird.org>
2021-02-15 12:04:09 +0100

    Refs #153 Refactor and add test coverage for ButtonMenuEditor

---
+++ creme/creme_config/static/creme_config/js/button-menu-editor.js
@@ -0,0 +1,80 @@
+/*******************************************************************************
+    Creme is a free/open-source Customer Relationship Management software
+    Copyright (C) 2020-2021  Hybird
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+
+(function($) {
+    "use strict";
+
+function __buildButtonChoice(choice) {
+    return $((
+        '<span class="menu_button" title="${description}">' +
+            '${label}<input type="hidden" name="${name}" value="${value}"/>' +
+        '</span>'
+    ).template(choice)).prop("disabled", !choice.selected);
+}
+
+function __buildWidget(widget, choices) {
+    choices.forEach(
+        function (choice) {
+            if (choice.selected) {
+                widget.find(".widget-selected .widget-container").append(
+                    __buildButtonChoice(choice)
+                );
+            } else {
+                widget.find(".widget-available .widget-container").append(
+                    __buildButtonChoice(choice)
+                );
+            }
+        }
+    );
+}
+
+creme.ButtonMenuEditor = creme.component.Component.sub({
+    _init_: function(element, options) {
+        options = options || {};
+
+        if (options.optionsId) {
+            __buildWidget(element, JSON.parse(
+                $('#${optionsId}'.template(options)).text() || '[]'
+            ));
+        }
+
+        function onSortEventHandler(event) {
+            element.find(".widget-available input").prop("disabled", true);
+            element.find(".widget-selected input").prop("disabled", false);
+        };
+
+        var groupName = element.attr("id");
+
+        this._availableList = new Sortable(
+            element.find(".widget-available .widget-container").get(0), {
+                group: groupName,
+                animation: 150,
+                sort: false,
+                onSort: onSortEventHandler
+            });
+
+        this._selectedList = new Sortable(
+            element.find(".widget-selected .widget-container").get(0), {
+                group: groupName,
+                animation: 150,
+                onSort: onSortEventHandler
+            });
+    }
+});
+
+}(jQuery));
---
+++ creme/creme_config/static/creme_config/js/tests/button-menu-editor.js
@@ -0,0 +1,85 @@
+(function($) {
+"use strict";
+
+QUnit.module("creme.ButtonMenuEditor", new QUnitMixin(QUnitEventMixin,
+                                                      QUnitMouseMixin, {
+    createJSONDataHtml: function(data) {
+        var html = '<script type="application/json" id="buttons-widget-choices">${data}</script>'.template({
+            data: JSON.stringify(data)
+        });
+
+        return html;
+    },
+
+    createButtonEditorHtml: function(options) {
+        options = $.extend({
+            id: 'id'
+        }, options || {});
+
+        var html = (
+            '<div class="buttonmenu-edit-widget" id="${id}">' +
+                '<div class="widget-available buttons-list instance-buttons">' +
+                    '<div class="buttons-list-header">{% trans "Available buttons" %}</div>' +
+                    '<div class="widget-container"></div>' +
+                '</div>' +
+                '<div class="widget-selected buttons-list instance-buttons">' +
+                    '<div class="buttons-list-header">{% trans "Selected buttons" %}</div>' +
+                    '<div class="widget-container" style="width: 100px;height: 100px;"></div>' +
+                '</div>' +
+            '</div>'
+        ).template(options);
+
+        return html;
+    }
+}));
+
+QUnit.test('creme.ButtonMenuEditor (empty)', function(assert) {
+    var element = $(this.createButtonEditorHtml()).appendTo(this.qunitFixture());
+    var controller = new creme.ButtonMenuEditor(element); /* eslint-disable-line */
+
+    equal(element.find('.widget-available .widget-container .menu_button').length, 0);
+    equal(element.find('.widget-selected .widget-container .menu_button').length, 0);
+});
+
+QUnit.test('creme.ButtonMenuEditor (invalid data)', function(assert) {
+    var element = $(this.createButtonEditorHtml()).appendTo(this.qunitFixture());
+    var controller = new creme.ButtonMenuEditor(element); /* eslint-disable-line */
+
+    equal(element.find('.widget-available .widget-container .menu_button').length, 0);
+    equal(element.find('.widget-selected .widget-container .menu_button').length, 0);
+});
+
+QUnit.test('creme.ButtonMenuEditor (default)', function(assert) {
+    var element = $(this.createButtonEditorHtml()).appendTo(this.qunitFixture());
+    this.qunitFixture().append($('<script type="application/json" id="buttons-widget-choices">invalid !</script>'));
+
+    this.assertRaises(function() {
+        return new creme.ButtonMenuEditor(element, {  /* eslint-disable-line */
+            optionsId: 'buttons-widget-choices'
+        });
+    }, Error);
+
+    equal(element.find('.widget-available .widget-container .menu_button').length, 0);
+    equal(element.find('.widget-selected .widget-container .menu_button').length, 0);
+});
+
+QUnit.test('creme.ButtonMenuEditor (selected)', function(assert) {
+    var element = $(this.createButtonEditorHtml()).appendTo(this.qunitFixture());
+
+    this.qunitFixture().append($(this.createJSONDataHtml([{
+        name: 'button-a', value: 'a', label: 'Button A', description: 'This is a button'
+    }, {
+        name: 'button-b', value: 'b', label: 'Button B', description: 'This is a button', selected: true
+    }])));
+
+    var controller = new creme.ButtonMenuEditor(element, {  /* eslint-disable-line */
+        optionsId: 'buttons-widget-choices'
+    });
+
+    equal(element.find('.widget-available .widget-container .menu_button').length, 1);
+    equal(element.find('.widget-selected .widget-container .menu_button').length, 1);
+});
+
+// TODO : drag n drop test here
+
+}(jQuery));
