79b8e631f65cfe057543a2230ff391ecf0945b3f
Guillaume Englert <genglert@hybird.org>
2021-07-12 12:06:30 +0200

    In assistants' forms, the field "user" is the first field (again).

--- creme/assistants/models/memo.py
+++ creme/assistants/models/memo.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2020  Hybird
+#    Copyright (C) 2009-2021  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -33,10 +33,10 @@ class MemoManager(models.Manager):
 
 
 class Memo(creme_models.CremeModel):
+    user          = creme_fields.CremeUserForeignKey(verbose_name=_('Owner user'))
     content       = models.TextField(_('Content'))
     on_homepage   = models.BooleanField(_('Displayed on homepage'), blank=True, default=False)
     creation_date = creme_fields.CreationDateTimeField(_('Creation date'), editable=False)
-    user          = creme_fields.CremeUserForeignKey(verbose_name=_('Owner user'))
 
     entity_content_type = creme_fields.EntityCTypeForeignKey(related_name='+', editable=False)
     entity = models.ForeignKey(
