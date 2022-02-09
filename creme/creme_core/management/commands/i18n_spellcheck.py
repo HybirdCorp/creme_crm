d79706b9c2284b1376c5f1f7cf077a528abd038c
Hugo Smett <hsmett@hybird.org>
2022-02-09 11:02:02 +0100

    i18n: Update headers; Compile messages

--- creme/creme_core/management/commands/i18n_spellcheck.py
+++ creme/creme_core/management/commands/i18n_spellcheck.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #
-# Copyright (c) 2009-2022 Hybird
+# Copyright (c) 2022 Hybird
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
