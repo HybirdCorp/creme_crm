78b331853a9e96234fbcabdecbf02c237da01b11
Fabre Florian <ffabre@hybird.org>
2022-04-11 19:25:10 +0200

    Add JQUERY_MIGRATE_MUTE setting to disable almost all the annoying warnings

--- creme/creme_core/context_processors.py
+++ creme/creme_core/context_processors.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2018  Hybird
+#    Copyright (C) 2009-2022  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -59,6 +59,12 @@ def get_version(request):
 
 def get_hidden_value(request):
     return {'HIDDEN_VALUE': settings.HIDDEN_VALUE}
+
+
+def get_jqmigrate_mute(request):
+    return {
+        'JQUERY_MIGRATE_MUTE': getattr(settings, 'JQUERY_MIGRATE_MUTE', False)
+    }
 
 
 def get_django_version(request):
