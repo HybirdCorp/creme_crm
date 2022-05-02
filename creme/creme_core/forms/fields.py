1cb4953568026c4c4d9c4b57db5eae83b055a499
Guillaume Englert <genglert@hybird.org>
2022-05-02 21:03:23 +0200

    Add doc-strings in 'creme_core.forms.fields'.

--- creme/creme_core/forms/fields.py
+++ creme/creme_core/forms/fields.py
@@ -1268,6 +1268,13 @@ class FilteredEntityTypeField(JSONField):
 
 
 class OptionalField(fields.MultiValueField):
+    """Base class for fields combining a BooleanField & another field, the first
+    one controlling the inhibition of the second one: when the value of the
+    BooleanField is <False>, the value of the second field is ignored.
+
+    Hint: in the child classes, you should probably use a widget inheriting
+          'OptionalWidget'.
+    """
     sub_field = fields.Field
     widget: Type[core_widgets.OptionalWidget] = core_widgets.OptionalWidget
 
@@ -1654,6 +1661,9 @@ class DurationField(fields.MultiValueField):
 
 
 class ChoiceOrCharField(fields.MultiValueField):
+    """Field which combines a ChoiceField & a CharField ; the user can fill the
+    second one if no choice is satisfying.
+    """
     widget = core_widgets.ChoiceOrCharWidget
 
     default_error_messages = {
@@ -1786,6 +1796,7 @@ class CTypeChoiceField(fields.Field):
 
 
 class MultiCTypeChoiceField(CTypeChoiceField):
+    """Multiple version of CTypeChoiceField."""
     widget = core_widgets.UnorderedMultipleChoiceWidget  # Beware: use Choice inner class
 
     def _build_ctype_choices(self, ctypes):
@@ -1826,12 +1837,16 @@ class MultiCTypeChoiceField(CTypeChoiceField):
 
 
 class EntityCTypeChoiceField(CTypeChoiceField):
+    """Version of CTypeChoiceField where all ContentTypes correspond to classes
+    inheriting CremeEntity.
+    """
     def __init__(self, *, ctypes=None, **kwargs):
         ctypes = ctypes or entity_ctypes
         super().__init__(ctypes=ctypes, **kwargs)
 
 
 class MultiEntityCTypeChoiceField(MultiCTypeChoiceField):
+    """Multiple version of EntityCTypeChoiceField."""
     def __init__(self, *, ctypes=None, **kwargs):
         ctypes = ctypes or entity_ctypes
         super().__init__(ctypes=ctypes, **kwargs)
@@ -2041,6 +2056,9 @@ class EnhancedModelMultipleChoiceField(mforms.ModelMultipleChoiceField):
 
 
 class ReadonlyMessageField(fields.CharField):
+    """Field made to display a message to the user.
+    POSTed value is ignored, the given default value is always returned.
+    """
     widget = core_widgets.Label
 
     def __init__(self, *, label, initial='', widget=None, return_value=None):
