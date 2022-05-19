743502ff6011606729d8c43e6829f5d609cfc8de
Guillaume Englert <genglert@hybird.org>
2022-05-19 18:35:55 +0200

    Avoid the RelationTypes with property constraints in the VCF import form.

--- creme/vcfs/forms/vcf.py
+++ creme/vcfs/forms/vcf.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2021  Hybird
+#    Copyright (C) 2009-2022  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -265,6 +265,9 @@ class VcfImportForm(CremeModelForm):
             # object_ctypes=_get_ct(Organisation),
             symmetric_type__subject_ctypes=_get_ct(Organisation),
             is_internal=False,
+            subject_properties__isnull=True,  # TODO: propose to add properties?
+            # TODO: validates Organisation properties instead of ignoring the RelationType
+            symmetric_type__subject_properties__isnull=True,
         )
 
         self._hide_fields()
