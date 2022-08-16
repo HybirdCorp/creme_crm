0c67ca897c74192822397590abb89aed5b8dcc6b
Fabre Florian <ffabre@hybird.org>
2022-08-16 10:21:07 +0200

    Replace jqPlot by flexbox stack bars in polls stats

--- creme/polls/tests/test_templatetags.py
+++ creme/polls/tests/test_templatetags.py
@@ -1,20 +1,12 @@
 from functools import partial
-from json import loads
 
 from django.template import Context, Template
-from django.utils.html import escape
 from django.utils.translation import gettext as _
 
 from ..core import PollLineType
-from ..models import PollFormLineCondition, PollFormSection, PollReplyLine
-from ..utils import NodeStyle, SectionTree, StatsTree
-from .base import (
-    PollForm,
-    PollReply,
-    _PollsTestCase,
-    skipIfCustomPollForm,
-    skipIfCustomPollReply,
-)
+from ..models import PollFormLineCondition, PollFormSection
+from ..utils import NodeStyle, SectionTree
+from .base import PollForm, _PollsTestCase, skipIfCustomPollForm
 
 
 @skipIfCustomPollForm
@@ -110,138 +102,4 @@ class PollsTagsTestCase(_PollsTestCase):
             'style="background-color: #D8E5EB;" '
             'style=""',
             render.strip(),
-        )
-
-    def test_stats_chart01(self):
-        "No stat."
-        user = self.create_user()
-        pform = PollForm.objects.create(user=user, name='Form#1')
-
-        self._get_formline_creator(pform=pform)('How many swallows?', qtype=PollLineType.INT)
-
-        with self.assertNoException():
-            render = Template(
-                r'{% load polls_tags %}'
-                r'{% for node in nodes %}{% poll_stats_chart node %}{% endfor %}'
-            ).render(Context({'nodes': SectionTree(pform)}))
-
-        self.assertEqual(escape(_('[An error occurred]')), render.strip())
-
-    @skipIfCustomPollReply
-    def test_stats_chart02(self):
-        "INT stat."
-        user = self.create_user()
-        pform = PollForm.objects.create(user=user, name='Form#1')
-        fline = self._get_formline_creator(pform=pform)(
-            'How many swallows?', qtype=PollLineType.INT,
-        )
-
-        def create_reply(i, answer):
-            reply = PollReply.objects.create(user=user, name=f'Reply#{i}', pform=pform)
-            PollReplyLine.objects.create(
-                preply=reply, pform_line=fline, type=fline.type, answer=answer,
-            )
-
-        create_reply(1, answer=12)
-        create_reply(2, answer=23)
-
-        with self.assertNoException():
-            render = Template(
-                r'{% load polls_tags %}'
-                r'{% for node in nodes %}{% poll_stats_chart node %}{% endfor %}'
-            ).render(Context({'nodes': StatsTree(pform)}))
-
-        tree = self.get_html_tree(render)
-        div_node = self.get_html_node_or_fail(tree, './/div')
-        self.assertIn('poll-stat-chart', div_node.attrib.get('class'))
-
-        script_node = self.get_html_node_or_fail(div_node, './/script')
-
-        with self.assertNoException():
-            chart_data = loads(script_node.text.replace('<!--', '').replace('-->', ''))
-
-        self.assertIsInstance(chart_data, dict)
-
-        with self.assertNoException():
-            yaxis = chart_data['options']['axes']['yaxis']
-
-        self.assertDictEqual(
-            {
-                'pad': 1.0,
-                'renderer': 'jqplot.CategoryAxisRenderer',
-                'tickRenderer': 'jqplot.AxisTickRenderer',
-                'tickOptions': {
-                    'textColor': 'black',
-                    'fontSize': '8pt',
-                },
-                'labelOptions': {'show': False},
-            },
-            yaxis,
-        )
-
-        with self.assertNoException():
-            values = chart_data['data']
-
-        self.assertListEqual(
-            [[[50.0, '12'], [50.0, '23']]],
-            values,
-        )
-
-    @skipIfCustomPollReply
-    def test_stats_chart03(self):
-        "BOOL stat."
-        user = self.create_user()
-        pform = PollForm.objects.create(user=user, name='Form#1')
-        fline = self._get_formline_creator(pform=pform)(
-            'Do you like swallows?', qtype=PollLineType.BOOL,
-        )
-
-        def create_reply(i, answer):
-            reply = PollReply.objects.create(user=user, name=f'Reply#{i}', pform=pform)
-            PollReplyLine.objects.create(
-                preply=reply, pform_line=fline, type=fline.type, answer=answer,
-            )
-
-        create_reply(1, answer=True)
-        create_reply(2, answer=False)
-
-        with self.assertNoException():
-            render = Template(
-                r'{% load polls_tags %}'
-                r'{% for node in nodes %}{% poll_stats_chart node %}{% endfor %}'
-            ).render(Context({'nodes': StatsTree(pform)}))
-
-        tree = self.get_html_tree(render)
-        div_node = self.get_html_node_or_fail(tree, './/div')
-        self.assertIn('poll-stat-chart', div_node.attrib.get('class'))
-
-        script_node = self.get_html_node_or_fail(div_node, './/script')
-
-        with self.assertNoException():
-            chart_data = loads(script_node.text.replace('<!--', '').replace('-->', ''))
-
-        self.assertIsInstance(chart_data, dict)
-
-        with self.assertNoException():
-            yaxis = chart_data['options']['axes']['yaxis']
-
-        self.assertDictEqual(
-            {
-                'renderer': 'jqplot.CategoryAxisRenderer',
-                'tickOptions': {'show': False},
-                'labelOptions': {'show': False},
-                'ticks': [''],
-            },
-            yaxis,
-        )
-
-        with self.assertNoException():
-            values = chart_data['data']
-
-        self.assertListEqual(
-            [
-                [[50.0, 1, f"{_('No')} \\u2212 50.0 %"]],
-                [[50.0, 1, f"{_('Yes')} \\u2212 50.0 %"]]
-            ],
-            values,
         )
