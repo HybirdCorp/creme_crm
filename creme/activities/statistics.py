6d2cef46e24c3d4282fa4a3711fb097c700a3ddb
Guillaume Englert <genglert@hybird.org>
2022-09-15 17:30:50 +0200

    Remove a warning message about using ngettext() with a float number.

--- creme/activities/statistics.py
+++ creme/activities/statistics.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2018-2020  Hybird
+#    Copyright (C) 2018-2022  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -64,7 +64,9 @@ class AveragePerMonthStatistics:
 
         if count:
             average = count / months
-            stat = (item['messages'] % average).format(
+            # NB: ngettext() warns if you pass a float ;
+            #     is round() always the right plural rules in all languages??
+            stat = (item['messages'] % round(average)).format(
                 count=number_format(average, decimal_pos=1, use_l10n=True),
             )
         else:
