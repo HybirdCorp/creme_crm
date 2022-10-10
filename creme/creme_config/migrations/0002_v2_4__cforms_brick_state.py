3ec01951168ed3aa640c43fb5d2a245b5bf4ed28
Guillaume Englert <genglert@hybird.org>
2022-10-10 14:01:49 +0200

    Fix a bug in a migration with JSONField not managed correctly.

--- creme/creme_config/migrations/0002_v2_4__cforms_brick_state.py
+++ creme/creme_config/migrations/0002_v2_4__cforms_brick_state.py
@@ -1,3 +1,5 @@
+import json
+
 from django.db import migrations
 
 BRICK_STATE_SHOW_CFORMS_DETAILS  = 'creme_config-show_customforms_details'
@@ -5,10 +7,15 @@ BRICK_STATE_SHOW_CFORMS_DETAILS  = 'creme_config-show_customforms_details'
 
 def convert_brick_state(apps, schema_editor):
     for state in apps.get_model('creme_core', 'BrickState').objects.filter(
-            brick_id='block_creme_config-custom_forms',
+        brick_id='block_creme_config-custom_forms',
     ):
-        ct_id = state.json_extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS]
-        state.json_extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS] = {'ctype': ct_id}
+        # ct_id = state.json_extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS]
+        extra_data = json.loads(state.json_extra_data)
+        ct_id = extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS]
+
+        # state.json_extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS] = {'ctype': ct_id}
+        extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS] = {'ctype': ct_id}
+        state.json_extra_data = json.dumps(extra_data)
         state.save()
 
 
