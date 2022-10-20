4ff57f69b37d57a2093c4f36c3454186200a98f2
Guillaume Englert <genglert@hybird.org>
2022-10-20 16:54:12 +0200

    Fix a crash in Product/Service form with Category without SubCategory.

--- creme/products/forms/base.py
+++ creme/products/forms/base.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2014-2021  Hybird
+#    Copyright (C) 2014-2022  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -42,8 +42,9 @@ class SubCategorySubCell(CustomFormExtraSubCell):
         return field
 
     def post_clean_instance(self, *, instance, value, form):
-        instance.category = value.category
-        instance.sub_category = value
+        if value:
+            instance.category = value.category
+            instance.sub_category = value
 
 
 # class _BaseForm(CremeEntityForm):
