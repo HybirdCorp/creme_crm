6f3033f8d8d0b16a8c84bf9446f178c6c58210e2
Guillaume Englert <genglert@hybird.org>
2022-11-02 15:31:15 +0100

    Fix the count of managers/employees in hat card for Organisations. It was wrong when greater than 16.

--- creme/persons/bricks.py
+++ creme/persons/bricks.py
@@ -257,6 +257,8 @@ class OrganisationCardHatBrick(Brick):
     ]
     template_name = 'persons/bricks/organisation-hat-card.html'
 
+    max_related_contacts = 15
+
     def detailview_display(self, context):
         organisation = context['object']
         user = context['user']
@@ -264,6 +266,20 @@ class OrganisationCardHatBrick(Brick):
 
         get_fconfigs = context['fields_configs'].get_for_model
         is_hidden = get_fconfigs(Organisation).is_fieldname_hidden
+
+        max_contacts = self.max_related_contacts
+
+        def retrieve_contacts_n_count(qs):
+            qs = EntityCredentials.filter(user, qs)
+            contacts = qs[:max_contacts]
+            count = len(contacts)
+            if count == max_contacts:
+                count = qs.count()
+
+            return contacts, count
+
+        managers, managers_count = retrieve_contacts_n_count(organisation.get_managers())
+        employees, employees_count = retrieve_contacts_n_count(organisation.get_employees())
 
         return self._render(self.get_template_context(
             context,
@@ -283,8 +299,13 @@ class OrganisationCardHatBrick(Brick):
                 relations__object_entity=organisation.id,
             ).exists(),
 
-            managers=EntityCredentials.filter(user, organisation.get_managers())[:16],
-            employees=EntityCredentials.filter(user, organisation.get_employees())[:16],
+            # managers=EntityCredentials.filter(user, organisation.get_managers())[:16],
+            # employees=EntityCredentials.filter(user, organisation.get_employees())[:16],
+            max_contacts=max_contacts,
+            managers=managers,
+            managers_count=managers_count,
+            employees=employees,
+            employees_count=employees_count,
 
             activities=Activities4Card.get(context, organisation),
             opportunities=Opportunities4Card.get(context, organisation),
