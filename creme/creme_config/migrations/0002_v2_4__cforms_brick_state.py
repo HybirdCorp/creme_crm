a2894987376ba837878881400f9b662728373050
Guillaume Englert <genglert@hybird.org>
2022-11-23 17:55:32 +0100

    Rework a migration.

--- creme/creme_config/migrations/0002_v2_4__cforms_brick_state.py
+++ creme/creme_config/migrations/0002_v2_4__cforms_brick_state.py
@@ -1,5 +1,3 @@
-import json
-
 from django.db import migrations
 
 BRICK_STATE_SHOW_CFORMS_DETAILS  = 'creme_config-show_customforms_details'
@@ -9,18 +7,15 @@ def convert_brick_state(apps, schema_editor):
     for state in apps.get_model('creme_core', 'BrickState').objects.filter(
         brick_id='block_creme_config-custom_forms',
     ):
-        # ct_id = state.json_extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS]
-        extra_data = json.loads(state.json_extra_data)
-        ct_id = extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS]
+        ct_id = state.json_extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS]
 
-        # state.json_extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS] = {'ctype': ct_id}
-        extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS] = {'ctype': ct_id}
-        state.json_extra_data = json.dumps(extra_data)
+        state.json_extra_data[BRICK_STATE_SHOW_CFORMS_DETAILS] = {'ctype': ct_id}
         state.save()
 
 
 class Migration(migrations.Migration):
     dependencies = [
+        ('creme_core', '0108_v2_4__bricks_jsonfields'),  # NB: to get a JSONField
         ('creme_config', '0001_initial'),
     ]
 
