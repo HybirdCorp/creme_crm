cf8e3956f35bc39e2a8d17bd5dafd6b8c289ca2b
Fabre Florian <ffabre@hybird.org>
2023-01-10 16:17:59 +0100

    Upgrade jQuery 3.6.3

--- creme/settings.py
+++ creme/settings.py
@@ -809,8 +809,8 @@ CREME_LIB_JS = [
 
     'creme_core/js/media.js',
     'creme_core/js/lib/underscore/underscore-1.13.2.js',
-    'creme_core/js/jquery/3.x/jquery-3.6.0.js',
-    'creme_core/js/jquery/3.x/jquery-migrate-3.3.2.js',
+    'creme_core/js/jquery/3.x/jquery-3.6.3.js',
+    'creme_core/js/jquery/3.x/jquery-migrate-3.4.0.js',
     'creme_core/js/jquery/ui/jquery-ui-1.13.1.js',
     'creme_core/js/jquery/ui/jquery-ui-locale.js',
     # 'creme_core/js/jquery/extensions/jqplot-1.0.8/excanvas.js',
