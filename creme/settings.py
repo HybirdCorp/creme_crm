ced572ac58c7e042a15755e717ffb1764ede6187
Guillaume Englert <genglert@hybird.org>
2023-06-29 14:40:13 +0200

    Upgrade the version of Closure.

--- creme/settings.py
+++ creme/settings.py
@@ -1154,7 +1154,7 @@ YUICOMPRESSOR_PATH = join(
     dirname(__file__), 'static', 'utils', 'yui', 'yuicompressor-2.4.2.jar',
 )
 CLOSURE_COMPILER_PATH = join(
-    dirname(__file__), 'static', 'utils', 'closure', 'closure-compiler-v20220905.jar',
+    dirname(__file__), 'static', 'utils', 'closure', 'closure-compiler-v20230502.jar',
 )
 
 COPY_MEDIA_FILETYPES = {
