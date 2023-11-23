9caa18be777a15530deaf730c7b682d3c5812aed
Fabre Florian <ffabre@hybird.org>
2023-11-23 15:17:31 +0100

    Fixup : remove filedrop.

--- creme/creme_core/utils/html.py
+++ creme/creme_core/utils/html.py
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# Copyright (c) 2015-2024 Hybird
+# Copyright (C) 2015-2024  Hybird
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
