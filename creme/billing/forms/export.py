1308d6f5d036f45e9667e3d73f75935752c6d470
Guillaume Englert <genglert@hybird.org>
2024-01-23 13:50:40 +0100

    Fix an issue in billing export when the configured engine has been uninstalled.

--- creme/billing/forms/export.py
+++ creme/billing/forms/export.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2020-2023  Hybird
+#    Copyright (C) 2020-2024  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -383,12 +383,15 @@ class ExporterThemeStep(CremeModelForm):
 
         instance = self.instance
         if instance.engine_id:
-            # TODO: only if the engine is the same ?
-            exporter_f.initial = engine_manager.exporter(
+            exporter = engine_manager.exporter(
                 engine_id=instance.engine_id,
                 flavour_id=instance.flavour_id,
                 model=model,
-            ).id
+            )
+
+            # TODO: only if the engine is the same ?
+            if exporter is not None:
+                exporter_f.initial = exporter.id
 
     def save(self, *args, **kwargs):
         instance = self.instance
