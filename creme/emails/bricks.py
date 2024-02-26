47f2fb415a08f975e5346266c487889d03bcb877
Guillaume Englert <genglert@hybird.org>
2024-02-26 11:20:52 +0100

    Avoid to display 2 bricks in "my configuration" when the users cannot access to the related apps.  - activities.bricks.UserCalendarsBrick  - emails.bricks.MySignaturesBrick

--- creme/emails/bricks.py
+++ creme/emails/bricks.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2023  Hybird
+#    Copyright (C) 2009-2024  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -354,6 +354,10 @@ class MySignaturesBrick(QuerysetBrick):
 
     def detailview_display(self, context):
         user = context['user']
+
+        # TODO: better permissions management in "my configuration" (done in Creme 2.6)
+        if not user.has_perm_to_access('emails'):
+            return ''
 
         return self._render(self.get_template_context(
             context,
