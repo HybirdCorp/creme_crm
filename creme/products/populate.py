b2f5187176be39d573e9913c44856922cfec815f
Guillaume Englert <genglert@hybird.org>
2024-03-11 21:39:07 +0100

    Fix UUIDs for populated instances of 'products.Category' & 'products.SubCategory'.

--- creme/products/populate.py
+++ creme/products/populate.py
@@ -64,6 +64,65 @@ class Populator(BasePopulator):
             'name', 'description', 'category__name', 'sub_category__name',
         ],
     }
+    CATEGORIES = [
+        [
+            Category(uuid='3fb0ef3c-45d0-40bd-8e71-b1ab49fca8d3', name=_('Jewelry')),
+            [
+                SubCategory(uuid='2d6555fe-4c25-4098-8128-25395cf2c10b', name=_('Ring')),
+                SubCategory(uuid='c160d58f-6878-4dd6-87d6-ee05db310f3a', name=_('Bracelet')),
+                SubCategory(uuid='83b79894-122e-4212-bf18-929573f57c74', name=_('Necklace')),
+                SubCategory(uuid='6514cb2a-ee59-4abf-bdef-92488fac3a42', name=_('Earrings')),
+            ]
+        ], [
+            Category(uuid='74c91fab-d671-4054-984f-ba395d7dffcb', name=_('Mobile')),
+            [
+                SubCategory(uuid='2abfa34f-d30f-4629-9a45-8cd63ce0a362', name=_('Iphone')),
+                SubCategory(uuid='4c55bae2-44d3-44ca-aade-e8433339f2aa', name=_('Blackberry')),
+                SubCategory(uuid='5fc12768-bc62-4412-a9d4-91aa9967bfac', name=_('Samsung')),
+                SubCategory(uuid='62e549a1-f132-4c82-a836-e8d94ee8b29b', name=_('Android')),
+            ]
+        ], [
+            Category(uuid='69084c8f-068c-41e3-80e3-42ed312e9815', name=_('Electronics')),
+            [
+                SubCategory(uuid='25f7c8db-a0d1-42af-a342-97727a2229fd', name=_('Laptops')),
+                SubCategory(uuid='6e3c21d1-f81e-492c-b2ce-da1fbc46727f', name=_('Desktops')),
+                SubCategory(uuid='13463841-90df-4036-b830-62ad75668213', name=_('Tablet')),
+                SubCategory(uuid='3a14d682-77b5-43b7-9b9c-8db9bcd44b0d', name=_('Notebook')),
+            ]
+        ], [
+            Category(uuid='213e14fa-bda1-4850-a22d-d3e6bb832a98', name=_('Travels')),
+            [
+                SubCategory(uuid='79c7d28f-eed2-4565-9815-1d1887f3ccaf', name=_('Fly')),
+                SubCategory(uuid='9812ff36-b3f3-4670-8d4a-b8e9c916bf0d', name=_('Hotel')),
+                SubCategory(uuid='15e6f072-bd3e-4591-9963-be2af1888520', name=_('Week-end')),
+                SubCategory(uuid='abaca40b-51c9-49c1-95c4-ba93e51bdc40', name=_('Rent')),
+            ]
+        ], [
+            Category(uuid='684f4e8a-aad8-4eb5-980c-fbb8fc28776e', name=_('Vehicle')),
+            [
+                SubCategory(uuid='7c3492c1-7621-456f-a849-1fc6af829435', name=_('Car')),
+                SubCategory(uuid='1f5664c6-2a88-42d0-a554-e271dbe7fd84', name=_('Bike')),
+                SubCategory(uuid='669d7604-0d71-48b1-aa4e-6ca52f8923f8', name=_('Boat')),
+                SubCategory(uuid='886757aa-e019-4683-b83b-942ca9798e0b', name=_('Plane')),
+            ]
+        ], [
+            Category(uuid='8ef405ac-9109-4ea6-94eb-f068b33c617d', name=_('Games & Toys')),
+            [
+                SubCategory(uuid='f5f5efc4-b03e-47b9-b485-f85dfb1a2630', name=_('Boys')),
+                SubCategory(uuid='39d811f7-707b-4419-8750-b15179d5b3eb', name=_('Girls')),
+                SubCategory(uuid='d92fd9d0-a8cd-4d6e-9d8c-24037a2121fe', name=_('Teens')),
+                SubCategory(uuid='dc8d1368-55d7-4abc-80c2-28ba1d3d1d3b', name=_('Babies')),
+            ]
+        ], [
+            Category(uuid='14474aee-9ba7-4a0e-816e-e19bab639af9', name=_('Clothes')),
+            [
+                SubCategory(uuid='01109ac2-e539-45fd-a2a8-b3fff7992933', name=_('Men')),
+                SubCategory(uuid='eaf8bc61-e1a1-4181-a679-84c606037922', name=_('Women')),
+                SubCategory(uuid='ecf095f8-f3a7-4271-921c-81b1dff714a6', name=_('Kids')),
+                SubCategory(uuid='ba170185-892e-400a-b212-1810fc86f204', name=_('Babies')),
+            ]
+        ],
+    ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
