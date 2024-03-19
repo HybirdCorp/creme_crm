7da1dea28ccc65a7d8a28d2c02b39cc8384e5f43
Guillaume Englert <genglert@hybird.org>
2024-03-19 12:38:19 +0100

    Better fix for bulk-edition.

--- creme/documents/tests/test_folder.py
+++ creme/documents/tests/test_folder.py
@@ -536,7 +536,7 @@ class FolderTestCase(BrickTestCaseMixin, _DocumentsTestCase):
         folder1 = create_folder(title='Test folder#1')
         folder2 = create_folder(title='Test folder#2', parent_folder=folder1)
         folder3 = create_folder(title='Test folder#3', parent_folder=folder2)
-        folder4 = create_folder(title='Test folder#4')
+        folder4 = create_folder(title='Test folder#0')  # First by title
 
         folders = [folder1, folder3, folder4]
         field_name = 'parent_folder'
@@ -552,6 +552,7 @@ class FolderTestCase(BrickTestCaseMixin, _DocumentsTestCase):
                 formfield_name: folder3.id,
             },
         )
+        self.assertNoFormError(response2)
         self.assertContains(
             response2,
             _('This folder is one of the child folders of «%(folder)s»') % {
