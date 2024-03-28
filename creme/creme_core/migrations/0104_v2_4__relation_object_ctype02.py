719ad45f16f65e0da69bc4089a171705b0ebfd27
Hugo Smett <hsmett@hybird.org>
2024-03-28 07:32:55 +0100

    Optimize migration creme_core 0104

--- creme/creme_core/migrations/0104_v2_4__relation_object_ctype02.py
+++ creme/creme_core/migrations/0104_v2_4__relation_object_ctype02.py
@@ -2,9 +2,11 @@ from django.db import migrations
 
 
 def fill_object_ctype(apps, schema_editor):
-    for relation in apps.get_model('creme_core', 'Relation').objects.select_related('object_entity'):
-        relation.object_ctype_id = relation.object_entity.entity_type_id
-        relation.save()
+    ContentType = apps.get_model("contenttypes", "ContentType")
+    Relation = apps.get_model("creme_core", "Relation")
+
+    for ctid in ContentType.objects.values_list("id", flat=True):
+        Relation.objects.filter(object_entity__entity_type_id=ctid).update(object_ctype_id=ctid)
 
 
 class Migration(migrations.Migration):
