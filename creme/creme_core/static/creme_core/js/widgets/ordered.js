6b01dcbe3dc371e5c2f482a2af2523ced22c3103
Fabre Florian <ffabre@hybird.org>
2024-04-15 18:43:17 +0200

    Fix ui-creme-ordered declaration as creme.widget.OrderedListSelect. Add tests and do some refactor.

--- creme/creme_core/static/creme_core/js/widgets/ordered.js
+++ creme/creme_core/static/creme_core/js/widgets/ordered.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
     Creme is a free/open-source Customer Relationship Management software
-    Copyright (C) 2022  Hybird
+    Copyright (C) 2022-2024  Hybird
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -19,49 +19,66 @@
 (function($) {
 "use strict";
 
-/* TODO: unit test */
 /* TODO: button to deselect all? */
 /* TODO: input to search/filter in choices? */
-creme.widget.SelectOrInputWidget = creme.widget.declare('ui-creme-ordered', {
+/* TODO: use <select> instead ? */
+creme.widget.OrderedListSelect = creme.widget.declare('ui-creme-ordered', {
     _create: function(element, options) {
-        this._dataIdAttr = options['dataIdAttr'] || 'data-choice-id';
-        this._notDraggableClass = options['notDraggableClass'] || 'not-draggable';
+        options = options || {};
 
-        this._targetInput = element.find('.ordered-widget-value');
-        this._availableContainer = element.find('.ordered-widget-available-choices');
-        this._enabledContainer = element.find('.ordered-widget-enabled-choices');
+        this._dataIdAttr = options.dataIdAttr || 'data-choice-id';
+        this._notDraggableClass = options.notDraggableClass || 'not-draggable';
 
-        var choices = JSON.parse(creme.utils.JSON.readScriptText(element.find('.ordered-widget-choices')));
+        this._targetInput = element.find('input.ordered-widget-value');
+        this._choicesContainer = element.find('.ordered-widget-available-choices');
+        this._selectionContainer = element.find('.ordered-widget-enabled-choices');
 
-        // We compute the order to use when a chosen element is deselected, to re-insert it among available elements.
-        choices.forEach(function(choice, index) {
-            choice.originalOrder = index;
-        });
+        this._setupItems(element);
+        this._setupDragNDrop(element);
 
-        var selected = JSON.parse(this._targetInput.val());
-        if (!Array.isArray(selected)) {
-            throw new Error('SelectOrInputWidget: invalid selected values', selected);
-        }
+        element.on('dblclick', '.ordered-widget-choice', function(e) {
+            e.preventDefault();
+            this._moveItem($(e.target).closest('.ordered-widget-choice'));
+        }.bind(this));
 
-        selected.forEach(
-            function(selected_id) {
-                var index = choices.findIndex(function(choice) {
-                    return (choice.value === selected_id);
-                });
+        element.on('click', '.ordered-widget-enabled-choices .deselect-choice', function(e) {
+            e.preventDefault();
+            this._moveItem($(e.target).closest('.ordered-widget-choice'));
+        }.bind(this));
 
-                this._enabledContainer.append(this._asEnabledEntry(this._buildEntry(choices[index])));
-                choices.splice(index, 1);  // We remove the choice from the available ones.
-            }.bind(this)
-        );
-        choices.forEach(
-            function(choice) {
-                this._availableContainer.append(this._buildEntry(choice));
-            }.bind(this)
-        );
+        element.addClass('widget-ready');
+    },
 
+    _setupItems: function(element) {
+        var choices = this.choices(element).slice();
+        var selected = this.selected(element);
+
+        var availableItems = [];
+        var selectedItems = [];
+
+        selected.forEach(function(value) {
+            var index = choices.findIndex(function(choice) {
+                return choice.value === value;
+            });
+
+            if (index !== -1) {
+                selectedItems.push(this._itemHtml(choices[index]));
+                choices.splice(index, 1);
+            }
+        }.bind(this));
+
+        choices.forEach(function(choice) {
+            availableItems.push(this._itemHtml(choice));
+        }.bind(this));
+
+        this._choicesContainer.html(availableItems.join(''));
+        this._selectionContainer.html(selectedItems.join(''));
+    },
+
+    _setupDragNDrop: function(element) {
         var groupName = element.attr('id');
 
-        this._availableChoices = new Sortable(this._availableContainer.get(0), {
+        this._choicesSortable = new Sortable(this._choicesContainer.get(0), {
             group: {
                 name: groupName,
                 put: false
@@ -70,98 +87,87 @@ creme.widget.SelectOrInputWidget = creme.widget.declare('ui-creme-ordered', {
             filter: '.' + this._notDraggableClass,
             sort: false
         });
-        this._enabledChoices = new Sortable(this._enabledContainer.get(0), {
+
+        this._selectionSortable = new Sortable(this._selectionContainer.get(0), {
             group: groupName,
             dataIdAttr: this._dataIdAttr,
             filter: '.' + this._notDraggableClass,
-            onSort: this._updateValue.bind(this),
-            onAdd: function(event) { this._asEnabledEntry($(event.item)); }.bind(this)
+            onSort: this._updateInput.bind(this)
         });
-
-        element.addClass('widget-ready');
     },
 
-    _buildEntry: function(choice) {
-        var choiceClass = 'ordered-widget-choice';
-
-        var entry = $('<div>').attr('class', choiceClass)
-                              .attr(this._dataIdAttr, choice.value)
-                              .attr('data-order', choice.originalOrder)
-                              .attr('title', choice.help)
-                              .append('<span>' + choice.label + '</span>');
-
-        if (choice.disabled) {
-            entry.addClass(this._notDraggableClass);
-        } else {
-            entry.dblclick(function(e) {
-                this._transferChosen($(e.target).closest('.' + choiceClass));
-            }.bind(this));
-        }
-
-        return entry;
+    _updateInput: function(e) {
+        this._targetInput.val(JSON.stringify(this._selectionSortable.toArray()));
     },
 
-    _updateValue: function() {
-        this._targetInput.val(JSON.stringify(this._enabledChoices.toArray()));
+    selected: function(element) {
+        var data = JSON.parse(this._targetInput.val());
+        return Array.isArray(data) ? data : [];
     },
 
-    _asAvailableEntry: function(entryDiv) {
-        entryDiv.find('button.deselect-choice').remove();
+    choices: function(element) {
+        if (Object.isNone(this._choices)) {
+            var script = element.find('script[type$="/json"]');
+            var choices = [];
 
-        return entryDiv;
-    },
-
-    _asEnabledEntry: function(entryDiv) {
-        var deselectButton = $(
-            '<button type="button" class="deselect-choice">${label}</button>'.template({
-                label: gettext('Deselect')
-            })
-        );
-
-        if (entryDiv.hasClass(this._notDraggableClass)) {
-            // NB: we add the button to facilitate the layout, but we disable it (the CSS will hide it wink wink).
-            deselectButton.prop('disabled', true);
-        } else {
-            deselectButton.on('click', function(e) {
-                e.preventDefault();
-                this._transferChosen(entryDiv);
-            }.bind(this));
-        }
-
-        entryDiv.append(deselectButton);
-
-        return entryDiv;
-    },
-
-    _transferChosen: function(entryDiv) {
-        var parent = entryDiv.parent('.ordered-widget-choices');
-
-        // Detach from its parent but keep the event handlers (double-click...)
-        entryDiv.detach();
-
-        if (parent.hasClass('ordered-widget-enabled-choices')) {
-            var order = parseInt(entryDiv.attr('data-order'));
-            var elementAfter = null;
-
-            entryDiv = this._asAvailableEntry(entryDiv);
-
-            this._availableContainer.children().each(function(index, element) {
-                if (order < parseInt($(element).attr('data-order'))) {
-                    elementAfter = element;
-                    return false;
+            try {
+                if (!Object.isEmpty(script)) {
+                    var data = creme.utils.JSON.readScriptText(script);
+                    choices = Object.isEmpty(data) ? [] : JSON.parse(data);
                 }
-            });
-
-            if (elementAfter === null) {
-                this._availableContainer.append(entryDiv);
-            } else {
-                $(elementAfter).before(entryDiv);
+            } catch (e) {
+                console.warn(e);
             }
-        } else {
-            this._enabledContainer.append(this._asEnabledEntry(entryDiv));
+
+            // We compute the order to use when a chosen element is deselected, to re-insert it among available elements.
+            this._choices = choices.map(function(choice, index) {
+                choice.initialOrder = index;
+                return choice;
+            });
         }
 
-        this._updateValue();
+        return this._choices;
+    },
+
+    _itemHtml: function(choice) {
+        return (
+            '<div class="ordered-widget-choice ${notDraggable}" ${idAttr}="${value}" data-order="${order}" ${help}>' +
+                '<span>${label}</span>' +
+                '<button type="button" class="deselect-choice" ${buttonDisabled}>${buttonLabel}</button>' +
+            '</div>'
+        ).template({
+            idAttr: this._dataIdAttr,
+            value: choice.value,
+            order: choice.initialOrder,
+            help: choice.help ? 'title="${help}"'.template(choice) : '',
+            label: choice.label,
+            notDraggable: choice.disabled ? this._notDraggableClass : '',
+            buttonDisabled: choice.disabled ? 'disabled' : '',
+            buttonLabel: gettext('Deselect')
+        });
+    },
+
+    _moveItem: function(item) {
+        var container = item.parent('.ordered-widget-choices');
+        var selected = container.hasClass('ordered-widget-enabled-choices');
+
+        // Move to available choices
+        if (selected) {
+            var order = parseInt(item.data('order'));
+            var target = this._choicesContainer.find('.ordered-widget-choice').filter(function() {
+                return (order < parseInt($(this).data('order')));
+            }).first().get(0);
+
+            if (Object.isNone(target)) {
+                this._choicesContainer.append(item);
+            } else {
+                $(target).before(item);
+            }
+        } else {  // Move to selected choices
+            this._selectionContainer.append(item);
+        }
+
+        this._updateInput();
     }
 });
 
