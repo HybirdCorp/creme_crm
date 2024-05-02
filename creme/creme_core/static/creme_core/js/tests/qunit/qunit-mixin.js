ff49c377642c32d4bb3a079be26f52e246c0398a
joehybird <ffabre@hybird.org>
2024-05-02 06:57:58 +0200

    Fix deprecations of mouseleave(), mouseenter(), mouseover() & focusout()

--- creme/creme_core/static/creme_core/js/tests/qunit/qunit-mixin.js
+++ creme/creme_core/static/creme_core/js/tests/qunit/qunit-mixin.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
     Creme is a free/open-source Customer Relationship Management software
-    Copyright (C) 2017-2022  Hybird
+    Copyright (C) 2017-2024  Hybird
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -205,7 +205,7 @@
                 }
 
                 // Trigger events for some XSS issues
-                $('[data-test="qunitXSS"]').mouseover().trigger('click');
+                $('[data-test="qunitXSS"]').trigger('mouseover').trigger('click');
             }.bind(this));
         },
 
