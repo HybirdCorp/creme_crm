becd462c6f92ee99238dcd961ab122f4613275eb
Guillaume Englert <genglert@hybird.org>
2024-05-03 15:04:18 +0200

    Fix a migration order issue in 'commercial'.

--- creme/commercial/migrations/0020_v2_6__ptype_uuid05.py
+++ creme/commercial/migrations/0020_v2_6__ptype_uuid05.py
@@ -2,6 +2,10 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
+    run_before = [
+        ('creme_core', '0134_v2_6__propertytype_uuid05')
+    ]
+
     dependencies = [
         ('commercial', '0019_v2_6__ptype_uuid04'),
     ]
