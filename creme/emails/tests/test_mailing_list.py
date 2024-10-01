acb3d3308ed5593d7b2f81943bff42dab5df5559
Guillaume Englert <genglert@hybird.org>
2024-10-01 16:02:24 +0200

    Improve the cloning of mailing lists. Add unit tests for cloning in 'emails'.

--- creme/emails/tests/test_mailing_list.py
+++ creme/emails/tests/test_mailing_list.py
@@ -749,3 +749,33 @@ class MailingListsTestCase(BrickTestCaseMixin, _EmailsTestCase):
         user = self.login_as_root_and_get()
         orga = FakeOrganisation.objects.create(user=user, name='Dojo')
         self.assertGET404(reverse('emails__add_child_mlists', args=(orga.id,)))
+
+    def test_clone(self):
+        user = self.get_root_user()
+        create_ml = partial(MailingList.objects.create, user=user)
+        mlist = create_ml(name='ml01')
+        child = create_ml(name='ml02')
+
+        mlist.children.add(child)
+
+        contact = Contact.objects.create(
+            user=user, first_name='Spike', last_name='Spiegel',
+        )
+        mlist.contacts.add(contact)
+
+        orga = Organisation.objects.create(user=user, name='Bepop')
+        mlist.organisations.add(orga)
+
+        email = 'faye@bebop.mrs'
+        EmailRecipient.objects.create(ml=mlist, address=email)
+
+        cloned_mlist = mlist.clone()
+        self.assertIsInstance(cloned_mlist, MailingList)
+        self.assertNotEqual(mlist.pk, cloned_mlist.pk)
+        self.assertEqual(mlist.name, cloned_mlist.name)
+        self.assertCountEqual([child],   cloned_mlist.children.all())
+        self.assertCountEqual([contact], cloned_mlist.contacts.all())
+        self.assertCountEqual([orga],    cloned_mlist.organisations.all())
+        self.assertCountEqual(
+            [email], cloned_mlist.emailrecipient_set.values_list('address', flat=True),
+        )
