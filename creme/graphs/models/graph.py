0c88df131410088748775b512d6b61a2f7dcc482
Guillaume Englert <genglert@hybird.org>
2024-10-01 14:06:52 +0200

    Fix the cloning of Graph.

--- creme/graphs/models/graph.py
+++ creme/graphs/models/graph.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2023  Hybird
+#    Copyright (C) 2009-2024  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -232,8 +232,10 @@ class AbstractGraph(CremeEntity):
 
     def _post_save_clone(self, source):
         for node in RootNode.objects.filter(graph=source):
-            rn = RootNode.objects.create(graph=self, entity=node.entity)
-            rn.relation_types = node.relation_types.all()
+            # rn = RootNode.objects.create(graph=self, entity=node.entity)
+            # rn.relation_types = node.relation_types.all()
+            rn = RootNode.objects.create(graph=self, real_entity=node.entity)
+            rn.relation_types.set(node.relation_types.all())
 
 
 class Graph(AbstractGraph):
