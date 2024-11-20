8e1414e033aed871465f0ce5802a74e6da888bb1
Guillaume Englert <genglert@hybird.org>
2024-11-20 17:01:46 +0100

    In 'creme_core.auth.backend', the constants '_ADD_PREFIX', '_LINK_PREFIX' & '_EXPORT_PREFIX' have been removed.

--- creme/creme_core/auth/__init__.py
+++ creme/creme_core/auth/__init__.py
@@ -4,10 +4,16 @@ from .entity_credentials import EntityCredentials  # NOQA
 SUPERUSER_PERM = '*superuser*'
 STAFF_PERM     = '*staff*'
 
+_CREATION_PREFIX = 'add_'
+_LINK_PREFIX = 'link_'
+_EXPORT_PREFIX = 'export_'
+
 
 def build_creation_perm(model):
-    return f'{model._meta.app_label}.add_{model.__name__.lower()}'
+    # return f'{model._meta.app_label}.add_{model.__name__.lower()}'
+    return f'{model._meta.app_label}.{_CREATION_PREFIX}{model.__name__.lower()}'
 
 
 def build_link_perm(model):
-    return f'{model._meta.app_label}.link_{model.__name__.lower()}'
+    # return f'{model._meta.app_label}.link_{model.__name__.lower()}'
+    return f'{model._meta.app_label}.{_LINK_PREFIX}{model.__name__.lower()}'
