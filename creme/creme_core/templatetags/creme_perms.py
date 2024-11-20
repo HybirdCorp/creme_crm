2be5df184cbbe95f1782eaa983b92b8531b78714
Guillaume Englert <genglert@hybird.org>
2024-11-20 21:05:48 +0100

    Improve the code of 2 templatetags.

--- creme/creme_core/templatetags/creme_perms.py
+++ creme/creme_core/templatetags/creme_perms.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2023  Hybird
+#    Copyright (C) 2023-2024  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -24,14 +24,12 @@ register = template.Library()
 
 @register.filter
 def has_perm_to_create(user, ctype):
-    # TODO: <return user.has_perm_to_create(ctype)>?
-    return user.has_perm(f'{ctype.app_label}.add_{ctype.model}')
+    return user.has_perm_to_create(ctype)
 
 
 @register.filter
 def has_perm_to_export(user, ctype):
-    # TODO: <return user.has_perm_to_export(ctype)>?
-    return user.has_perm(f'{ctype.app_label}.export_{ctype.model}')
+    return user.has_perm_to_export(ctype)
 
 
 @register.filter
@@ -49,9 +47,9 @@ def has_perm_to_delete(user, entity):
     return user.has_perm_to_delete(entity)
 
 
-# TODO: entity_or_ctype?
 @register.filter
 def has_perm_to_link(user, entity_or_ctype):
+    # TODO: rework has_perm_to_link to accept ContentType?
     return user.has_perm_to_link(
         entity_or_ctype.model_class()
         if isinstance(entity_or_ctype, ContentType) else
