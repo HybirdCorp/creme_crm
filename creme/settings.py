73689f2a78f38e3cc11a0a6727c8206a50d88d12
Fabre Florian <ffabre@hybird.org>
2025-01-02 17:16:43 +0100

    Refactor creme.ActivityCalendar  - Split the "calendar user page" features to creme.ActivityCalendarController : floating events, sidebar selection, state within url hash  - creme.ActivityCalendar can be now used as independent component.  - Add headlessMode flag  - Add allowEventOverlaps property (boolean or method).  - Add allowEventCreate property (boolean)  - Move calendar selection state queries to ActivityCalendarController. Improve performance.

--- creme/settings.py
+++ creme/settings.py
@@ -1000,6 +1000,7 @@ CREME_OPT_JS = [  # OPTIONAL APPS
     ('creme.persons',       'persons/js/persons.js'),
 
     ('creme.activities',    'activities/js/activities.js'),
+    ('creme.activities',    'activities/js/calendar.js'),
     ('creme.activities',    'activities/js/activities-calendar.js'),
 
     ('creme.billing',       'billing/js/billing.js'),
@@ -1143,7 +1144,9 @@ TEST_CREME_CORE_JS = [
 
 TEST_CREME_OPT_JS = [
     # ('creme.my_app',       'my_app/js/tests/my_app.js'),
+    ('creme.activities',    'activities/js/tests/qunit-calendar-mixin.js'),
     ('creme.activities',    'activities/js/tests/activities-listview.js'),
+    ('creme.activities',    'activities/js/tests/calendar.js'),
     ('creme.activities',    'activities/js/tests/activities-calendar.js'),
     ('creme.billing',       'billing/js/tests/billing.js'),
     ('creme.billing',       'billing/js/tests/billing-actions.js'),
