22625f220543466299d1b9d9369b2afef8f37aa5
Fabre Florian <ffabre@hybird.org>
2025-01-15 11:51:35 +0100

    Use native Object.assign instead of $.extend for libs.

--- creme/creme_core/static/creme_core/js/lib/browser.js
+++ creme/creme_core/static/creme_core/js/lib/browser.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
     Creme is a free/open-source Customer Relationship Management software
-    Copyright (C) 2020-2021  Hybird
+    Copyright (C) 2020-2025  Hybird
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
 
-(function($) {
+(function() {
 "use strict";
 
 /* globals BrowserVersion */
@@ -92,4 +92,4 @@ window.BrowserVersion = {
     }
 };
 
-}(jQuery));
+}());
