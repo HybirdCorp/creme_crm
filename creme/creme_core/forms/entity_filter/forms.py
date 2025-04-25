c491dfb4bc034df58c418f732be9f2113b0e74e8
Guillaume Englert <genglert@hybird.org>
2025-04-25 10:50:49 +0200

    Fix the creation view for EntityFilter to accept models with digits at the end of their name.

--- creme/creme_core/forms/entity_filter/forms.py
+++ creme/creme_core/forms/entity_filter/forms.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2024  Hybird
+#    Copyright (C) 2009-2025  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -131,7 +131,7 @@ class EntityFilterCreationForm(_EntityFilterForm):
         generate_string_id_and_save(
             EntityFilter, [instance],
             # f'creme_core-userfilter_{ct.app_label}-{ct.model}',
-            f'{self.pk_prefix}{ct.app_label}-{ct.model}',
+            f'{self.pk_prefix}{ct.app_label}-{ct.model}-',
         )
 
         instance.set_conditions(
