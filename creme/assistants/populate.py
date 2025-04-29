3d1c407a460025dd78d759e68f81d8b3072f9c76
Guillaume Englert <genglert@hybird.org>
2025-04-29 13:39:53 +0200

    Remove a useless type annotation.

--- creme/assistants/populate.py
+++ creme/assistants/populate.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2024  Hybird
+#    Copyright (C) 2009-2025  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -90,7 +90,7 @@ class Populator(BasePopulator):
     #         },
     #     )
 
-    def _populate_notification_channels(self) -> None:
+    def _populate_notification_channels(self):
         NotificationChannel.objects.get_or_create(
             uuid=constants.UUID_CHANNEL_USERMESSAGES,
             defaults={
