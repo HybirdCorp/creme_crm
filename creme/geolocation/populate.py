63a3d92e74d24514840b01dba1b927b6a92246f1
Guillaume Englert <genglert@hybird.org>
2025-05-07 18:47:38 +0200

    Split 'geolocation.populate'.

--- creme/geolocation/populate.py
+++ creme/geolocation/populate.py
@@ -37,60 +37,63 @@ logger = logging.getLogger(__name__)
 class Populator(BasePopulator):
     dependencies = ['creme_core', 'persons']
 
-    def populate(self):
-        already_populated = SettingValue.objects.exists_4_key(
+    SETTING_VALUES = [
+        SettingValue(key=setting_keys.use_entity_icon_key, value=False),
+        SettingValue(key=setting_keys.google_api_key, value=''),
+        SettingValue(
+            key=setting_keys.neighbourhood_distance_key,
+            value=constants.DEFAULT_SEPARATING_NEIGHBOURS,
+        ),
+    ]
+
+    def _already_populated(self):
+        return SettingValue.objects.exists_4_key(
             setting_keys.neighbourhood_distance_key
         )
 
-        SettingValue.objects.get_or_create(
-            key_id=setting_keys.use_entity_icon_key.id,
-            defaults={'value': False}
-        )
+    def _first_populate(self):
+        super()._first_populate()
+        self._populate_towns()
 
-        if already_populated:
-            return
-
-        SettingValue.objects.set_4_key(
-            setting_keys.neighbourhood_distance_key,
-            constants.DEFAULT_SEPARATING_NEIGHBOURS
-        )
-
-        SettingValue.objects.set_4_key(setting_keys.google_api_key, '')
-
+    def _populate_towns(self):
         if self.verbosity:
             self.stdout.write('\n ', ending='')
             self.stdout.flush()
 
         GeolocationCommand().import_town_all(verbosity=self.verbosity)
 
+    def _populate_bricks_config_for_persons(self):
         for model in (persons.get_organisation_model(), persons.get_contact_model()):
             BrickDetailviewLocation.objects.multi_create(
                 defaults={'model': model},
                 data=[
                     {
-                        # 'brick': bricks.GoogleDetailMapBrick, 'order': 70,
                         'brick': bricks.OpenStreetMapDetailMapBrick, 'order': 70,
                         'zone': BrickDetailviewLocation.LEFT,
-                    },
-                    {
-                        # 'brick': bricks.GoogleNeighboursMapBrick, 'order': 600,
+                    }, {
                         'brick': bricks.OpenStreetMapNeighboursMapBrick, 'order': 600,
                         'zone': BrickDetailviewLocation.BOTTOM,
                     },
                 ],
             )
 
+    def _populate_bricks_config_for_mypage(self):
         BrickMypageLocation.objects.create(
             brick_id=bricks.OpenStreetMapFilteredMapBrick.id, order=20, user=None,
         )
 
+    def _populate_bricks_config_for_root(self):
         # Add this block only if the root user exists (creme_core populated)
         root = get_user_model().objects.filter(pk=1).first()
         if root:
             logger.info(
-                # 'Creme core is installed => the block GoogleFilteredMapBrick can be activated'
                 'The block OpenStreetMapFilteredMapBrick is set on the page of "root" user.'
             )
             BrickMypageLocation.objects.create(
                 brick_id=bricks.OpenStreetMapFilteredMapBrick.id, order=20, user=root,
             )
+
+    def _populate_bricks_config(self):
+        self._populate_bricks_config_for_persons()
+        self._populate_bricks_config_for_mypage()
+        self._populate_bricks_config_for_root()
