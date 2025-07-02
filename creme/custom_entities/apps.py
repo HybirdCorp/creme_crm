2b15883be716c86e9a6dfa21aa40311ad0b0849c
Guillaume Englert <genglert@hybird.org>
2025-07-02 17:10:06 +0200

    Add specific icon for custom entity type.

--- creme/custom_entities/apps.py
+++ creme/custom_entities/apps.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2024  Hybird
+#    Copyright (C) 2024-2025  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -46,8 +46,7 @@ class CustomEntitiesConfig(CremeAppConfig):
 
         register = icon_registry.register
         for model in all_custom_models:
-            # TODO: specific icon
-            register(model, 'images/document_%(size)s.png')
+            register(model, 'images/wall_%(size)s.png')
 
     def register_menu_entries(self, menu_registry):
         from creme.creme_core.gui import menu
