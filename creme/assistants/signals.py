a6196b5528092edcc58cb38165f1f1f1f3c4b45f
Guillaume Englert <genglert@hybird.org>
2025-07-17 18:51:03 +0200

    Fix dynamic trigger dates in Alert with entities merging.

--- creme/assistants/signals.py
+++ creme/assistants/signals.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2015-2022  Hybird
+#    Copyright (C) 2015-2025  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -23,6 +23,7 @@ from django.dispatch.dispatcher import receiver
 
 from creme.creme_core.core.entity_cell import CELLS_MAP
 from creme.creme_core.models import CremeEntity
+from creme.creme_core.signals import pre_merge_related
 from creme.creme_core.utils import update_model_instance
 from creme.creme_core.utils.date_period import date_period_registry
 
@@ -40,7 +41,28 @@ def _refresh_alert_reminder_job(sender, instance, **kwargs):
         reminder_type.refresh_job()
 
 
-@receiver(post_save)
+# TODO: build a correct abstraction (DateOffset class?)
+def __get_date_offset_info(model, offset_dict):
+    cell = CELLS_MAP.build_cell_from_dict(
+        model=model, dict_cell=offset_dict['cell'],
+    )
+    if cell is None:
+        raise ValueError(f'offset cell <{offset_dict}> is invalid')
+
+    period = date_period_registry.deserialize(offset_dict['period'])
+    if period is None:
+        raise ValueError(f'offset period <{offset_dict}> is invalid')
+
+    sign = offset_dict['sign']
+    if sign not in (1, -1):
+        raise ValueError(f'offset sign <{offset_dict}> is invalid')
+
+    return cell, sign, period
+
+
+# NB: "@receiver(post_save, sender=CremeEntity)" does not work
+#     (the signal is sent for final class & strict class comparison is done)
+@receiver(post_save, dispatch_uid='assistants-update_alert_trigger')
 def _update_alert_trigger_date(sender, instance, created, **kwargs):
     # NB: "@receiver(post_save, sender=CremeEntity)" does not work
     #     (the signal is sent for final class & strict class comparison is done)
@@ -56,41 +78,51 @@ def _update_alert_trigger_date(sender, instance, created, **kwargs):
         trigger_offset__has_key='cell',
         is_validated=False,
     ):
-        offset_dict = alert.trigger_offset
-
-        cell = CELLS_MAP.build_cell_from_dict(
-            model=type(instance),
-            dict_cell=offset_dict['cell'],
-        )
-        if cell is None:
-            logger.critical(
-                'Signal handler _update_alert_trigger_date: offset cell is '
-                'invalid in the Alert id="%s" : %s',
-                alert.id, offset_dict,
+        try:
+            cell, sign, period = __get_date_offset_info(
+                model=type(instance), offset_dict=alert.trigger_offset,
             )
-            continue
-
-        period = date_period_registry.deserialize(offset_dict['period'])
-        if period is None:
+        except ValueError as e:
             logger.critical(
-                'Signal handler _update_alert_trigger_date: offset period is '
-                'invalid in the Alert id="%s" : %s',
-                alert.id, offset_dict,
+                'Error in signal handler _update_alert_trigger_date with the '
+                'Alert id="%s": %s', alert.id, e,
             )
-            continue
+        else:
+            update_model_instance(
+                alert,
+                trigger_date=alert.trigger_date_from_offset(
+                    entity=instance,
+                    cell=cell, sign=sign, period=period,
+                ),
+            )
 
-        sign = offset_dict['sign']
-        if sign not in (1, -1):
+
+# TODO: factorise better
+# This handler is called when "sender" (entity #1) is merged with "other_entity"
+# ('sender' is kept, 'other_entity' will be removed)
+#  - BEFORE the instances referencing 'other_entity' are modified to reference 'sender'.
+#  - AFTER 'sender' has been updated to store its final fields.
+@receiver(pre_merge_related, dispatch_uid='assistants-merge_alert_trigger')
+def _merge_alert_trigger_date(sender, other_entity, **kwargs):
+    for alert in Alert.objects.filter(
+        entity_id=other_entity.id,
+        trigger_offset__has_key='cell',
+        is_validated=False,
+    ):
+        try:
+            cell, sign, period = __get_date_offset_info(
+                model=type(sender), offset_dict=alert.trigger_offset,
+            )
+        except ValueError as e:
             logger.critical(
-                'Signal handler _update_alert_trigger_date: offset sign is '
-                'invalid in the Alert id="%s" : %s',
-                alert.id, offset_dict,
+                'Error in signal handler _merge_alert_trigger_date with the '
+                'Alert id="%s": %s', alert.id, e,
             )
-            continue
-
-        update_model_instance(
-            alert,
-            trigger_date=alert.trigger_date_from_offset(
-                cell=cell, sign=sign, period=period,
-            ),
-        )
+        else:
+            update_model_instance(
+                alert,
+                trigger_date=alert.trigger_date_from_offset(
+                    entity=sender,
+                    cell=cell, sign=sign, period=period,
+                ),
+            )
