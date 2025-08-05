481e7c35b20b570b4dfcc24a9c1858c286fd4ba1
Guillaume Englert <genglert@hybird.org>
2025-08-05 18:23:18 +0200

    Remove things deprecated in Creme 2.7 (crudity).

--- creme/crudity/constants.py
+++ creme/crudity/constants.py
@@ -1,19 +1,16 @@
-import warnings
+# import warnings
 
 # Used as a multiline delimiter. /!\ They have to have the same length
 LEFT_MULTILINE_SEP  = '[['
 RIGHT_MULTILINE_SEP = ']]'
 
-# SETTING_CRUDITY_SANDBOX_BY_USER = 'crudity-crudity_sandbox_by_user'
-
-
-def __getattr__(name):
-    if name == 'SETTING_CRUDITY_SANDBOX_BY_USER':
-        warnings.warn(
-            '"SETTING_CRUDITY_SANDBOX_BY_USER" is deprecated; '
-            'use crudity.setting_keys.sandbox_key.id instead.',
-            DeprecationWarning,
-        )
-        return 'crudity-crudity_sandbox_by_user'
-
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+# def __getattr__(name):
+#     if name == 'SETTING_CRUDITY_SANDBOX_BY_USER':
+#         warnings.warn(
+#             '"SETTING_CRUDITY_SANDBOX_BY_USER" is deprecated; '
+#             'use crudity.setting_keys.sandbox_key.id instead.',
+#             DeprecationWarning,
+#         )
+#         return 'crudity-crudity_sandbox_by_user'
+#
+#     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
