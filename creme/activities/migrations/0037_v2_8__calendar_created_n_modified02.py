6d0692111325f715ede2cc09198e943041b06f00
Guillaume Englert <genglert@hybird.org>
2025-10-15 11:37:48 +0200

    Add fields "created" & "modified" in Calendar.

---
+++ creme/activities/migrations/0037_v2_8__calendar_created_n_modified02.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+from django.utils.timezone import now
+
+import creme.creme_core.models.fields as core_fields
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('activities', '0036_v2_8__calendar_created_n_modified01'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='calendar',
+            name='created',
+            field=core_fields.CreationDateTimeField(blank=True, default=now, editable=False),
+        ),
+        migrations.AlterField(
+            model_name='calendar',
+            name='modified',
+            field=core_fields.ModificationDateTimeField(blank=True, default=now, editable=False),
+        ),
+    ]
