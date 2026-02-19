01455c3471e3dccb4d7f5cfdb8601e9224815117
Guillaume Englert <genglert@hybird.org>
2026-02-19 16:51:09 +0100

    Improve the root script.

--- creme/manage.py
+++ creme/manage.py
@@ -3,16 +3,35 @@ import os
 import sys
 
 
-def is_coverage_parallel_mode(argv):
-    if 'test' not in argv or '--parallel' not in argv:
-        # Not running tests, or running tests with one process
-        return False
+# def is_coverage_parallel_mode(argv):
+#     if 'test' not in argv or '--parallel' not in argv:
+#         # Not running tests, or running tests with one process
+#         return False
+#
+#     # `COVERAGE_PROCESS_START` environment variable only has to be defined
+#     if os.getenv("COVERAGE_PROCESS_START") is None:
+#         return False
+#
+#     return True
+def is_test_mode(argv):
+    return 'test' in argv
 
-    # `COVERAGE_PROCESS_START` environment variable only has to be defined
-    if os.getenv("COVERAGE_PROCESS_START") is None:
-        return False
 
-    return True
+def is_parallel_mode(argv):
+    if '--parallel' in argv:
+        return True
+
+    prefix = '--parallel='
+    for value in argv:
+        if value.startswith(prefix):
+            return value.removeprefix(prefix) != '1'
+
+    return False
+
+
+def is_coverage_mode():
+    # NB: `COVERAGE_PROCESS_START` environment variable only has to be defined
+    return os.getenv("COVERAGE_PROCESS_START") is not None
 
 
 def start_parallel_coverage():
@@ -33,7 +52,9 @@ def execute():
     from django.core.management import execute_from_command_line
 
     argv = sys.argv
-    if is_coverage_parallel_mode(argv):
+    # if is_coverage_parallel_mode(argv):
+    #     start_parallel_coverage()
+    if is_test_mode(argv) and is_parallel_mode(argv) and is_coverage_mode():
         start_parallel_coverage()
 
     execute_from_command_line(argv)
