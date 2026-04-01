cc0ed50e5338729b73e67a21425d51bdc1471bfc
Guillaume Englert <genglert@hybird.org>
2026-04-01 15:11:14 +0200

    In 'commercial', the classes 'bricks.*MatrixBrick' are not registered anymore.

--- creme/commercial/apps.py
+++ creme/commercial/apps.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2015-2024  Hybird
+#    Copyright (C) 2015-2026  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -56,9 +56,9 @@ class CommercialConfig(CremeAppConfig):
             bricks.AssetsBrick,
             bricks.CharmsBrick,
             bricks.EvaluatedOrgasBrick,
-            bricks.AssetsMatrixBrick,
-            bricks.CharmsMatrixBrick,
-            bricks.AssetsCharmsMatrixBrick,
+            # bricks.AssetsMatrixBrick,
+            # bricks.CharmsMatrixBrick,
+            # bricks.AssetsCharmsMatrixBrick,
             bricks.ActObjectivesBrick,
             bricks.RelatedOpportunitiesBrick,
             bricks.PatternComponentsBrick,
