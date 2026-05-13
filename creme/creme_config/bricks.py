79c8a3b56408aaae54705ae5daa7b6668dfff37a
Guillaume Englert <genglert@hybird.org>
2026-05-13 11:39:36 +0200

    Fix the permissions on users/teams configuration bricks.

--- creme/creme_config/bricks.py
+++ creme/creme_config/bricks.py
@@ -619,6 +619,7 @@ class UsersBrick(_ConfigAdminBrick):
     dependencies = (User,)
     order_by = 'username'
     template_name = 'creme_config/bricks/users.html'
+    permissions = config_auth.user_config_perm.as_perm
     search_fields = ['username', 'last_name', 'first_name', 'displayed_name']
 
     # def detailview_display(self, context):
@@ -680,6 +681,7 @@ class TeamsBrick(_ConfigAdminBrick):
     dependencies = (User,)
     order_by = 'username'
     template_name = 'creme_config/bricks/teams.html'
+    permissions = config_auth.user_config_perm.as_perm
 
     # def detailview_display(self, context):
     def render(self, context):
