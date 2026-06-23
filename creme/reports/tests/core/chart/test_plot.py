6c3f8b5a16308753492f348dcc699acd82050ece
Guillaume Englert <genglert@hybird.org>
2026-06-23 09:23:05 +0200

    Add unit tests for 'reports.core.chart.plot'.

---
+++ creme/reports/tests/core/chart/test_plot.py
@@ -0,0 +1,32 @@
+from creme.creme_core.tests.base import CremeTestCase
+from creme.reports.core.chart import plot
+
+
+class PlotTestCase(CremeTestCase):
+    def test_plot(self):
+        name1 = 'barchart'
+        label1 = 'Histogram'
+        plot1 = plot.Bar(name=name1, label=label1)
+        self.assertEqual(name1, plot1.name)
+        self.assertEqual(label1, plot1.label)
+
+        # ---
+        name2 = 'piechart'
+        label2 = 'Pie'
+        plot2 = plot.Pie(name=name2, label=label2)
+        self.assertEqual(name2, plot2.name)
+        self.assertEqual(label2, plot2.label)
+
+    def test_registry(self):
+        name1 = 'barchart'
+        name2 = 'piechart'
+
+        plot1 = plot.Bar(name=name1, label='Histogram')
+        plot2 = plot.Pie(name=name2, label='Pie')
+        registry = plot.PlotRegistry().register(plot1, plot2)
+
+        self.assertIs(plot1, registry.get(name1))
+        self.assertIs(plot2, registry.get(name2))
+        self.assertIsNone(registry.get('invalid'))
+
+        self.assertListEqual([plot1, plot2], [*registry])
