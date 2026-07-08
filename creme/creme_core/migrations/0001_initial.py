3c25bc28d6d4c5bf410817e6c756fc4dd7bbac7f
Guillaume Englert <genglert@hybird.org>
2026-07-08 09:49:21 +0200

    Natural ordering for CustomFieldEnumValue is now "id".

--- creme/creme_core/migrations/0001_initial.py
+++ creme/creme_core/migrations/0001_initial.py
@@ -490,6 +490,9 @@ class Migration(migrations.Migration):
                     )
                 ),
             ],
+            options={
+                'ordering': ('id',),
+            },
         ),
         migrations.CreateModel(
             name='CustomFieldEnum',
