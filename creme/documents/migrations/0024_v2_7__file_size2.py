49c5f02702de4ca293b30cd0ef3fe0570426de05
Hugo Smett <hsmett@hybird.org>
2026-08-25 15:59:22 +0200

    Fix slow documents migration

--- creme/documents/migrations/0024_v2_7__file_size2.py
+++ creme/documents/migrations/0024_v2_7__file_size2.py
@@ -1,14 +1,42 @@
 from django.db import migrations
 
+BULK_SIZE = 2000
+SHOW_PROGRESS = False
 
 def fill_sizes(apps, schema_editor):
-    for document in apps.get_model('documents', 'Document').objects.filter(file_size=None):
-        try:
-            document.file_size = document.filedata.size
-        except FileNotFoundError:
-            document.file_size = 0
+    model = apps.get_model('documents', 'Document')
+    queryset = model.objects.filter(file_size=None).order_by("pk").only('pk', "filedata")
 
-        document.save()
+    if SHOW_PROGRESS:
+        total = queryset.count()
+
+    def fetch_documents(previous_batch_last_pk=None):
+        qs = queryset.all()
+        if previous_batch_last_pk:
+            qs = qs.filter(pk__gt=previous_batch_last_pk)
+        return list(qs[:BULK_SIZE])
+
+    documents = fetch_documents()
+
+    if SHOW_PROGRESS:
+        count = 0
+        print()
+
+    while documents:
+        for document in documents:
+            try:
+                document.file_size = document.filedata.size
+            except FileNotFoundError:
+                document.file_size = 0
+
+        model.objects.bulk_update(documents, ["file_size"])
+
+        if SHOW_PROGRESS:
+            count += len(documents)
+            percent = count / total * 100
+            print(f"Processed {percent:.2f}%", end="\r", flush=True)
+
+        documents = fetch_documents(previous_batch_last_pk=documents[-1].pk)
 
 
 class Migration(migrations.Migration):
@@ -17,5 +45,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(fill_sizes),
+        migrations.RunPython(fill_sizes, migrations.RunPython.noop),
     ]
