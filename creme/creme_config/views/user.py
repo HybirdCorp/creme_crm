dbd87386816f0d59aba1c55c90bf4f3e8bf949aa
Guillaume Englert <genglert@hybird.org>
2026-09-08 19:20:57 +0200

    Fix the permissions of the view which hides inactive users.

--- creme/creme_config/views/user.py
+++ creme/creme_config/views/user.py
@@ -1,6 +1,6 @@
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2025  Hybird
+#    Copyright (C) 2009-2026  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -207,5 +207,6 @@ class UserActivation(generic.CheckedView):
 
 
 class HideInactiveUsers(BrickStateExtraDataSetting):
+    permissions = user_config_perm.as_perm
     brick_cls = bricks.UsersBrick
     data_key = constants.BRICK_STATE_HIDE_INACTIVE_USERS
