3dbce9a601d1bcf9bc34d541fc0b9c6a1ac07c91
Guillaume Englert <genglert@hybird.org>
2026-09-11 15:04:47 +0200

    Improve unit tests for QuerysetBrick.

--- creme/creme_core/tests/gui/test_bricks.py
+++ creme/creme_core/tests/gui/test_bricks.py
@@ -3,6 +3,7 @@ from functools import partial
 
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import PermissionDenied
+from django.db.models.query import QuerySet
 from django.utils.translation import gettext as _
 
 from creme.creme_core.constants import MODELBRICK_ID
@@ -2502,7 +2503,8 @@ class QuerysetBrickTestCase(CremeTestCase):
             FakeContact.objects.filter(description=description),
         )
 
-        page = template_context['page']
+        with self.assertNoException():
+            page = template_context['page']
         self.assertEqual(2, page.number)
 
     def test_order__not_in_request(self):
@@ -2523,9 +2525,13 @@ class QuerysetBrickTestCase(CremeTestCase):
 
         with self.assertNoException():
             page = template_context['page']
-            model = page.object_list.model
+            order_by = template_context['order_by']
+            qs = page.object_list
 
-        self.assertEqual(FakeContact, model)
+        self.assertIsInstance(qs, QuerySet)
+        self.assertEqual(FakeContact, qs.model)
+        self.assertTupleEqual(('last_name', ), qs.query.order_by)
+        self.assertEqual('last_name', order_by)
         self._assertPageOrderedLike(page, [cranel, crozzo, wallen])
 
     def test_order__invalid_attribute(self):
@@ -2544,12 +2550,16 @@ class QuerysetBrickTestCase(CremeTestCase):
         cranel = create_contact(first_name='Bell', last_name='Cranel')
         crozzo = create_contact(first_name='Welf', last_name='Crozzo')
 
-        brick = ProblematicBrick()
-        template_context = brick.get_template_context(
-            self.build_context(user=user),
-            FakeContact.objects.all(),
-        )
-        self._assertPageOrderedLike(template_context['page'], [cranel, crozzo, wallen])
+        with self.assertLogs(level='WARNING'):
+            ctxt = ProblematicBrick().get_template_context(
+                self.build_context(user=user),
+                FakeContact.objects.all(),
+            )
+        self.assertEqual('', ctxt['order_by'])
+
+        page = ctxt['page']
+        self.assertTupleEqual((), page.object_list.query.order_by)
+        self._assertPageOrderedLike(page, [cranel, crozzo, wallen])
 
     def test_order__in_request(self):
         """Order in request: valid field."""
@@ -2564,19 +2574,27 @@ class QuerysetBrickTestCase(CremeTestCase):
         # brick = self.OrderedBrick()
         brick = self.TestOrderedBrick()
 
-        # ASC
-        template_context = brick.get_template_context(
+        # ASC ---
+        asc_ctxt = brick.get_template_context(
             self.build_context(user=user, url=f'/?{brick.id}_order=first_name'),
             FakeContact.objects.all(),
         )
-        self._assertPageOrderedLike(template_context['page'], [aiz, bell, lili, welf])
+        self.assertEqual('first_name', asc_ctxt['order_by'])
 
-        # DESC
-        template_context = brick.get_template_context(
+        asc_page = asc_ctxt['page']
+        self.assertTupleEqual(('first_name', ), asc_page.object_list.query.order_by)
+        self._assertPageOrderedLike(asc_page, [aiz, bell, lili, welf])
+
+        # DESC ---
+        desc_ctxt = brick.get_template_context(
             self.build_context(user=user, url=f'/?{brick.id}_order=-first_name'),
             FakeContact.objects.all(),
         )
-        self._assertPageOrderedLike(template_context['page'], [welf, lili, bell, aiz])
+        self.assertEqual('-first_name', desc_ctxt['order_by'])
+
+        desc_page = desc_ctxt['page']
+        self.assertTupleEqual(('-first_name',), desc_page.object_list.query.order_by)
+        self._assertPageOrderedLike(desc_page, [welf, lili, bell, aiz])
 
     def test_order__in_request__invalid_field(self):
         """Order in request: invalid field."""
@@ -2589,15 +2607,20 @@ class QuerysetBrickTestCase(CremeTestCase):
 
         # brick = self.OrderedBrick()
         brick = self.TestOrderedBrick()
-        template_context = brick.get_template_context(
-            self.build_context(user=user, url=f'/?{brick.id}_order=unknown'),
-            FakeContact.objects.all(),
-        )
+
+        with self.assertLogs(level='WARNING'):
+            template_context = brick.get_template_context(
+                self.build_context(user=user, url=f'/?{brick.id}_order=unknown'),
+                FakeContact.objects.all(),
+            )
 
         with self.assertNoException():
             page = template_context['page']
+            order_by = template_context['order_by']
             [*page.object_list]  # NOQA
 
+        self.assertEqual('', order_by)
+        self.assertTupleEqual((), page.object_list.query.order_by)
         self._assertPageOrderedLike(page, [cranel, crozzo, wallen])
 
     def test_order__in_request__no_sortable(self):
@@ -2611,15 +2634,22 @@ class QuerysetBrickTestCase(CremeTestCase):
 
         # brick = self.OrderedBrick()
         brick = self.TestOrderedBrick()
-        template_context = brick.get_template_context(
-            self.build_context(user=user, url=f'/?{brick.id}_order=languages'),
-            FakeContact.objects.all()
-        )
+
+        with self.assertLogs(level='WARNING') as log_mngr:
+            template_context = brick.get_template_context(
+                self.build_context(user=user, url=f'/?{brick.id}_order=languages'),
+                FakeContact.objects.all(),
+            )
+        self.assertTrue(log_mngr.output)
+        self.assertIn('the field "languages" is not sortable', log_mngr.output[0])
 
         with self.assertNoException():
             page = template_context['page']
+            order_by = template_context['order_by']
             [*page.object_list]  # NOQA
 
+        self.assertEqual('', order_by)
+        self.assertTupleEqual((), page.object_list.query.order_by)
         self._assertPageOrderedLike(page, [cranel, crozzo, wallen])
 
 
