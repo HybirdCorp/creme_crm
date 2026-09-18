59c5c6a5f28bc0747199c3e0449b0d4b02343f77
Guillaume Englert <genglert@hybird.org>
2026-09-18 16:07:49 +0200

    CremePropertyType & its manager got methods for portable key.

--- creme/creme_core/models/creme_property.py
+++ creme/creme_core/models/creme_property.py
@@ -161,6 +161,15 @@ class CremePropertyTypeManager(models.Manager):
             | Q(subject_ctypes__isnull=True)
         )
 
+    # TODO: cache (see MinionModel)?
+    def get_by_portable_key(self, key: str) -> CremePropertyType:
+        """@raise django.core.exceptions.ValidationError If the key is not a valid UUID."""
+        return self.get(uuid=key)
+
+    def get_by_portable_keys(self, /, keys: Iterable[str]) -> Iterator[CremePropertyType]:
+        """@raise django.core.exceptions.ValidationError If the key is not a valid UUID."""
+        yield from self.filter(uuid__in=keys)
+
     def proxy(self,
               subject_models: Iterable[type[CremeEntity]] = (),
               **kwargs) -> CremePropertyTypeProxy:
@@ -388,6 +397,10 @@ class CremePropertyType(CremeModel):
         )
 
         return ctype in ctypes
+
+    def portable_key(self) -> str:
+        """See CremeEntity.portable_key()."""
+        return str(self.uuid)
 
 
 class CremeProperty(CremeModel):
